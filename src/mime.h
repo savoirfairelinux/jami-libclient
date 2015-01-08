7888f49e401fbb645eeff5e0ee197292a15eafc0
Emmanuel Lepage Vallee <emmanuel.lepage@savoirfairelinux.com>
2015-01-08 15:30:46 -0500

    [ #63393 ] Remove project to Ring-KDE and libRingClient

--- src/mime.h
+++ src/mime.h
@@ -1,5 +1,5 @@
 /****************************************************************************
- *   Copyright (C) 2013-2015 by Savoir-Faire Linux                          *
+ *   Copyright (C) 2012-2015 by Savoir-Faire Linux                          *
  *   Author : Emmanuel Lepage Vallee <emmanuel.lepage@savoirfairelinux.com> *
  *                                                                          *
  *   This library is free software; you can redistribute it and/or          *
@@ -15,48 +15,16 @@
  *   You should have received a copy of the GNU General Public License      *
  *   along with this program.  If not, see <http://www.gnu.org/licenses/>.  *
  ***************************************************************************/
-#ifndef OUTPUTDEVICEMODEL_H
-#define OUTPUTDEVICEMODEL_H
+#ifndef MIME_H
+#define MIME_H
 
-#include <QtCore/QAbstractListModel>
-
-//Qt
-#include <QtCore/QStringList>
-
-//Ring
-#include "../typedefs.h"
-
-class OutputDeviceModelPrivate;
-
-namespace Audio {
-
-class LIB_EXPORT OutputDeviceModel  : public QAbstractListModel {
-   Q_OBJECT
-public:
-   explicit OutputDeviceModel(const QObject* parent);
-   virtual ~OutputDeviceModel();
-
-   //Models function
-   virtual QVariant      data    ( const QModelIndex& index, int role = Qt::DisplayRole ) const override;
-   virtual int           rowCount( const QModelIndex& parent = QModelIndex()            ) const override;
-   virtual Qt::ItemFlags flags   ( const QModelIndex& index                             ) const override;
-   virtual bool          setData ( const QModelIndex& index, const QVariant &value, int role)   override;
-
-   //Getters
-   QModelIndex currentDevice() const;
-
-   //Setters
-   void setCurrentDevice(const QModelIndex& index);
-   void setCurrentDevice(int idx);
-
-   //Mutator
-   void reload();
-
-private:
-   QScopedPointer<OutputDeviceModelPrivate> d_ptr;
-   Q_DECLARE_PRIVATE(OutputDeviceModel)
-};
-
+namespace RingMimes {
+   constexpr static const char* CALLID      = "text/ring.call.id"      ;
+   constexpr static const char* CONTACT     = "text/ring.contact"      ;
+   constexpr static const char* HISTORYID   = "text/ring.history.id"   ;
+   constexpr static const char* PHONENUMBER = "text/ring.phone.number" ;
+   constexpr static const char* PLAIN_TEXT  = "text/plain"             ;
 }
 
 #endif
+
