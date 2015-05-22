8925175c8c24b8a86db81397b6e547611253fb82
Emmanuel Lepage Vallee <emmanuel.lepage@savoirfairelinux.com>
2015-05-22 15:54:06 -0400

    macro: Move the code from libkring to LRC
    The KDE client had this code because it had external dependencies.
    Those dependencies have been merged into Qt5Core and thus can be merged
    into LRC.
    
    The macro system allow keyboard shortcut or other user
    experience element to be executed. For example, it allow to attach
    a keyboard shortcut to call someone, even if it has a phone "menu"
    behind the number.
    
    Refs #73706
    
    Change-Id: Idff34193e229144cba0412ca07fecae03e0abe40

---
+++ src/macro.h
@@ -0,0 +1,78 @@
+/****************************************************************************
+ *   Copyright (C) 2013-2015 by Savoir-Faire Linux                          *
+ *   Author : Emmanuel Lepage Vallee <emmanuel.lepage@savoirfairelinux.com> *
+ *                                                                          *
+ *   This library is free software; you can redistribute it and/or          *
+ *   modify it under the terms of the GNU Lesser General Public             *
+ *   License as published by the Free Software Foundation; either           *
+ *   version 2.1 of the License, or (at your option) any later version.     *
+ *                                                                          *
+ *   This library is distributed in the hope that it will be useful,        *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of         *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU      *
+ *   Lesser General Public License for more details.                        *
+ *                                                                          *
+ *   You should have received a copy of the GNU General Public License      *
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.  *
+ ***************************************************************************/
+#ifndef MACRO_H
+#define MACRO_H
+
+//Qt
+#include <QtCore/QObject>
+#include <QtCore/QDateTime>
+#include <QtCore/QModelIndex>
+
+//Ring
+class MacroModel;
+#include "typedefs.h"
+
+class MacroPrivate;
+
+class LIB_EXPORT Macro : public QObject {
+   #pragma GCC diagnostic push
+   #pragma GCC diagnostic ignored "-Wzero-as-null-pointer-constant"
+   Q_OBJECT
+   #pragma GCC diagnostic pop
+
+   friend class MacroModel       ; //Use factory method
+   friend class MacroModelPrivate; //Use factory method
+
+public:
+   explicit Macro(const Macro* macro);
+
+   //Getters
+   QString  name        () const;
+   QString  description () const;
+   QString  sequence    () const;
+   QString  escaped     () const;
+   QString  id          () const;
+   int      delay       () const;
+   QString  category    () const;
+   QVariant action      () const;
+
+   QModelIndex index();
+
+   //Setters
+   void setName         (const QString &value);
+   void setDescription  (const QString &value);
+   void setSequence     (const QString &value);
+   void setEscaped      (const QString &value);
+   void setId           (const QString &value);
+   void setDelay        (int            value);
+   void setCategory     (const QString &value);
+
+private:
+   explicit Macro(QObject* parent = nullptr);
+
+   MacroPrivate* d_ptr;
+   Q_DECLARE_PRIVATE(Macro)
+
+public Q_SLOTS:
+   void execute();
+
+Q_SIGNALS:
+   void changed(Macro*);
+};
+
+#endif
