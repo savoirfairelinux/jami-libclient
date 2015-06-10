b3e5beac78e47ec31f7ce20ee100e06e0764c8b0
Emmanuel Lepage Vallee <emmanuel.lepage@savoirfairelinux.com>
2015-06-10 15:25:50 -0400

    recentmodel: Add the model skeleton
    It doesn't do anything yet and the design may still change.
    
    Refs #75337
    
    Change-Id: I3081bde50197918426c5ad0870a7cd68c82f2417

---
+++ src/recentmodel.h
@@ -0,0 +1,53 @@
+/************************************************************************************
+ *   Copyright (C) 2015 by Savoir-Faire Linux                                       *
+ *   Author : Emmanuel Lepage Vallee <emmanuel.lepage@savoirfairelinux.com>         *
+ *                                                                                  *
+ *   This library is free software; you can redistribute it and/or                  *
+ *   modify it under the terms of the GNU Lesser General Public                     *
+ *   License as published by the Free Software Foundation; either                   *
+ *   version 2.1 of the License, or (at your option) any later version.             *
+ *                                                                                  *
+ *   This library is distributed in the hope that it will be useful,                *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of                 *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU              *
+ *   Lesser General Public License for more details.                                *
+ *                                                                                  *
+ *   You should have received a copy of the GNU Lesser General Public               *
+ *   License along with this library; if not, write to the Free Software            *
+ *   Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA *
+ ***********************************************************************************/
+#ifndef RECENTMODEL_H
+#define RECENTMODEL_H
+#include <QtCore/QAbstractItemModel>
+
+#include <typedefs.h>
+
+class RecentModelPrivate;
+
+class LIB_EXPORT RecentModel : public QAbstractItemModel
+{
+   Q_OBJECT
+public:
+
+   //Model implementation
+   virtual bool          setData     ( const QModelIndex& index, const QVariant &value, int role   ) override;
+   virtual QVariant      data        ( const QModelIndex& index, int role = Qt::DisplayRole        ) const override;
+   virtual int           rowCount    ( const QModelIndex& parent = QModelIndex()                   ) const override;
+   virtual Qt::ItemFlags flags       ( const QModelIndex& index                                    ) const override;
+   virtual int           columnCount ( const QModelIndex& parent = QModelIndex()                   ) const override;
+   virtual QModelIndex   parent      ( const QModelIndex& index                                    ) const override;
+   virtual QModelIndex   index       ( int row, int column, const QModelIndex& parent=QModelIndex()) const override;
+   virtual QVariant      headerData  ( int section, Qt::Orientation orientation, int role = Qt::DisplayRole ) const override;
+   virtual QHash<int,QByteArray> roleNames() const override;
+
+   static RecentModel* instance();
+private:
+   explicit RecentModel(QObject* parent = nullptr);
+   virtual ~RecentModel();
+
+   RecentModelPrivate* d_ptr;
+   Q_DECLARE_PRIVATE(RecentModel)
+};
+Q_DECLARE_METATYPE(RecentModel*)
+
+#endif
