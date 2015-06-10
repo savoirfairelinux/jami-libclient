87a6f3836f8fce0b212fcaf8294b50fb1ff36fab
Emmanuel Lepage Vallee <emmanuel.lepage@savoirfairelinux.com>
2015-06-10 15:25:50 -0400

    recentmodel: Add the model skeleton
    It doesn't do anything yet and the design may still change.
    
    Refs #75337
    
    Change-Id: I3081bde50197918426c5ad0870a7cd68c82f2417

---
+++ src/recentmodel.cpp
@@ -0,0 +1,201 @@
+/************************************************************************************
+ *   Copyright (C) 2015 by Savoir-Faire Linux                                       *
+ *   Author : Emmanuel Lepage Vallee <emmanuel.lepage@savoirfairelinux.com>         *
+ *                                                                                  *
+ *   This library is free software; you can redistribute it and/or                  *
+ *   modify it under the terms of the GNU Lesser General Public                     *
+ *   License as published by the Free Software Foundation; either                   *
+ *   version 2.1 of the License, or (at your option) any later version.             *
+ *                                                                                  *
+ *   This library is distributed in the hope that it will be useful,                *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of                 *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU              *
+ *   Lesser General Public License for more details.                                *
+ *                                                                                  *
+ *   You should have received a copy of the GNU Lesser General Public               *
+ *   License along with this library; if not, write to the Free Software            *
+ *   Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA *
+ ***********************************************************************************/
+#include "recentmodel.h"
+
+//Qt
+#include <QtCore/QCoreApplication>
+
+//Ring
+#include <call.h>
+#include <person.h>
+#include <contactmethod.h>
+
+struct CallGroup
+{
+   QVector<Call*>  m_lCalls   ;
+   Call::Direction m_Direction;
+   bool            m_Missed   ;
+};
+
+struct RecentViewNode
+{
+   //Types
+   enum class Type {
+      PERSON            ,
+      CONTACT_METHOD    ,
+      CALL              ,
+      CALL_GROUP        ,
+      TEXT_MESSAGE      ,
+      TEXT_MESSAGE_GROUP,
+   };
+
+   //Constructor
+   explicit RecentViewNode();
+   virtual ~RecentViewNode();
+
+   //Attributes
+   int                    m_Index    ;
+   Type                   m_Type     ;
+   RecentViewNode*        m_pParent  ;
+   QList<RecentViewNode*> m_lChildren;
+   union {
+      Person*        m_pPerson       ;
+      ContactMethod* m_pContactMethod;
+      Call*          m_pCall         ;
+      CallGroup*     m_pCallGroup    ;
+      //ImConversationIterator; //TODO
+      //ImConversationIterator;
+   } m_uContent;
+};
+
+class RecentModelPrivate
+{
+public:
+   QVector<RecentViewNode*> m_lTopLevel;
+};
+
+RecentModel::RecentModel(QObject* parent) : QAbstractItemModel(parent), d_ptr(new RecentModelPrivate())
+{
+
+}
+
+RecentModel::~RecentModel()
+{
+   for (RecentViewNode* n : d_ptr->m_lTopLevel)
+      delete n;
+
+   delete d_ptr;
+}
+
+RecentViewNode::RecentViewNode()
+{
+
+}
+
+RecentViewNode::~RecentViewNode()
+{
+   for (RecentViewNode* n : m_lChildren) {
+      delete n;
+   }
+}
+
+RecentModel* RecentModel::instance()
+{
+   RecentModel* m_spInstance =  new RecentModel(QCoreApplication::instance());
+   return m_spInstance;
+}
+
+QHash<int,QByteArray> RecentModel::roleNames() const
+{
+   static QHash<int, QByteArray> roles = QAbstractItemModel::roleNames();
+   /*static bool initRoles = false;
+   if (!initRoles) {
+      initRoles = true;
+   }*/
+
+   return roles;
+}
+
+bool RecentModel::setData( const QModelIndex& index, const QVariant &value, int role)
+{
+   Q_UNUSED(index)
+   Q_UNUSED(value)
+   Q_UNUSED(role)
+   return false;
+}
+
+QVariant RecentModel::data( const QModelIndex& index, int role ) const
+{
+   if (!index.isValid())
+      return QVariant();
+
+   RecentViewNode* node = static_cast<RecentViewNode*>(index.internalPointer());
+
+   switch(node->m_Type) {
+      case RecentViewNode::Type::PERSON            :
+         return node->m_uContent.m_pPerson->roleData(role);
+      case RecentViewNode::Type::CONTACT_METHOD    :
+         return node->m_uContent.m_pContactMethod->roleData(role);
+      case RecentViewNode::Type::CALL              :
+         return node->m_uContent.m_pCall->roleData(role);
+      case RecentViewNode::Type::CALL_GROUP        :
+         return node->m_uContent.m_pCallGroup->m_lCalls[0]->roleData(role);
+      case RecentViewNode::Type::TEXT_MESSAGE      :
+      case RecentViewNode::Type::TEXT_MESSAGE_GROUP:
+         //TODO
+         break;
+   }
+
+   return QVariant();
+}
+
+int RecentModel::rowCount( const QModelIndex& parent ) const
+{
+   if (!parent.isValid())
+      return d_ptr->m_lTopLevel.size();
+
+   RecentViewNode* node = static_cast<RecentViewNode*>(parent.internalPointer());
+
+   return node->m_lChildren.size();
+}
+
+Qt::ItemFlags RecentModel::flags( const QModelIndex& index ) const
+{
+   return index.isValid() ? Qt::ItemIsEnabled | Qt::ItemIsSelectable : Qt::NoItemFlags;
+}
+
+int RecentModel::columnCount( const QModelIndex& parent ) const
+{
+   Q_UNUSED(parent)
+   return 1;
+}
+
+QModelIndex RecentModel::parent( const QModelIndex& index ) const
+{
+   if (!index.isValid())
+      return QModelIndex();
+
+   RecentViewNode* node = static_cast<RecentViewNode*>(index.internalPointer());
+
+   if (!node->m_pParent)
+      return QModelIndex();
+
+   return createIndex(node->m_pParent->m_Index, 0, node->m_pParent);
+}
+
+QModelIndex RecentModel::index( int row, int column, const QModelIndex& parent) const
+{
+   if (!parent.isValid() && row >= 0 && row < d_ptr->m_lTopLevel.size() && !column)
+      return createIndex(row, 0, d_ptr->m_lTopLevel[row]);
+
+   RecentViewNode* node = static_cast<RecentViewNode*>(parent.internalPointer());
+
+   if (row >= 0 && row < node->m_lChildren.size())
+      return createIndex(row, 0, node->m_lChildren[row]);
+
+   return QModelIndex();
+}
+
+QVariant RecentModel::headerData( int section, Qt::Orientation orientation, int role) const
+{
+   if (!section && role == Qt::DisplayRole && orientation == Qt::Horizontal)
+      return tr("Recent persons");
+
+   return QVariant();
+}
