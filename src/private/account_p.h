3d5f3fea35cd36e8bfa02102e337325304236020
Emmanuel Lepage Vallee <emmanuel.lepage@savoirfairelinux.com>
2015-07-03 14:56:29 -0400

    security: Implement the trust requests
    This commit implement the daemon trust request system.
    
    Both incoming and outgoing requests should be using the
    daemon API. No tests have been done, but the general
    framework is in place.
    
    Refs #76869
    
    Change-Id: I0113ba43ceaee2a850074dc2b9b6d4c3ba0ade8a

--- src/private/account_p.h
+++ src/private/account_p.h
@@ -35,6 +35,7 @@ class ProtocolModel;
 class NetworkInterfaceModel;
 class BootstrapModel;
 class DaemonCertificateCollection;
+class PendingTrustRequestModel;
 
 typedef void (AccountPrivate::*account_function)();
 
@@ -120,22 +121,23 @@ public:
    void save   ();
    void reloadMod() {reload();modify();}
 
-   CredentialModel*             m_pCredentials            ;
-   CodecModel*                  m_pCodecModel             ;
-   KeyExchangeModel*            m_pKeyExchangeModel       ;
-   CipherModel*                 m_pCipherModel            ;
-   AccountStatusModel*          m_pStatusModel            ;
-   SecurityEvaluationModel*     m_pSecurityEvaluationModel;
-   TlsMethodModel*              m_pTlsMethodModel         ;
-   ProtocolModel*               m_pProtocolModel          ;
-   BootstrapModel*              m_pBootstrapModel         ;
-   QAbstractItemModel*          m_pKnownCertificates      ;
-   QAbstractItemModel*          m_pBannedCertificates     ;
-   QAbstractItemModel*          m_pAllowedCertificates    ;
-   NetworkInterfaceModel*       m_pNetworkInterfaceModel  ;
-   DaemonCertificateCollection* m_pAllowedCerts           ;
-   DaemonCertificateCollection* m_pBannedCerts            ;
-   Account::EditState           m_CurrentState            ;
+   CredentialModel*             m_pCredentials             ;
+   CodecModel*                  m_pCodecModel              ;
+   KeyExchangeModel*            m_pKeyExchangeModel        ;
+   CipherModel*                 m_pCipherModel             ;
+   AccountStatusModel*          m_pStatusModel             ;
+   SecurityEvaluationModel*     m_pSecurityEvaluationModel ;
+   TlsMethodModel*              m_pTlsMethodModel          ;
+   ProtocolModel*               m_pProtocolModel           ;
+   BootstrapModel*              m_pBootstrapModel          ;
+   QAbstractItemModel*          m_pKnownCertificates       ;
+   QAbstractItemModel*          m_pBannedCertificates      ;
+   QAbstractItemModel*          m_pAllowedCertificates     ;
+   NetworkInterfaceModel*       m_pNetworkInterfaceModel   ;
+   DaemonCertificateCollection* m_pAllowedCerts            ;
+   DaemonCertificateCollection* m_pBannedCerts             ;
+   Account::EditState           m_CurrentState             ;
+   PendingTrustRequestModel*    m_pPendingTrustRequestModel;
 
    QHash<int, Account::RoleStatus> m_hRoleStatus;
 
