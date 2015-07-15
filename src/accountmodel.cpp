a26445cbe3b78201f9c098d9433ea60ad1a7c955
Emmanuel Lepage Vallee <emmanuel.lepage@savoirfairelinux.com>
2015-07-15 18:22:52 -0400

    security: Implement the trust requests
    This commit implement the daemon trust request system.
    
    Both incoming and outgoing requests should be using the
    daemon API. No tests have been done, but the general
    framework is in place.
    
    Refs #76869
    
    Change-Id: I0113ba43ceaee2a850074dc2b9b6d4c3ba0ade8a

--- src/accountmodel.cpp
+++ src/accountmodel.cpp
@@ -33,6 +33,8 @@
 #include "mime.h"
 #include "profilemodel.h"
 #include "protocolmodel.h"
+#include "trustrequest.h"
+#include "pendingtrustrequestmodel.h"
 #include "private/account_p.h"
 #include "private/accountmodel_p.h"
 #include "accountstatusmodel.h"
@@ -40,6 +42,7 @@
 #include "dbus/callmanager.h"
 #include "dbus/instancemanager.h"
 #include "codecmodel.h"
+#include "private/pendingtrustrequestmodel_p.h"
 
 QHash<QByteArray,AccountPlaceHolder*> AccountModelPrivate::m_hsPlaceHolder;
 AccountModel*     AccountModelPrivate::m_spAccountList;
@@ -79,6 +82,8 @@ void AccountModelPrivate::init()
       SLOT(slotVolatileAccountDetailsChange(QString,MapStringString)));
    connect(&configurationManager, SIGNAL(mediaParametersChanged(QString))                 ,this ,
       SLOT(slotMediaParametersChanged(QString)));
+   connect(&configurationManager, &ConfigurationManagerInterface::incomingTrustRequest, this,
+           &AccountModelPrivate::slotIncomingTrustRequest);
 
 }
 
@@ -386,6 +391,21 @@ void AccountModelPrivate::slotVolatileAccountDetailsChange(const QString& accoun
       const Account::RegistrationState state = fromDaemonName(a->d_ptr->accountDetail(DRing::Account::ConfProperties::Registration::STATUS));
       a->d_ptr->m_RegistrationState = state;
    }
+}
+
+///When a Ring-DHT trust request arrive
+void AccountModelPrivate::slotIncomingTrustRequest(const QString& accountId, const QString& hash, const QByteArray& payload, time_t time)
+{
+   qDebug() << "INCOMING REQUEST" << accountId << hash << time;
+   Account* a = q_ptr->getById(accountId.toLatin1());
+
+   if (!a) {
+      qWarning() << "Incoming trust request for unknown account" << accountId;
+      return;
+   }
+
+   TrustRequest* r = new TrustRequest(a, hash, time);
+   a->pendingTrustRequestModel()->d_ptr->addRequest(r);
 }
 
 ///Update accounts
