1dae8565de8cd5b7a1ecc58769cc1f17d82b75d6
Edric Milaret <edric.ladent-milaret@savoirfairelinux.com>
2015-07-27 14:19:58 -0400

    FOR TESTING: Implement the "last contacted" smartview
    - Mainly bug fix
    - Also fill the model with call
    
    Issue: #75334
    
    Change-Id: I7d68a4ca7620386122a74e3f4e6b1e1a20e4cf8e

--- src/uri.cpp
+++ src/uri.cpp
@@ -211,7 +211,7 @@ QString URI::hostname() const
 
 /**
  * Check if the URI has an hostname
- * 
+ *
  * This will return true if there is something between '@' and ';' (or an end of line)
  */
 bool URI::hasHostname() const
@@ -538,19 +538,19 @@ QDataStream& operator<<( QDataStream& stream, const URI::ProtocolHint& ph )
 {
    switch(ph) {
       case URI::ProtocolHint::SIP_OTHER:
-         stream << "SIP_OTHER";
+         stream << QStringLiteral("SIP_OTHER");
          break;
       case URI::ProtocolHint::IAX      :
-         stream << "IAX";
+         stream << QStringLiteral("IAX");
          break;
       case URI::ProtocolHint::RING     :
-         stream << "RING";
+         stream << QStringLiteral("RING");
          break;
       case URI::ProtocolHint::IP       :
-         stream << "IP";
+         stream << QStringLiteral("IP");
          break;
       case URI::ProtocolHint::SIP_HOST :
-         stream << "SIP_HOST";
+         stream << QStringLiteral("SIP_HOST");
          break;
    }
    return stream;
