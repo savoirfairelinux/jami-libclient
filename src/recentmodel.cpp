0273af26b76cb9632315af8c23c8c8ea6d494ca9
Stepan Salenikovich <stepan.salenikovich@savoirfairelinux.com>
2015-10-20 17:54:29 -0400

    recentmodel: update indeces when call is removed
    They must be updated because the m_Index refers to the row
    of the child in the model and is used to generate the
    corresponding QModelIndex. Unless the removed call is the
    last child, the m_Index of the other calls may be wrong
    and they will point to the wrong QModelIndex.
    
    Issue: #81454
    Change-Id: I873e11f509eb9323cf153c84bcf9458595b17e74

--- src/recentmodel.cpp
+++ src/recentmodel.cpp
@@ -620,9 +620,16 @@ RecentModelPrivate::removeCall(Call *call)
 {
     if (auto parent = parentNode(call)) {
         if (auto callNode = parent->childNode(call)) {
-            q_ptr->beginRemoveRows(q_ptr->index(parent->m_Index,0), callNode->m_Index, callNode->m_Index);
-            parent->m_lChildren.removeAt(callNode->m_Index);
+            const auto removedIndex = callNode->m_Index;
+            q_ptr->beginRemoveRows(q_ptr->index(parent->m_Index,0), removedIndex, removedIndex);
+            parent->m_lChildren.removeAt(removedIndex);
             delete callNode;
+
+            // update the indices of the remaining children
+            for (int i = removedIndex; i < parent->m_lChildren.size(); ++i) {
+                if (auto child = parent->m_lChildren.at(i))
+                    --child->m_Index;
+            }
             q_ptr->endRemoveRows();
 
             auto parentIdx = q_ptr->index(parent->m_Index, 0);
