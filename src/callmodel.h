b4a3ef3e678abf8b147f824289a279ffc1069b28
Edric Milaret <edric.ladent-milaret@savoirfairelinux.com>
2015-10-20 09:25:14 -0400

    recentmodel: add conference
    - add conference node in recentModel
    - modify createConference to handle more cases
    - add a method in callModel to get conf participant
    - add posibility for a call to have a parent call
    - add a method to join a call to its parent and
    one to know if it has a one
    
    Tuleap: #12
    Change-Id: I4d889bc96e9dc009bcab3e3c037d41bedd8052bb

--- src/callmodel.h
+++ src/callmodel.h
@@ -79,8 +79,8 @@ public:
    Q_PROPERTY(UserActionModel* userActionModel READ userActionModel )
 
    //Call related
-   Q_INVOKABLE Call*       dialingCall       ( const QString& peerName=QString(), Account* account=nullptr );
-   Q_INVOKABLE Call*       dialingCall       ( ContactMethod* cm                                           );
+   Q_INVOKABLE Call*       dialingCall       ( const QString& peerName=QString(), Account* account=nullptr, Call* parent = nullptr );
+   Q_INVOKABLE Call*       dialingCall       (ContactMethod* cm                 , Call* parent = nullptr);
    Q_INVOKABLE void        attendedTransfer  ( Call* toTransfer , Call* target                             );
    Q_INVOKABLE void        transfer          ( Call* toTransfer , const ContactMethod* target              );
    Q_INVOKABLE QByteArray  getMime           ( const Call* call                                            ) const;
@@ -108,6 +108,7 @@ public:
    QItemSelectionModel* selectionModel      () const;
 
    Q_INVOKABLE Call* getCall ( const QModelIndex& idx ) const;
+   Q_INVOKABLE QList<Call*> getConferenceParticipants(Call *conf) const;
 
    //Model implementation
    virtual bool          setData      ( const QModelIndex& index, const QVariant &value, int role   ) override;
