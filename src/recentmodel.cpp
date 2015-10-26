2e024b83eea22959163d98c54d4fd16a7f9c3d51
Stepan Salenikovich <stepan.salenikovich@savoirfairelinux.com>
2015-10-26 16:49:00 -0400

    recentmodel: fix move rows location
    'newPos' is already the correct new location of the row, there
    is no need to do '+ 1' in the case that newPos is not 0
    
    Change-Id: I419ed05ce01c46ed5bdc58785d51ded040c43023
    Tuleap: #48

--- src/recentmodel.cpp
+++ src/recentmodel.cpp
@@ -480,9 +480,9 @@ void RecentModelPrivate::insertNode(RecentViewNode* n, time_t t, bool isNew)
    if (!isNew) {
       if (newPos == n->m_Index)
          return; //Nothing to do
-      if (not q_ptr->beginMoveRows(QModelIndex(), n->m_Index, n->m_Index, QModelIndex(), newPos ? newPos+1 : newPos )) {
+      if (not q_ptr->beginMoveRows(QModelIndex(), n->m_Index, n->m_Index, QModelIndex(), newPos)) {
           qWarning() << "RecentModel: Invalid move detected index : " << n->m_Index
-                     << "newPos: " << (newPos ? newPos+1 : newPos) << "size: " << m_lTopLevelReverted.size();
+                     << "newPos: " << newPos << "size: " << m_lTopLevelReverted.size();
           return;
       }
       m_lTopLevelReverted.removeAt(m_lTopLevelReverted.size()-1-n->m_Index);
