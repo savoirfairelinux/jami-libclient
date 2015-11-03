9e9cf129a9d5648498b053870fe1948bf1aa018f
Alexandre Lision <alexandre.lision@savoirfairelinux.com>
2015-11-03 14:27:51 -0500

    recentmodel: show reachable items
    Only show reachable persons and contact methods in smartlist
    
    Change-Id: I738f4190a3d41105e886169179dd694d6cd07d97
    Tuleap: #55

--- src/recentmodel.cpp
+++ src/recentmodel.cpp
@@ -70,6 +70,7 @@ struct RecentViewNode
    long int               m_Index    ;
    Type                   m_Type     ;
    RecentViewNode*        m_pParent  ;
+   bool                   m_Visible  ;
    QList<RecentViewNode*> m_lChildren;
    QMetaObject::Connection m_ConnectionChanged;
    union {
@@ -218,6 +219,7 @@ RecentViewNode::RecentViewNode()
 
 RecentViewNode::RecentViewNode(Call* c, RecentModelPrivate *model)
 {
+    m_Visible           = true                      ;
     m_pModel            = model                     ;
     m_Type              = RecentViewNode::Type::CALL;
     m_uContent.m_pCall  = c                         ;
@@ -456,7 +458,15 @@ int RecentModel::rowCount( const QModelIndex& parent ) const
 
 Qt::ItemFlags RecentModel::flags( const QModelIndex& index ) const
 {
-   return index.isValid() ? Qt::ItemIsEnabled | Qt::ItemIsSelectable : Qt::NoItemFlags;
+    if (!index.isValid())
+       return Qt::NoItemFlags;
+
+   RecentViewNode* node = static_cast<RecentViewNode*>(index.internalPointer());
+   if (node && node->m_Visible) {
+       return Qt::ItemIsEnabled | Qt::ItemIsSelectable;
+   }
+
+   return Qt::NoItemFlags;
 }
 
 int RecentModel::columnCount( const QModelIndex& parent ) const
@@ -613,6 +623,7 @@ void RecentModelPrivate::slotLastUsedTimeChanged(const Person* p, time_t t)
       n = new RecentViewNode(p, this);
       m_hPersonsToNodes[p] = n;
    }
+   n->m_Visible = p->isActive() && p->isReachable();
 
    insertNode(n, t, isNew);
 }
@@ -628,6 +639,8 @@ void RecentModelPrivate::slotLastUsedChanged(ContactMethod* cm, time_t t)
          n = new RecentViewNode(cm, this);
          m_hCMsToNodes[cm] = n;
       }
+      n->m_Visible = cm->isReachable();
+
       insertNode(n, t, isNew);
    }
 }
@@ -636,6 +649,7 @@ void RecentModelPrivate::slotLastUsedChanged(ContactMethod* cm, time_t t)
 void RecentModelPrivate::slotContactChanged(ContactMethod* cm, Person* np, Person* op)
 {
     Q_UNUSED(np)
+    Q_UNUSED(op)
     // m_hCMsToNodes contains RecentViewNode pointers, take will return a default
     // constructed ptr (e.g nullptr) if key is not in the QHash
     if (auto n = m_hCMsToNodes.take(cm)) {
@@ -855,7 +869,8 @@ PeopleProxy::filterAcceptsRow(int source_row, const QModelIndex & source_parent)
 {
     //Always show the top nodes; in the case of childre, only show if there is more than one
     if (!source_parent.isValid())
-        return QSortFilterProxyModel::filterAcceptsRow(source_row, source_parent);
+        return QSortFilterProxyModel::filterAcceptsRow(source_row, source_parent) &&
+        (sourceModel()->index(source_row,0,source_parent).flags() & Qt::ItemIsEnabled);
     else if (sourceModel()->rowCount(source_parent) > 1 )
         return true;
     return false;
