07dc79bc575fdfda95e773c1016239438ea1a120
Emmanuel Lepage Vallee <elv1313@gmail.com>
2015-12-04 19:19:00 -0500

    interface: Add an Action interface
    This interface will be used to make the UserActionModel
    more flexible
    
    Tuleap: #165
    
    Change-Id: Ib3b025f36091f04f478dde4cd1ab7aa8b6752e0b

---
+++ src/actionextenderdefault.cpp
@@ -0,0 +1,52 @@
+/****************************************************************************
+ *   Copyright (C) 2015 by Savoir-faire Linux                               *
+ *   Author : Emmanuel Lepage <emmanuel.lepage@savoirfairelinux.com>        *
+ *                                                                          *
+ *   This library is free software; you can redistribute it and/or          *
+ *   modify it under the terms of the GNU Lesser General Public             *
+ *   License as published by the Free Software Foundation; either           *
+ *   version 2.1 of the License, or (at your option) any later version.     *
+ *                                                                          *
+ *   This library is distributed in the hope that it will be useful,        *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of         *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU      *
+ *   Lesser General Public License for more details.                        *
+ *                                                                          *
+ *   You should have received a copy of the GNU General Public License      *
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.  *
+ ***************************************************************************/
+#include "actionextenderdefault.h"
+
+#include "macro.h"
+
+namespace Interfaces {
+
+void ActionExtenderDefault::editPerson(Person* p)
+{
+   Q_UNUSED(p)
+}
+
+void ActionExtenderDefault::viewChatHistory(ContactMethod* cm)
+{
+   Q_UNUSED(cm)
+}
+
+void ActionExtenderDefault::copyInformation(QMimeData* data)
+{
+   Q_UNUSED(data)
+}
+
+bool ActionExtenderDefault::warnDeletePerson(Person* p)
+{
+   Q_UNUSED(p)
+   return true;
+}
+
+bool ActionExtenderDefault::warnDeleteCall(Call* c)
+{
+   Q_UNUSED(c)
+   return true;
+}
+
+
+} // namespace Interfaces
