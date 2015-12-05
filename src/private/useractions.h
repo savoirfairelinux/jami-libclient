56d6a068ebb822bcc57f103a8066a6ce2fda370e
Emmanuel Lepage Vallee <elv1313@gmail.com>
2015-12-04 19:49:47 -0500

    UMA: Port trivials actions from KDE client to LRC
    This commit implement many previously "TODO" actions into the
    UserActionModel. Those actions can be used right away without
    additional client specific work.
    
    * Remove calls from history
    * Bookmark assets
    * Call assets
    * Send emails using external application (XDG compatible[1])
    * Delete contacts
    
    The next batch of actions will require client specifics bits.
    
    Also remove left over commented code from when this code was
    ported from the KDE client.
    
    [1] Will require more work for Windows and OSX in later commits
    
    Tuleap #165
    
    Change-Id: I15b14caefb2bb5f55591411e134fcd6ed5953256

--- src/private/useractions.h
+++ src/private/useractions.h
@@ -17,6 +17,13 @@
  ***************************************************************************/
 #pragma once
 
+// Qt
+#include <QtCore/QProcess>
+
+// LRC
+#include <categorizedbookmarkmodel.h>
+#include <categorizedhistorymodel.h>
+
 #include "media/media.h"
 #include "media/audio.h"
 #include "media/video.h"
@@ -29,262 +36,327 @@
  */
 namespace UserActions {
 
-bool addNew();
-bool accept(const QList<Call*> calls);
-bool hangup(const QList<Call*> calls);
-bool refuse(const QList<Call*> calls);
-bool hold(const QList<Call*> calls);
-bool unhold(const QList<Call*> calls);
-bool transfer(const QList<Call*> calls);
-bool recordAudio(const QList<Call*> calls);
-bool recordVideo(const QList<Call*> calls);
-bool recordText(const QList<Call*> calls);
-bool muteAudio(const QList<Call*> calls, bool state);
-bool muteVideo(const QList<Call*> calls, bool state);
+bool addNew           (                        );
+bool accept           ( Call* call             );
+bool hangup           ( Call* call             );
+bool refuse           ( Call* call             );
+bool hold             ( Call* call             );
+bool unhold           ( Call* call             );
+bool transfer         ( Call* call             );
+bool recordAudio      ( Call* call             );
+bool recordVideo      ( Call* call             );
+bool recordText       ( Call* call             );
+bool muteAudio        ( Call* call, bool state );
+bool muteVideo        ( Call* call, bool state );
+bool sendEmail        ( const Person* p        );
+bool callAgain        ( ContactMethod* cm      );
+bool bookmark         ( ContactMethod* cm      );
+bool deleteContact    ( Person* p              );
+bool removeFromHistory( Call* c                );
 
 bool addNew()
 {
-    Call* call = CallModel::instance().dialingCall();
-    CallModel::instance().selectionModel()->setCurrentIndex(CallModel::instance().getIndex(call), QItemSelectionModel::ClearAndSelect);
-    return true;
+   Call* call = CallModel::instance().dialingCall();
+   CallModel::instance().selectionModel()->setCurrentIndex(CallModel::instance().getIndex(call), QItemSelectionModel::ClearAndSelect);
+   return true;
 }
 
-bool accept(const QList<Call*> calls)
+bool accept(Call* call)
 {
    bool ret = true;
 
    //Add a new call if none is there
-   if (!calls.size()) {
+   if (!call) {
       return addNew();
    }
 
-   for (Call* call : calls) {
-      const Call::State state = call->state();
-      //TODO port to lifeCycle code
-      if (state == Call::State::RINGING || state == Call::State::CURRENT || state == Call::State::HOLD
-         || state == Call::State::BUSY || state == Call::State::FAILURE || state == Call::State::ERROR) {
-         qDebug() << "Calling when item currently ringing, current, hold or busy. Opening an item.";
-         Call* c2 = CallModel::instance().dialingCall();
-         CallModel::instance().selectionModel()->setCurrentIndex(CallModel::instance().getIndex(c2), QItemSelectionModel::ClearAndSelect);
+   const Call::State state = call->state();
+   //TODO port to lifeCycle code
+   if (state == Call::State::RINGING || state == Call::State::CURRENT || state == Call::State::HOLD
+      || state == Call::State::BUSY || state == Call::State::FAILURE || state == Call::State::ERROR) {
+      qDebug() << "Calling when item currently ringing, current, hold or busy. Opening an item.";
+      Call* c2 = CallModel::instance().dialingCall();
+      CallModel::instance().selectionModel()->setCurrentIndex(CallModel::instance().getIndex(c2), QItemSelectionModel::ClearAndSelect);
+   }
+   else {
+      try {
+         call->performAction(Call::Action::ACCEPT);
       }
-      else {
-         try {
-            call->performAction(Call::Action::ACCEPT);
-         }
-         catch(const char * msg) {
-//                KMessageBox::error(Ring::app(),i18n(msg));
-            ret = false;
-         }
+      catch(const char * msg) {
+         ret = false;
       }
    }
    return ret;
 } //accept
 
 ///Call
-bool hangup(const QList<Call*> calls)
+bool hangup(Call* call)
 {
    bool ret = true;
-   for (Call* call : calls) {
-      if (call) {
-         try {
-            call->performAction(Call::Action::REFUSE);
-         }
-         catch(const char * msg) {
-//             KMessageBox::error(Ring::app(),i18n(msg));
-         }
+
+   if (call) {
+      try {
+         call->performAction(Call::Action::REFUSE);
+      }
+      catch(const char * msg) {
+         return false;
       }
    }
+
    return ret;
 } //hangup
 
 ///Refuse call
-bool refuse(const QList<Call*> calls)
+bool refuse(Call* call)
 {
    bool ret = true;
-   for (Call* call : calls) {
-      if(!call) {
-         qDebug() << "Error : Hanging up when no item selected. Should not happen.";
+
+   if(!call) {
+      qDebug() << "Error : Hanging up when no item selected. Should not happen.";
+   }
+   else {
+      try {
+         call->performAction(Call::Action::REFUSE);
       }
-      else {
-         try {
-            call->performAction(Call::Action::REFUSE);
-         }
-         catch(const char * msg) {
-//             KMessageBox::error(Ring::app(),i18n(msg));
-         }
+      catch(const char * msg) {
+         return false;
       }
    }
+
    return ret;
 }
 
 ///Put call on hold
-bool hold(const QList<Call*> calls)
+bool hold(Call* call)
 {
    bool ret = true;
-   for (Call* call : calls) {
-      if(!call) {
-         qDebug() << "Error : Holding when no item selected. Should not happen.";
+
+   if(!call) {
+      qDebug() << "Error : Holding when no item selected. Should not happen.";
+   }
+   else {
+      try {
+         call->performAction(Call::Action::HOLD);
       }
-      else {
-         try {
-            call->performAction(Call::Action::HOLD);
-         }
-         catch(const char * msg) {
-//             KMessageBox::error(Ring::app(),i18n(msg));
-         }
+      catch(const char * msg) {
+         return false;
       }
    }
+
    return ret;
 }
 
 ///Remove call from hold
-bool unhold(const QList<Call*> calls)
+bool unhold(Call* call)
 {
    bool ret = true;
-   for (Call* call : calls) {
-      if(!call) {
-         qDebug() << "Error : Un-Holding when no item selected. Should not happen.";
+
+   if(!call) {
+      qDebug() << "Error : Un-Holding when no item selected. Should not happen.";
+   }
+   else {
+      try {
+         call->performAction(Call::Action::HOLD);
       }
-      else {
-         try {
-            call->performAction(Call::Action::HOLD);
-         }
-         catch(const char * msg) {
-//             KMessageBox::error(Ring::app(),i18n(msg));
-         }
+      catch(const char * msg) {
+         return false;
       }
    }
+
    return ret;
 }
 
 ///Transfer a call
-bool transfer(const QList<Call*> calls)
+bool transfer(Call* call)
 {
    bool ret = true;
-   for (Call* call : calls) {
-      if(!call) {
-         qDebug() << "Error : Transferring when no item selected. Should not happen.";
+
+   if(!call) {
+      qDebug() << "Error : Transferring when no item selected. Should not happen.";
+   }
+   else {
+      try {
+         call->performAction(Call::Action::TRANSFER);
       }
-      else {
-         try {
-            call->performAction(Call::Action::TRANSFER);
-         }
-         catch(const char * msg) {
-//             KMessageBox::error(Ring::app(),i18n(msg));
-         }
+      catch(const char * msg) {
+         return false;
       }
    }
+
    return ret;
 }
 
 ///Record a call
-bool recordAudio(const QList<Call*> calls)
+bool recordAudio(Call* call)
 {
    bool ret = true;
-   for (Call* call : calls) {
-      if(!call) {
-         qDebug() << "Error : Recording when no item selected. Should not happen.";
+
+   if(!call) {
+      qDebug() << "Error : Recording when no item selected. Should not happen.";
+   }
+   else {
+      try {
+         call->performAction(Call::Action::RECORD_AUDIO);
       }
-      else {
-         try {
-            call->performAction(Call::Action::RECORD_AUDIO);
-         }
-         catch(const char * msg) {
-//             KMessageBox::error(Ring::app(),i18n(msg));
-         }
+      catch(const char * msg) {
+         return false;
       }
    }
+
    return ret;
 }
 
 ///Record a call
-bool recordVideo(const QList<Call*> calls)
+bool recordVideo(Call* call)
 {
    bool ret = true;
-   for (Call* call : calls) {
-      if(!call) {
-         qDebug() << "Error : Recording when no item selected. Should not happen.";
+
+   if(!call) {
+      qDebug() << "Error : Recording when no item selected. Should not happen.";
+   }
+   else {
+      try {
+         call->performAction(Call::Action::RECORD_VIDEO);
       }
-      else {
-         try {
-            call->performAction(Call::Action::RECORD_VIDEO);
-         }
-         catch(const char * msg) {
-//             KMessageBox::error(Ring::app(),i18n(msg));
-         }
+      catch(const char * msg) {
+         return false;
       }
    }
+
    return ret;
 }
 
 ///Record a call
-bool recordText(const QList<Call*> calls)
+bool recordText(Call* call)
 {
    bool ret = true;
-   for (Call* call : calls) {
-      if(!call) {
-         qDebug() << "Error : Recording when no item selected. Should not happen.";
+
+   if(!call) {
+      qDebug() << "Error : Recording when no item selected. Should not happen.";
+   }
+   else {
+      try {
+         call->performAction(Call::Action::RECORD_TEXT);
       }
-      else {
-         try {
-            call->performAction(Call::Action::RECORD_TEXT);
-         }
-         catch(const char * msg) {
-//             KMessageBox::error(Ring::app(),i18n(msg));
-         }
+      catch(const char * msg) {
+         return false;
       }
    }
+
    return ret;
 }
 
 ///Mute call audio
-bool muteAudio(const QList<Call*> calls, bool state)
+bool muteAudio(Call* call, bool state)
 {
    bool ret = true;
-   for (auto call : calls) {
-      if(!call) {
-         qDebug() << "Error : Muting audio when no item selected. Should not happen.";
-         continue;
-      }
-      auto audioOut = call->firstMedia<Media::Audio>(Media::Media::Direction::OUT);
-      if (!audioOut) {
-         qDebug() << "Error : No audio media for this call";
-         continue;
-      }
-      if (state) {
-         if (!audioOut->mute())
-            qDebug() << "Error : Could not mute audio of selected call";
-      } else {
-         if (!audioOut->unmute())
-            qDebug() << "Error : Could not un-mute audio of selected call";
-      }
+
+   if(!call) {
+      qDebug() << "Error : Muting audio when no item selected. Should not happen.";
+      return false;
    }
+   auto audioOut = call->firstMedia<Media::Audio>(Media::Media::Direction::OUT);
+   if (!audioOut) {
+      qDebug() << "Error : No audio media for this call";
+      return false;
+   }
+   if (state) {
+      if (!audioOut->mute())
+         qDebug() << "Error : Could not mute audio of selected call";
+   } else {
+      if (!audioOut->unmute())
+         qDebug() << "Error : Could not un-mute audio of selected call";
+   }
+
    return ret;
 }
 
 ///Mute call video
-bool muteVideo(const QList<Call*> calls, bool state)
+bool muteVideo(Call* call, bool state)
 {
    bool ret = true;
-   for (auto call : calls) {
-      if(!call) {
-         qDebug() << "Error : Muting video when no item selected. Should not happen.";
-         continue;
-      }
-      auto videoOut = call->firstMedia<Media::Video>(Media::Media::Direction::OUT);
-      if (!videoOut) {
-         qDebug() << "Error : No video media for this call";
-         continue;
-      }
-      if (state) {
-         if (!videoOut->mute())
-            qDebug() << "Error : Could not mute video of selected call";
-      } else {
-         if (!videoOut->unmute())
-            qDebug() << "Error : Could not un-mute video of selected call";
-      }
+
+   if(!call) {
+      qDebug() << "Error : Muting video when no item selected. Should not happen.";
+      return false;
    }
+   auto videoOut = call->firstMedia<Media::Video>(Media::Media::Direction::OUT);
+   if (!videoOut) {
+      qDebug() << "Error : No video media for this call";
+      return false;
+   }
+   if (state) {
+      if (!videoOut->mute())
+         qDebug() << "Error : Could not mute video of selected call";
+   } else {
+      if (!videoOut->unmute())
+         qDebug() << "Error : Could not un-mute video of selected call";
+   }
+
    return ret;
+}
+
+bool sendEmail(const Person* p)
+{
+   if (!p || p->preferredEmail().isEmpty())
+      return false;
+
+   qDebug() << "Sending email";
+   QProcess *myProcess = new QProcess(QCoreApplication::instance());
+   QStringList arguments;
+#ifdef Q_OS_WIN32
+   myProcess->start("start", { "mailto:"+p->preferredEmail() });
+#elif defined Q_OS_DARWIN
+   myProcess->start("open", { "mailto:"+p->preferredEmail() });
+#else
+   myProcess->start("xdg-email", { "mailto:"+p->preferredEmail() });
+#endif
+   return true;
+}
+
+bool callAgain(ContactMethod* cm)
+{
+   if (!cm)
+      return false;
+
+   Call* call = CallModel::instance().dialingCall(cm);
+
+   if (call) {
+      call->performAction(Call::Action::ACCEPT);
+      return true;
+   }
+
+   return false;
+}
+
+bool bookmark(ContactMethod* cm)
+{
+   if (!cm)
+      return false;
+
+   CategorizedBookmarkModel::instance().addBookmark(cm);
+
+   return true;
+}
+
+bool deleteContact(Person* p)
+{
+   if (!p)
+      return false;
+
+   return p->remove();
+}
+
+bool removeFromHistory(Call* c)
+{
+   if (c && c->collection()->supportedFeatures() & CollectionInterface::SupportedFeatures::REMOVE) {
+
+      CategorizedHistoryModel::instance().deleteItem(c); //TODO add add and remove to the manager
+
+      return true;
+   }
+
+   return false;
 }
 
 } //namespace UserActions
