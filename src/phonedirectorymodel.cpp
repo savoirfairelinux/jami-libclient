7937f0e0465cc4e6dd350ba3899baa234dae9657
Emmanuel Lepage Vallee <elv1313@gmail.com>
2015-12-05 06:27:56 -0500

    model: Handle Qt::DecorationRole in roledata()
    When clients require simple widgets that filter "raw"
    model data, such as selecting a contact when receiving
    a call from an unknown contact method, it is overkill
    for them to have to implement a view delegate to display
    the picture.
    
    This commit add DecorationRole to all models based on
    Ring::Role. The pixmap manipulation interface need to
    implement the new functions to make use of this.
    
    The interface is responsible to support resolution
    independance.
    
    Tuleap #170
    
    Change-Id: Ibad44805485b71195805eaca82f08a4a56864f50

--- src/phonedirectorymodel.cpp
+++ src/phonedirectorymodel.cpp
@@ -100,15 +100,7 @@ QVariant PhoneDirectoryModel::data(const QModelIndex& index, int role ) const
    const ContactMethod* number = d_ptr->m_lNumbers[index.row()];
    switch (static_cast<PhoneDirectoryModelPrivate::Columns>(index.column())) {
       case PhoneDirectoryModelPrivate::Columns::URI:
-         switch (role) {
-            case Qt::DisplayRole:
-               return number->uri();
-            case Qt::DecorationRole :
-               return GlobalInstances::pixmapManipulator().callPhoto(number,QSize(16,16));
-            case (int) Role::Object:
-               return QVariant::fromValue(const_cast<ContactMethod*>(number));
-         }
-         break;
+         return number->roleData(role);
       case PhoneDirectoryModelPrivate::Columns::TYPE:
          switch (role) {
             case Qt::DisplayRole:
