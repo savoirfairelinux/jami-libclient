deb4483248415d9757209386844bc96840fa2702
Emmanuel Lepage Vallee <elv1313@gmail.com>
2015-12-05 06:27:56 -0500

    model: Handle Qt::DecorationRole in roledata()
    When clients require simple widgets that filter "raw"
    model data, such as selecting a contact when receiving
    a call from an unknown contact method, it is overkill
    for them to have to implement a view delegate to display
    the picture.
    
    This commit add DecorationRole to all models based on
    Ring::Role. The pixmap manipulation interface need to
    implement the new functions to make use of this.
    
    The interface is responsible to support resolution
    independance.
    
    Tuleap: #170
    
    Change-Id: Ibad44805485b71195805eaca82f08a4a56864f50

--- src/interfaces/pixmapmanipulatori.h
+++ src/interfaces/pixmapmanipulatori.h
@@ -75,6 +75,10 @@ public:
     virtual QVariant   securityLevelIcon(const SecurityEvaluationModel::SecurityLevel level) const = 0;
     virtual QVariant   historySortingCategoryIcon(const CategorizedHistoryModel::SortedProxy::Categories cat) const = 0;
     virtual QVariant   contactSortingCategoryIcon(const CategorizedContactModel::SortedProxy::Categories cat) const = 0;
+    virtual QVariant   decorationRole(const QModelIndex&   index) = 0;
+    virtual QVariant   decorationRole(const Call*          c    ) = 0;
+    virtual QVariant   decorationRole(const ContactMethod* cm   ) = 0;
+    virtual QVariant   decorationRole(const Person*        p    ) = 0;
 
     /**
      * Return the icons associated with the action and its state
