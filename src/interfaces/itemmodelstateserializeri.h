4daf15aa19c4df274183b803cf7f52454470e64c
Emmanuel Lepage Vallee <elv1313@gmail.com>
2015-12-16 23:34:45 -0500

    uma: Add way to select a default collections for actions
    This commit add a new method to the ItemModelSerializationInterface
    and user it in useractions.h
    
    Change-Id: I50bcbdfdf705e850a3d6cd3c0b7df799ab51a8d9
    Tuleap: #165

--- src/interfaces/itemmodelstateserializeri.h
+++ src/interfaces/itemmodelstateserializeri.h
@@ -20,8 +20,9 @@
 #include <typedefs.h>
 
 // Ring
-class CollectionInterface;
+#include <collectioninterface.h>
 class Account;
+class CollectionManagerInterfaceBase;
 
 namespace Interfaces {
 
@@ -31,13 +32,48 @@ namespace Interfaces {
  */
 class ItemModelStateSerializerI {
 public:
+   /**
+    * Hints for the preferredCollection implementation about how to select
+    * the right collection.
+    */
+   enum class Hints {
+      NONE            = 0x0 << 0,
+      ASK_USER        = 0x1 << 0,
+      DO_NOT_ASK_USER = 0x1 << 1,
+   };
+
     virtual ~ItemModelStateSerializerI() = default;
 
+    /**
+     * Called when the new enabled collection list need to be serialized
+     */
     virtual bool save() = 0;
+
+    /**
+     * Called when the enabled collection list is first needed
+     *
+     * @note You can call this manually if your collections depend on it
+     */
     virtual bool load() = 0;
 
     //Getter
     virtual bool isChecked(const CollectionInterface* backend) const = 0;
+
+    /**
+     * Allow to overload the default algoritm used to look for a default
+     * collection to store something new.
+     *
+     * @param manager The likes of PersonModel::instance() or CategorizedHistoryModel::instance()
+     * @param features A list of must have collection features. If NONE is set \
+     * the implementation is free to return whatever it want. If it is set, a \
+     * collection must return a compliant collection or the result will be \
+     * ignored.
+     */
+    virtual CollectionInterface* preferredCollection(CollectionManagerInterfaceBase* manager,
+      FlagPack<CollectionInterface::SupportedFeatures> features
+         = FlagPack<CollectionInterface::SupportedFeatures>(CollectionInterface::SupportedFeatures::NONE),
+      FlagPack<Hints> hints = FlagPack<Hints>(Hints::NONE)
+    ) = 0;
 
     //Setter
     virtual bool setChecked(const CollectionInterface* backend, bool enabled) = 0;
