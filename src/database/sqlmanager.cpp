525da867f7ab6ce6296a76d0f4b8081dde71ff7b
Edric Milaret <edric.ladent-milaret@savoirfairelinux.com>
2016-03-04 10:57:07 -0500

    implement singleton for sql connection manager
    Change-Id: I09acef8f531c0cf1156930f9adda6e316bffc4a4
    Tuleap: #TBD

---
+++ src/database/sqlmanager.cpp
@@ -0,0 +1,65 @@
+/************************************************************************************
+ *   Copyright (C) 2016 by Savoir-faire Linux                                       *
+ *   Author : Edric Milaret <edric.ladent-milaret@savoirfairelinux.com              *
+ *                                                                                  *
+ *   This library is free software; you can redistribute it and/or                  *
+ *   modify it under the terms of the GNU Lesser General Public                     *
+ *   License as published by the Free Software Foundation; either                   *
+ *   version 2.1 of the License, or (at your option) any later version.             *
+ *                                                                                  *
+ *   This library is distributed in the hope that it will be useful,                *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of                 *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU              *
+ *   Lesser General Public License for more details.                                *
+ *                                                                                  *
+ *   You should have received a copy of the GNU Lesser General Public               *
+ *   License along with this library; if not, write to the Free Software            *
+ *   Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA *
+ ***********************************************************************************/
+
+#include "sqlmanager.h"
+
+SqlManager::SqlManager() {
+    db = QSqlDatabase::addDatabase("QSQLITE");
+    db.setDatabaseName(QStandardPaths::writableLocation(QStandardPaths::DataLocation)
+                       + QDir::separator() +"lrc.db3");
+    db.open();
+    if (not verifySchemaVersion()) {
+        //TODO: Handle schema migration somehow
+        /*
+         * We could use QSqlRecord and some sort of file to describe database
+         * then handle migration that way
+         */
+    }
+}
+
+SqlManager::~SqlManager() {
+    if (db.isOpen())
+        db.close();
+}
+
+///Singleton
+SqlManager& SqlManager::instance()
+{
+    static auto instance = new SqlManager();
+
+    return *instance;
+}
+
+bool SqlManager::isOpen() const {
+    return db.isOpen();
+}
+
+bool SqlManager::verifySchemaVersion() const {
+    if (db.record("_info").isEmpty()) {
+        QSqlQuery createInfoTable("CREATE TABLE _info (version INTEGER)");
+        createInfoTable.exec();
+        QSqlQuery insertDbInfo(QString("INSERT INTO _info (version) VALUES (%1)").arg(version_));
+        insertDbInfo.exec();
+    } else {
+        QSqlQuery schemaVersion("SELECT * FROM _info");
+        schemaVersion.exec();
+        return (schemaVersion.first() && schemaVersion.value(0).toInt() == version_);
+    }
+    return true;
+}
