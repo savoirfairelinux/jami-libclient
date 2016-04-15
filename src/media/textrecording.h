4911eaf9bd8fc08c53ac910a6fa07c46ac4468d8
Edric Milaret <edric.ladent-milaret@savoirfairelinux.com>
2016-04-15 17:20:14 -0400

    im: connect text status api
    * Need new API in the daemon *
    
    Change-Id: I61a31bc886125764f49af3d1528e7026580398d8
    Tuleap: #647

--- src/media/textrecording.h
+++ src/media/textrecording.h
@@ -61,6 +61,8 @@ public:
       HTML                 ,
       HasText              ,
       ContactMethod        ,
+      IsSending            ,
+      HasSucceeded         ,
    };
 
    //Constructor
