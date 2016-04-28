a8d0b5407f62a24c8867e6f281ecd7fdd2327c6c
Edric Milaret <edric.ladent-milaret@savoirfairelinux.com>
2016-04-28 17:13:43 -0400

    recentmodel: fix selection on contact method upgraded
    Change-Id: I65e9a2eee4ab85cfe91d31770707fd924b69a418
    Tuleap: #529

--- src/recentmodel.cpp
+++ src/recentmodel.cpp
@@ -743,12 +743,14 @@ void RecentModelPrivate::slotContactChanged(ContactMethod* cm, Person* np, Perso
             } else
                 cmNode->m_pParent = nullptr;
         }
+
+        n->m_lChildren.clear();
+        removeNode(n);
+
         if (newParentNode && newParentNode->m_lChildren.size()) {
             q_ptr->selectionModel()->clear();
             q_ptr->selectionModel()->select(q_ptr->getIndex(const_cast<Person*>(newParentNode->m_uContent.m_pPerson)), QItemSelectionModel::ClearAndSelect);
         }
-        n->m_lChildren.clear();
-        removeNode(n);
     }
 }
 
