66d99e3c3ff376e27632fdcce530886bc9635004
Adrien Béraud <adrien.beraud@savoirfairelinux.com>
2016-05-11 17:33:47 -0400

    blockchain: add basic name lookup service
    This first version makes local requests for
    ring contacts, and if found, will show the username
    instead of the Ring ID in the UI.
    
    Tuleap: #684
    Change-Id: Ife6ef660b0bc3817601f597ed7d1eb9f582b822a

--- src/contactmethod.cpp
+++ src/contactmethod.cpp
@@ -46,6 +46,8 @@
 #include "globalinstances.h"
 #include "interfaces/pixmapmanipulatori.h"
 
+#include "namedirectory.h"
+
 //Private
 #include "private/phonedirectorymodel_p.h"
 #include "private/textrecording_p.h"
@@ -107,7 +109,8 @@ ContactMethodPrivate::ContactMethodPrivate(const URI& uri, NumberCategory* cat, 
    m_Uri(uri),m_pCategory(cat),m_Tracked(false),m_Present(false),m_LastUsed(0),
    m_Type(st),m_PopularityIndex(-1),m_pPerson(nullptr),m_pAccount(nullptr),
    m_LastWeekCount(0),m_LastTrimCount(0),m_HaveCalled(false),m_IsBookmark(false),m_TotalSeconds(0),
-   m_Index(-1),m_hasType(false),m_pTextRecording(nullptr), m_pCertificate(nullptr), q_ptr(q)
+   m_Index(-1),m_hasType(false),m_pTextRecording(nullptr), m_pCertificate(nullptr), q_ptr(q),
+   m_ns(NameDirectory::instance())
 {}
 
 ///Constructor
@@ -120,6 +123,14 @@ d_ptr(new ContactMethodPrivate(number,cat,st,this))
       NumberCategoryModel::instance().d_ptr->registerNumber(this);
    }
    d_ptr->m_lParents << this;
+   if (d_ptr->m_Uri.protocolHint() == URI::ProtocolHint::RING) {
+      auto ring_id = d_ptr->m_Uri.userinfo();
+      d_ptr->m_ns.addrLookup(ring_id, [this](const QString& name){
+         d_ptr->m_lookupName = name;
+         d_ptr->m_PrimaryName_cache.clear();
+         emit changed();
+      });
+   }
 }
 
 ContactMethod::~ContactMethod()
@@ -374,7 +385,7 @@ QString ContactMethod::primaryName() const
       if (d_ptr->m_hNames.size() == 1)
          ret =  d_ptr->m_hNames.constBegin().key();
       else {
-         QString toReturn = uri();
+         QString toReturn = d_ptr->m_lookupName.isEmpty() ? uri() : d_ptr->m_lookupName;
          QPair<int, time_t> max = {0, 0};
 
          for (QHash<QString,QPair<int, time_t>>::const_iterator i = d_ptr->m_hNames.begin(); i != d_ptr->m_hNames.end(); ++i) {
@@ -395,7 +406,7 @@ QString ContactMethod::primaryName() const
    }
    //Fallback: Use the URI
    if (d_ptr->m_PrimaryName_cache.isEmpty()) {
-      return uri();
+      return d_ptr->m_lookupName.isEmpty() ? uri() : d_ptr->m_lookupName;
    }
 
    //Return the cached primaryname
