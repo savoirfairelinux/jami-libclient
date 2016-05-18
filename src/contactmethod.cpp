cb5ee04e11cfb0b7cc3e0969f574c0e4c0d231a1
Stepan Salenikovich <stepan.salenikovich@savoirfairelinux.com>
2016-05-18 14:36:31 -0400

    contactmethod: fix wrong certificate id
    Seems that there was a typo, as the uid() method is used to create
    the corresponding certificate, instead of uri().
    
    uid() is used to create a hash of the ContactMethod object, but the
    certificate id requires the RingID, which is retreived via the uri()
    method.
    
    Change-Id: Ia7dbc2963acdd1f832aa9c9ac21a399fe746e8b1
    Tuleap: #712

--- src/contactmethod.cpp
+++ src/contactmethod.cpp
@@ -800,7 +800,7 @@ bool ContactMethod::isReachable() const
 Certificate* ContactMethod::certificate() const
 {
    if (protocolHint() == URI::ProtocolHint::RING) {
-      d_ptr->m_pCertificate = CertificateModel::instance().getCertificateFromId(uid(), account());
+      d_ptr->m_pCertificate = CertificateModel::instance().getCertificateFromId(uri().format(URI::Section::USER_INFO), account());
 
       if (d_ptr->m_pCertificate && !d_ptr->m_pCertificate->contactMethod())
          d_ptr->m_pCertificate->setContactMethod(const_cast<ContactMethod*>(this));
