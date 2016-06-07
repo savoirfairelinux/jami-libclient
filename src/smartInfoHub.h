ecfe486a90be75a4015783b492fab0c044e58088
Gasuleg <o.gregoire07@gmail.com>
2016-06-07 10:32:00 -0400

    API SmartInfoHub
    SmartInfo is an tools who will display a lot of information information in the client
    (CPU, GPU, Memory usage, Codecs used, pourcentage of packet lost, bandwidth...).
    These information will be really important for the consummer to understand what's happened in Ring when he don't work properly.
    Daemon: https://gerrit-ring.savoirfairelinux.com/#/c/4315/
    Client: https://gerrit-ring.savoirfairelinux.com/#/c/4384/
    
    -start and stop method
    -emit signal when something change
    -getter for all components of the map send by smartInfo signal
    
    What I do in this commit:
    	-Add callID in the public API
    
    Change-Id: I5eb464cdfacfbf2eda318c6945c128f8b64860fc

---
+++ src/smartInfoHub.h
@@ -0,0 +1,87 @@
+/****************************************************************************
+ *   Copyright (C) 2016 by Savoir-faire Linux                               *
+ *   Author: Olivier Grégoire <olivier.gregoire@savoirfairelinux.com>       *
+ *                                                                          *
+ *   This library is free software; you can redistribute it and/or          *
+ *   modify it under the terms of the GNU Lesser General Public             *
+ *   License as published by the Free Software Foundation; either           *
+ *   version 2.1 of the License, or (at your option) any later version.     *
+ *                                                                          *
+ *   This library is distributed in the hope that it will be useful,        *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of         *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU      *
+ *   Lesser General Public License for more details.                        *
+ *                                                                          *
+ *   You should have received a copy of the GNU General Public License      *
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.  *
+ ***************************************************************************/
+
+#pragma once
+
+#include <iostream>
+#include <map>
+#include <vector>
+
+#include <QtCore/QDebug>
+#include <QtCore/QCoreApplication>
+#include <QtCore/QMimeData>
+#include <QtCore/QItemSelectionModel>
+#include <QObject>
+#include <QtCore/QUrl>
+#include <QList>
+
+#include "callmodel.h"
+#include "typedefs.h"
+
+//variables contain on the map
+#define LOCAL_FPS          "local FPS"
+#define LOCAL_VIDEO_CODEC  "local video codec"
+#define LOCAL_AUDIO_CODEC  "local audio codec"
+#define REMOTE_FPS         "remote FPS"
+#define REMOTE_WIDTH       "remote width"
+#define REMOTE_HEIGHT      "remote height"
+#define REMOTE_VIDEO_CODEC "remote video codec"
+#define REMOTE_AUDIO_CODEC "remote audio codec"
+#define CALL_ID            "callID"
+
+
+class SmartInfoHubPrivate;
+
+class SmartInfoHub final : public QObject
+{
+    Q_OBJECT
+    public:
+        // Signleton
+        static SmartInfoHub& instance();
+
+        void start();
+        void stop();
+
+        void setRefreshTime(int timeMS);
+
+        /***********************************************************************
+         *                                                                     *
+         *                            Getter                                   *
+         *                                                                     *
+         ***********************************************************************/
+         float          localFps() const;
+         float          remoteFps() const;
+         int            remoteWidth() const;
+         int            remoteHeight() const;
+         QString        callID() const;
+         QString        localVideoCodec() const;
+         QString        localAudioCodec() const;
+         QString        remoteVideoCodec() const;
+         QString        remoteAudioCodec() const;
+         QList<QString> keyChanged();
+
+    signals:
+        /* Signal emit every tine something change. He contain a list of update
+         * You can connect it to a lambda function
+         */
+        void changed();
+
+    private:
+        SmartInfoHub();
+        SmartInfoHubPrivate* d_ptr;
+};
