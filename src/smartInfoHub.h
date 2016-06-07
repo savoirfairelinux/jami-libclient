b9b3b2b564bc4d7fe6d5d7bbef802130c8845c84
Gasuleg <o.gregoire07@gmail.com>
2016-06-07 10:32:00 -0400

    API SmartInfoHub
    SmartInfo is an tools who will display a lot of information information in the client
    (CPU, GPU, Memory usage, Codecs used, pourcentage of packet lost, bandwidth...).
    These information will be really important for the consummer to understand what's happened in Ring when he don't work properly.
    Daemon: https://gerrit-ring.savoirfairelinux.com/#/c/4315/
    LRC:    https://gerrit-ring.savoirfairelinux.com/#/c/4388/
    Client: https://gerrit-ring.savoirfairelinux.com/#/c/4384/
    
    -start and stop method
    -emit signal when something change
    -getter for all components of the map send by smartInfo signal
    
    Change-Id: I5eb464cdfacfbf2eda318c6945c128f8b64860fc

---
+++ src/smartInfoHub.h
@@ -0,0 +1,93 @@
+/****************************************************************************
+ *   Copyright (C) 2016 by Savoir-faire Linux                               *
+ *   Author: Olivier Grégoire <olivier.gregoire@savoirfairelinux.com>       *
+ *                                                                          *
+ *   This library is free software; you can redistribute it and/or          *
+ *   modify it under the terms of the GNU Lesser General Public             *
+ *   License as published by the Free Software Foundation; either           *
+ *   version 2.1 of the License, or (at your option) any later version.     *
+ *                                                                          *
+ *   This library is distributed in the hope that it will be useful,        *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of         *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU      *
+ *   Lesser General Public License for more details.                        *
+ *                                                                          *
+ *   You should have received a copy of the GNU General Public License      *
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.  *
+ ***************************************************************************/
+
+/*
+ * SmartInfo is an API who will retrieve a lot of information from the daemon.
+ *    -Local and remote video codec
+ *    -Local and remote frame rate
+ *    -Remote audio codec
+ *    -Remote resolution
+ * The goal is to display all that to the clients.
+ * That will help the user to debug his system and/or just show him what
+ * happened in the system.
+ */
+
+#pragma once
+
+#include <iostream>
+#include <map>
+#include <vector>
+
+#include <QtCore/QDebug>
+#include <QtCore/QCoreApplication>
+#include <QtCore/QMimeData>
+#include <QtCore/QItemSelectionModel>
+#include <QObject>
+#include <QtCore/QUrl>
+#include <QList>
+
+#include "callmodel.h"
+#include "typedefs.h"
+
+//variables contain on the map
+#define LOCAL_FPS          "local FPS"
+#define REMOTE_FPS         "remote FPS"
+#define REMOTE_WIDTH       "remote width"
+#define REMOTE_HEIGHT      "remote height"
+#define LOCAL_VIDEO_CODEC  "local video codec"
+#define REMOTE_VIDEO_CODEC "remote video codec"
+#define REMOTE_AUDIO_CODEC "remote audio codec"
+
+class SmartInfoHubPrivate;
+
+class SmartInfoHub final : public QObject
+{
+    Q_OBJECT
+    public:
+        // Signleton
+        static SmartInfoHub& instance();
+
+        void start();
+        void stop();
+
+        void setRefreshTime(int timeMS);
+
+        /***********************************************************************
+         *                                                                     *
+         *                            Getter                                   *
+         *                                                                     *
+         ***********************************************************************/
+         float          localFps();
+         float          remoteFps();
+         int            remoteWidth();
+         int            remoteHeight();
+         QString        localVideoCodec();
+         QString        remoteVideoCodec();
+         QString        remoteAudioCodec();
+         QList<QString> keyChanged();
+
+    signals:
+        /* Signal emit every tine something change. He contain a list of update
+         * You can connect it to a lambda function
+         */
+        void changed();
+
+    private:
+
+        SmartInfoHubPrivate* d_ptr;
+};
