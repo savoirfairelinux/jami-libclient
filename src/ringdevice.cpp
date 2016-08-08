db34c244baa29733373d532319274bdcb4d4da89
aviau <alexandre@alexandreviau.net>
2016-08-08 17:15:38 -0400

    Multi-device support
    Change-Id: I6cbbdd09bc368b76fe82eb4800fd87b6fdcc0aa7
    Tuleap: #896

---
+++ src/ringdevice.cpp
@@ -0,0 +1,76 @@
+/****************************************************************************
+ *   Copyright (C) 2016 by Savoir-faire Linux                               *
+ *   Author : Alexandre Viau <alexandre.viau@savoirfairelinux.com>          *
+ *                                                                          *
+ *   This library is free software you can redistribute it and/or           *
+ *   modify it under the terms of the GNU Lesser General Public             *
+ *   License as published by the Free Software Foundation either            *
+ *   version 2.1 of the License, or (at your option) any later version.     *
+ *                                                                          *
+ *   This library is distributed in the hope that it will be useful,        *
+ *   but WITHOUT ANY WARRANTY without even the implied warranty of          *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU      *
+ *   Lesser General Public License for more details.                        *
+ *                                                                          *
+ *   You should have received a copy of the GNU General Public License      *
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.  *
+ ***************************************************************************/
+
+ //Qt
+ #include <QtCore/QObject>
+
+#include "ringdevice.h"
+
+
+class RingDevicePrivate final : public QObject
+{
+public:
+   Q_OBJECT
+   Q_DECLARE_PUBLIC(RingDevice)
+
+   RingDevice*   q_ptr   ;
+   QString       m_pID   ;
+   QString       m_pName ;
+
+   //Constructor
+   explicit RingDevicePrivate(RingDevice* device);
+};
+
+ RingDevicePrivate::RingDevicePrivate(RingDevice* device) : QObject(device), q_ptr(device)
+ {
+ }
+
+ ///Constructors
+RingDevice::RingDevice(const QString& id, const QString& name)
+ {
+     d_ptr = new RingDevicePrivate(this);
+     d_ptr->m_pID = id;
+     d_ptr->m_pName = name;
+ }
+
+const QString RingDevice::id() const
+{
+    return d_ptr->m_pID;
+}
+
+const QString RingDevice::name() const
+{
+    return d_ptr->m_pName;
+}
+
+#define CAST(item) static_cast<int>(item)
+QVariant RingDevice::roleData(int role) const
+{
+   switch(role) {
+      case CAST(RingDevice::Role::Id):
+         return id();
+      case CAST(RingDevice::Role::Name):
+         return name();
+      default:
+         return QVariant();
+   }
+}
+#undef CAST
+
+
+#include <ringdevice.moc>
