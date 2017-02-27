4a8c81063ed82ea891693143eb54f8ac61b858a5
Nicolas Jager <nicolas.jager@savoirfairelinux.com>
2017-02-27 07:45:18 -0500

    Improve Person uid generation.
    - Removes uid paramater from the constructor of Person.
    
    - Adds Person::ensureUid() method to ensure a valid unique id.
    
    - It sets a random uid after checked if the uid is empty or if
    the one set is used by another Person object.
    
    - If a Person object with an empty uid is used to save a vcard,
    the vcard has no prefix name. This patch solves this bug by using
    ensureUid() in PeerProfileEditor.
    
    - LocalProfileCollection an ProfileModelPrivate used to use a time
    function to generate uids, wich may cause a collision. The replacement
    by the ensureUid() function avoid this bug.
    
    Change-Id: I88bd5744975945afe2d52c46409cf5cf7cae28ec
    Tuleap: #1519

--- src/person.cpp
+++ src/person.cpp
@@ -41,6 +41,9 @@
 #include "media/textrecording.h"
 #include "mime.h"
 
+// Std
+#include <random>
+
 class AddressPrivate final
 {
 public:
@@ -203,12 +206,9 @@ PersonPrivate::~PersonPrivate()
 }
 
 ///Constructor
-Person::Person(CollectionInterface* parent, const QByteArray& uid): ItemBase(nullptr),
+Person::Person(CollectionInterface* parent): ItemBase(nullptr),
    d_ptr(new PersonPrivate(this))
 {
-   if (!uid.isEmpty())
-      d_ptr->m_Uid = uid;
-
    setCollection(parent ? parent : &TransitionalPersonBackend::instance());
 
    d_ptr->m_isPlaceHolder = false;
@@ -755,5 +755,28 @@ void PersonPrivate::slotCallAdded(Call *call)
 {
     foreach (Person* c,m_lParents) {
         emit c->callAdded(call);
+    }
+}
+
+/**
+ * ensureUid ensures an unique Id. By checking the current uid first and generate a random one if not unique or empty.
+ */
+void
+Person::ensureUid()
+{
+    static std::random_device rdev;
+    static std::seed_seq seq {rdev(), rdev()};
+    static std::mt19937_64 rand {seq};
+    static std::uniform_int_distribution<uint64_t> id_generator;
+
+    if (d_ptr->m_Uid.isEmpty()) {
+        do {
+            d_ptr->m_Uid = std::to_string(id_generator(rand)).c_str();
+        } while (PersonModel::instance().getPersonByUid(d_ptr->m_Uid) != this);
+        return;
+    }
+    
+    while (PersonModel::instance().getPersonByUid(d_ptr->m_Uid) != this) {
+        d_ptr->m_Uid = std::to_string(id_generator(rand)).c_str();
     }
 }
