49ad7b034fdf6390b7dab73281b24f4f67aa2769
Emmanuel Lepage Vallee <elv1313@gmail.com>
2017-03-21 17:48:50 -0400

    account: Add the registered name role
    Change-Id: I9888eb7c17b7d1fcb94de4fb54f00850bf97305b

--- src/account.cpp
+++ src/account.cpp
@@ -1285,6 +1285,8 @@ QVariant Account::roleData(int role) const
          break;
       case CAST(Account::Role::LastStatusChangeTimeStamp):
          return QVariant::fromValue(statusModel()->lastTimeStamp());
+      case CAST(Account::Role::RegisteredName):
+         return registeredName();
       default:
          return QVariant();
    }
@@ -2282,6 +2284,7 @@ Account::RoleState Account::roleState(Account::Role role) const
             case Account::Role::AllowIncomingFromHistory:
             case Account::Role::AllowIncomingFromContact:
             case Account::Role::AllowIncomingFromUnknown:
+            case Account::Role::RegisteredName          :
             case Account::Role::HasCustomBootstrap      :
                return Account::RoleState::UNAVAILABLE;
          }
