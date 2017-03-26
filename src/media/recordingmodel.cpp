bbf345c42bfe599d70a7f2714ebf9a424e83525c
Emmanuel Lepage Vallee <elv1313@gmail.com>
2017-03-26 18:07:06 -0400

    recordingmodel: Complete the code
    It was half written before I left. This was known at the time but
    left as-is because the parts that worked were needed ASAP.
    
    This model is used to be able to list and delete individual chat
    conversations. It doesn't have many roles for now. This commit only
    finish what was already started.
    
    This commit also port it to the newer code convention put forward
    by the current maintainers:
    
     * Use std::vector for private structures (modern c++ first)
     * Use 4 spaces
     * Favor `auto` and `{}` instead of the explicit types.
     * Use shorter line length by using an exit early strategy
    
    This commit also drop the "FILE" media code as it doesn't look
    like the daemon will support it anytime soon and commented code
    is evil.
    
    Change-Id: I32a7cc37d10c5a8bf430a03189e07ce6072c8a8e

--- src/media/recordingmodel.cpp
+++ src/media/recordingmodel.cpp
@@ -1,5 +1,6 @@
 /****************************************************************************
- *   Copyright (C) 2015-2016 by Savoir-faire Linux                               *
+ *   Copyright (C) 2015-2017 by Savoir-faire Linux                          *
+ *   Copyright (C) 2017 by Emmanuel Lepage Vallee                           *
  *   Author : Emmanuel Lepage Vallee <emmanuel.lepage@savoirfairelinux.com> *
  *                                                                          *
  *   This library is free software; you can redistribute it and/or          *
@@ -17,6 +18,9 @@
  ***************************************************************************/
 #include "recordingmodel.h"
 
+// STD
+#include <vector>
+
 //Qt
 #include <QtCore/QMimeData>
 #include <QtCore/QCoreApplication>
@@ -25,7 +29,9 @@
 //Ring
 #include "recording.h"
 #include "textrecording.h"
+#include "avrecording.h"
 #include "media.h"
+#include "call.h"
 #include "localtextrecordingcollection.h"
 
 //DRing
@@ -33,87 +39,93 @@
 
 struct RecordingNode final
 {
-   ~RecordingNode();
+    // Types
+    enum class Type {
+        TOP_LEVEL,
+        SESSION  ,
+        TAG      ,
+    };
 
-   enum class Type {
-      TOP_LEVEL,
-      SESSION  ,
-      TAG      ,
-   };
+    // Constructors
+    RecordingNode(RecordingNode::Type type);
+    ~RecordingNode();
 
-   RecordingNode(RecordingNode::Type type);
-
-   //Attributes
-   RecordingNode::Type     m_Type     ;
-   int                     m_Index    ;
-   QString                 m_CatName  ;
-   Media::Recording*       m_pRec     ;
-   QVector<RecordingNode*> m_lChildren;
-   RecordingNode*          m_pParent  ;
-
+    //Attributes
+    RecordingNode::Type         m_Type      {       };
+    int                         m_Index     { -1    };
+    QString                     m_CatName   {       };
+    Media::Recording*           m_pRec      {nullptr};
+    std::vector<RecordingNode*> m_lChildren {       };
+    RecordingNode*              m_pParent   {nullptr};
 };
+
+namespace Media {
 
 class RecordingModelPrivate final : public QObject
 {
    Q_OBJECT
 public:
-   explicit RecordingModelPrivate(Media::RecordingModel* parent);
-   ~RecordingModelPrivate();
+    explicit RecordingModelPrivate(RecordingModel* parent);
+    ~RecordingModelPrivate();
 
-   //Attributes
-   QVector<RecordingNode*>        m_lCategories             ;
-   RecordingNode*                 m_pText          {nullptr};
-   RecordingNode*                 m_pAudioVideo    {nullptr};
-   LocalTextRecordingCollection*  m_pTextRecordingCollection;
-   int                            m_UnreadCount             ;
+    // Attributes
+    std::vector<RecordingNode*>    m_lCategories             ;
+    RecordingNode*                 m_pText          {nullptr};
+    RecordingNode*                 m_pAudioVideo    {nullptr};
+    LocalTextRecordingCollection*  m_pTextRecordingCollection;
+    int                            m_UnreadCount             ;
 
-   //RecordingNode*                 m_pFiles     ; //TODO uncomment when implemented in DRing
+    // Helpers
+    void initCategories();
+    void forwardInsertion(TextRecording* r, ContactMethod* cm, Media::Direction direction);
+    void updateUnreadCount(const int count);
+    void emitChangedProxy();
 
-   void forwardInsertion(const QMap<QString,QString>& message, ContactMethod* cm, Media::Media::Direction direction);
-   void updateUnreadCount(const int count);
+    Q_DISABLE_COPY(RecordingModelPrivate)
+    Q_DECLARE_PUBLIC(RecordingModel)
 
 private:
-   Media::RecordingModel* q_ptr;
+    RecordingModel* q_ptr;
 };
 
-RecordingNode::RecordingNode(RecordingNode::Type type) :
-   m_Type(type),m_pParent(nullptr), m_pRec(nullptr), m_Index(-1)
-{
+} // namespace Media::
 
+RecordingNode::RecordingNode(RecordingNode::Type type) : m_Type(type)
+{
 }
 
 RecordingNode::~RecordingNode()
 {
-   foreach(RecordingNode* c, m_lChildren)
-      delete c;
+    foreach(RecordingNode* c, m_lChildren)
+        delete c;
 }
 
-RecordingModelPrivate::RecordingModelPrivate(Media::RecordingModel* parent) : q_ptr(parent),m_pText(nullptr),
-m_pAudioVideo(nullptr)/*,m_pFiles(nullptr)*/
+Media::RecordingModelPrivate::RecordingModelPrivate(RecordingModel* parent) : q_ptr(parent),m_pText(nullptr),
+m_pAudioVideo(nullptr)
 {
 
 }
 
-RecordingModelPrivate::~RecordingModelPrivate()
+Media::RecordingModelPrivate::~RecordingModelPrivate()
 {
-   if (m_pTextRecordingCollection)
-      delete m_pTextRecordingCollection;
+    if (m_pTextRecordingCollection)
+        delete m_pTextRecordingCollection;
 
-   if (m_pText)
-      delete m_pText;
+    if (m_pText)
+        delete m_pText;
 
-   if (m_pAudioVideo)
-      delete m_pAudioVideo;
+    if (m_pAudioVideo)
+        delete m_pAudioVideo;
 }
 
-void RecordingModelPrivate::forwardInsertion(const QMap<QString,QString>& message, ContactMethod* cm, Media::Media::Direction direction)
+void Media::RecordingModelPrivate::forwardInsertion(TextRecording* r, ContactMethod* cm, Media::Media::Direction direction)
 {
-   Q_UNUSED(message)
-   Q_UNUSED(direction)
-   emit q_ptr->newTextMessage(static_cast<Media::TextRecording*>(sender()), cm);
+    Q_UNUSED(direction)
+    emit q_ptr->newTextMessage(r, cm);
+
 }
 
-void RecordingModelPrivate::updateUnreadCount(const int count)
+void Media::RecordingModelPrivate::updateUnreadCount(const int count)
 {
     m_UnreadCount += count;
     if (m_UnreadCount <= 0) {
@@ -124,20 +136,20 @@ void RecordingModelPrivate::updateUnreadCount(const int count)
 
 Media::RecordingModel::~RecordingModel()
 {
-   delete d_ptr;
+    delete d_ptr;
 }
 
 Media::RecordingModel::RecordingModel(QObject* parent) : QAbstractItemModel(parent), CollectionManagerInterface<Recording>(this),
 d_ptr(new RecordingModelPrivate(this))
 {
-   setObjectName("RecordingModel");
+    setObjectName("RecordingModel");
 
-   d_ptr->m_pTextRecordingCollection = addCollection<LocalTextRecordingCollection>();
+    d_ptr->m_pTextRecordingCollection = addCollection<LocalTextRecordingCollection>();
 
-   d_ptr->m_pTextRecordingCollection->listId([](const QList<CollectionInterface::Element>& e) {
-      //TODO
-      Q_UNUSED(e);
-   });
+    d_ptr->m_pTextRecordingCollection->listId([](const QList<CollectionInterface::Element>& e) {
+        //TODO
+        Q_UNUSED(e);
+    });
 }
 
 Media::RecordingModel& Media::RecordingModel::instance()
@@ -148,167 +160,233 @@ Media::RecordingModel& Media::RecordingModel::instance()
 
 QHash<int,QByteArray> Media::RecordingModel::roleNames() const
 {
-   static QHash<int, QByteArray> roles = QAbstractItemModel::roleNames();
-   /*static bool initRoles = false;
-   if (!initRoles) {
-      initRoles = true;
-   }*/
-   return roles;
+    static QHash<int, QByteArray> roles = QAbstractItemModel::roleNames();
+    /*static bool initRoles = false;
+    if (!initRoles) {
+        initRoles = true;
+    }*/
+    return roles;
 }
 
 //Do nothing
 bool Media::RecordingModel::setData( const QModelIndex& index, const QVariant &value, int role)
 {
-   Q_UNUSED(index)
-   Q_UNUSED(value)
-   Q_UNUSED(role)
-   return false;
+    Q_UNUSED(index)
+    Q_UNUSED(value)
+    Q_UNUSED(role)
+    return false;
 }
 
 ///Get bookmark model data RecordingNode::Type and Call::Role
 QVariant Media::RecordingModel::data( const QModelIndex& index, int role) const
 {
-   if (!index.isValid())
-      return QVariant();
+    if (!index.isValid())
+        return QVariant();
 
-   //RecordingNode* modelItem = static_cast<RecordingNode*>(index.internalPointer());
+    const auto modelItem = static_cast<RecordingNode*>(index.internalPointer());
 
-   switch (role) {
-      case Qt::DisplayRole:
-         return "foo";
-   }
+    if (modelItem->m_Type == RecordingNode::Type::TOP_LEVEL) {
+        if (index.column() == 0 && role == Qt::DisplayRole)
+            return modelItem->m_CatName;
+        if (index.column() == 1 && role == Qt::DisplayRole)
+            return static_cast<int>(modelItem->m_lChildren.size());
 
-   return QVariant();
+        return {};
+    }
+
+    const TextRecording* tRec  = nullptr;
+    const AVRecording*   avRec = nullptr;
+
+    if (modelItem->m_pRec->type() == Recording::Type::TEXT)
+        tRec = static_cast<const TextRecording*>(modelItem->m_pRec);
+    else if (modelItem->m_pRec->type() == Recording::Type::AUDIO_VIDEO)
+        avRec = static_cast<const AVRecording*>(modelItem->m_pRec);
+
+    switch(index.column()) {
+        case 0:
+            switch (role) {
+                case Qt::DisplayRole:
+                    if (tRec && tRec->peers().size())
+                        return tRec->peers().first()->primaryName();
+                    if (avRec && avRec->call())
+                        return avRec->call()->peerContactMethod()->primaryName();
+            }
+            break;
+        case 1:
+            switch (role) {
+                case Qt::DisplayRole:
+                    if (tRec && tRec->peers().size())
+                        return tRec->size();
+                    if (avRec && avRec->call())
+                        return tr("N/A");
+            }
+            break;
+    }
+
+    return QVariant();
 }
 
 ///Get header data
 QVariant Media::RecordingModel::headerData(int section, Qt::Orientation orientation, int role) const
 {
-   Q_UNUSED(section)
-   if (orientation == Qt::Horizontal && role == Qt::DisplayRole)
-      return QVariant(tr("Recordings"));
-   return QVariant();
+    Q_UNUSED(section)
+    if (orientation == Qt::Horizontal && role == Qt::DisplayRole) {
+        switch(section) {
+            case 0:
+                return QVariant(tr("Recordings"));
+            case 1:
+                return QVariant(tr("Count"));
+        }
+    }
+    return QVariant();
 }
 
 ///Get the number of child of "parent"
 int Media::RecordingModel::rowCount( const QModelIndex& parent ) const
 {
-   if (!parent.isValid())
-      return d_ptr->m_lCategories.size();
-   else {
-      const RecordingNode* modelItem = static_cast<RecordingNode*>(parent.internalPointer());
-      return modelItem->m_lChildren.size();
-   }
+    if (!parent.isValid())
+        return d_ptr->m_lCategories.size();
+
+    // Only the first column has a tree
+    if (parent.column())
+        return 0;
+
+    const auto modelItem = static_cast<RecordingNode*>(parent.internalPointer());
+
+    return modelItem->m_lChildren.size();
 }
 
 Qt::ItemFlags Media::RecordingModel::flags( const QModelIndex& index ) const
 {
-   if (!index.isValid())
-      return Qt::NoItemFlags;
+    if (!index.isValid())
+        return Qt::NoItemFlags;
 
-   return Qt::ItemIsEnabled | Qt::ItemIsSelectable | Qt::ItemIsEnabled;
+    return Qt::ItemIsEnabled | Qt::ItemIsSelectable | Qt::ItemIsEnabled;
 }
 
-///There is only 1 column
 int Media::RecordingModel::columnCount ( const QModelIndex& parent) const
 {
-   Q_UNUSED(parent)
-   return 1;
+    Q_UNUSED(parent)
+
+    if (!parent.isValid())
+        return 2;
+
+    const auto modelItem = static_cast<RecordingNode*>(parent.internalPointer());
+
+    return modelItem->m_lChildren.size() ? 2 : 0;
 }
 
 ///Get the bookmark parent
 QModelIndex Media::RecordingModel::parent( const QModelIndex& idx) const
 {
-   if (!idx.isValid())
-      return QModelIndex();
+    if (!idx.isValid())
+        return {};
 
-   RecordingNode* modelItem = static_cast<RecordingNode*>(idx.internalPointer());
+    const auto modelItem = static_cast<RecordingNode*>(idx.internalPointer());
 
-   if (modelItem->m_Type == RecordingNode::Type::SESSION) {
-      RecordingNode* item = static_cast<RecordingNode*>(modelItem)->m_pParent;
+    if (modelItem->m_Type != RecordingNode::Type::SESSION)
+        return {};
 
-      if (item)
-         return createIndex(item->m_Index,0,item);
+    if (auto item = static_cast<RecordingNode*>(modelItem)->m_pParent)
+        return createIndex(item->m_Index, 0, item);
 
-   }
-
-   return QModelIndex();
+    return {};
 } //parent
 
 ///Get the index
 QModelIndex Media::RecordingModel::index(int row, int column, const QModelIndex& parent) const
 {
-   if (column || (!parent.isValid() && row >= d_ptr->m_lCategories.size()))
-      return QModelIndex();
+    const int count = static_cast<int>(d_ptr->m_lCategories.size());
 
-   const RecordingNode* modelItem = parent.isValid() ? static_cast<const RecordingNode*>(parent.internalPointer()) : d_ptr->m_lCategories[row];
+    if (column > 1 || row < 0 || ((!parent.isValid()) && row >= count))
+        return {};
 
-   if (row >= modelItem->m_lChildren.size())
-      return QModelIndex();
+    if (!parent.isValid())
+        return createIndex(row, column, d_ptr->m_lCategories[row]);
 
-   return createIndex(row,0,modelItem->m_lChildren[row]);
+    const auto modelItem = static_cast<const RecordingNode*>(parent.internalPointer());
+
+    if (row >= static_cast<int>(modelItem->m_lChildren.size()))
+        return {};
+
+    return createIndex(row, column, modelItem->m_lChildren[row]);
+}
+
+void Media::RecordingModelPrivate::initCategories()
+{
+    if (m_lCategories.size())
+        return;
+
+    //Create some categories
+    q_ptr->beginInsertRows({}, 0 , 1);
+    m_pText = new RecordingNode(RecordingNode::Type::TOP_LEVEL);
+    m_pText->m_CatName = tr("Text messages");
+    m_pText->m_Index   = 0;
+    m_lCategories.push_back(m_pText);
+
+    m_pAudioVideo = new RecordingNode(RecordingNode::Type::TOP_LEVEL);
+    m_pAudioVideo->m_CatName = tr("Audio/Video");
+    m_pAudioVideo->m_Index   = 1;
+    m_lCategories.push_back(m_pAudioVideo);
+    q_ptr->endInsertRows();
 }
 
 bool Media::RecordingModel::addItemCallback(const Recording* item)
 {
-   Q_UNUSED(item)
+    Q_UNUSED(item)
 
-   //Create some categories
-   if (d_ptr->m_lCategories.size() == 0) {
-      d_ptr->m_pText = new RecordingNode(RecordingNode::Type::TOP_LEVEL);
-      d_ptr->m_pText->m_CatName = tr("Text messages");
-      d_ptr->m_pText->m_Index = 0;
-      d_ptr->m_lCategories << d_ptr->m_pText;
+    d_ptr->initCategories();
 
-      d_ptr->m_pAudioVideo = new RecordingNode(RecordingNode::Type::TOP_LEVEL);
-      d_ptr->m_pAudioVideo->m_CatName = tr("Audio/Video");
-      d_ptr->m_pAudioVideo->m_Index = 1;
-      d_ptr->m_lCategories << d_ptr->m_pAudioVideo;
+    //Categorize by general media group
+    RecordingNode* parent = nullptr;
 
-      /*d_ptr->m_pFiles = new RecordingNode(RecordingNode::Type::TOP_LEVEL);
-      d_ptr->m_pFiles->m_CatName = tr("Files");
-      d_ptr->m_pFiles->m_Index = 2;
-      d_ptr->m_lCategories << m_pFiles;*/
-   }
+    if (item->type() == Recording::Type::TEXT)
+        parent = d_ptr->m_pText;
+    else if (item->type() == Recording::Type::AUDIO_VIDEO)
+        parent = d_ptr->m_pAudioVideo;
 
-   //Categorize by general media group
-   RecordingNode* parent = nullptr;
+    if (!parent)
+        return false;
 
-   if (item->type() == Recording::Type::TEXT)
-      parent = d_ptr->m_pText;
-   else if (item->type() == Recording::Type::AUDIO_VIDEO)
-      parent = d_ptr->m_pAudioVideo;
-   /*else if (item->type() == Recording::Type::FILE))
-      parent = d_ptr->m_pFiles;*/
+    //Insert the item]
+    const int idx = parent->m_lChildren.size();
+    beginInsertRows(index(parent->m_Index,0), idx, idx);
 
-   //Insert the item
-   if (parent) {
+    RecordingNode* n = new RecordingNode       ( RecordingNode::Type::SESSION );
+    n->m_pRec        = const_cast<Recording*>  ( item );
+    n->m_Index       = idx;
+    n->m_pParent     = parent;
+    parent->m_lChildren.push_back(n);
 
-      beginInsertRows(index(parent->m_Index,0),parent->m_lChildren.size(),parent->m_lChildren.size());
+    endInsertRows();
 
-      RecordingNode* n = new RecordingNode       ( RecordingNode::Type::SESSION );
-      n->m_pRec        = const_cast<Recording*>  ( item                         );
-      n->m_Index       = parent->m_lChildren.size(                              );
-      parent->m_lChildren << n;
+    if (item->type() == Recording::Type::TEXT) {
+        const TextRecording* r = static_cast<const TextRecording*>(item);
+        connect(r, &TextRecording::unreadCountChange, d_ptr, &RecordingModelPrivate::updateUnreadCount);
+        connect(r, &TextRecording::messageInserted  , [n, this, parent](
+            const QMap<QString,QString>&, ContactMethod* cm, Media::Media::Direction d
+        ){
+            const auto par = index(parent->m_Index, 0);
 
-      endInsertRows();
+            emit dataChanged(
+                index(n->m_Index, 0, par),
+                index(n->m_Index, 1, par)
+            );
 
-      if (item->type() == Recording::Type::TEXT) {
-         const TextRecording* r = static_cast<const TextRecording*>(item);
-         connect(r, &TextRecording::messageInserted, d_ptr, &RecordingModelPrivate::forwardInsertion);
-         connect(r, &TextRecording::unreadCountChange, d_ptr, &RecordingModelPrivate::updateUnreadCount);
-      }
+            if (n->m_pRec->type() == Recording::Type::TEXT)
+                d_ptr->forwardInsertion(
+                    static_cast<TextRecording*>(n->m_pRec), cm, d
+                );
+        });
+    }
 
-      return true;
-   }
-
-   return false;
+    return true;
 }
 
 bool Media::RecordingModel::removeItemCallback(const Recording* item)
 {
-   Q_UNUSED(item)
-   return false;
+    Q_UNUSED(item)
+    return false;
 }
 
 bool Media::RecordingModel::clearAllCollections() const
@@ -329,35 +407,35 @@ void Media::RecordingModel::clear()
 
 void Media::RecordingModel::collectionAddedCallback(CollectionInterface* backend)
 {
-   Q_UNUSED(backend)
+    Q_UNUSED(backend)
 }
 
 ///Set where the call recordings will be saved
 void Media::RecordingModel::setRecordPath(const QString& path)
 {
-   ConfigurationManagerInterface& configurationManager = ConfigurationManager::instance();
-   configurationManager.setRecordPath(path);
+    ConfigurationManagerInterface& configurationManager = ConfigurationManager::instance();
+    configurationManager.setRecordPath(path);
 }
 
 ///Return the path where recordings are going to be saved
 QString Media::RecordingModel::recordPath() const
 {
-   ConfigurationManagerInterface& configurationManager = ConfigurationManager::instance();
-   return configurationManager.getRecordPath();
+    ConfigurationManagerInterface& configurationManager = ConfigurationManager::instance();
+    return configurationManager.getRecordPath();
 }
 
 ///are all calls recorded by default
 bool Media::RecordingModel::isAlwaysRecording() const
 {
-   ConfigurationManagerInterface& configurationManager = ConfigurationManager::instance();
-   return configurationManager.getIsAlwaysRecording();
+    ConfigurationManagerInterface& configurationManager = ConfigurationManager::instance();
+    return configurationManager.getIsAlwaysRecording();
 }
 
 ///Set if all calls needs to be recorded
 void Media::RecordingModel::setAlwaysRecording(bool record)
 {
-   ConfigurationManagerInterface& configurationManager = ConfigurationManager::instance();
-   configurationManager.setIsAlwaysRecording   ( record );
+    ConfigurationManagerInterface& configurationManager = ConfigurationManager::instance();
+    configurationManager.setIsAlwaysRecording   ( record );
 }
 
 int  Media::RecordingModel::unreadCount() const
@@ -368,9 +446,9 @@ int  Media::RecordingModel::unreadCount() const
 ///Create or load the recording associated with the ContactMethod cm
 Media::TextRecording* Media::RecordingModel::createTextRecording(const ContactMethod* cm)
 {
-   TextRecording* r = d_ptr->m_pTextRecordingCollection->createFor(cm);
+    TextRecording* r = d_ptr->m_pTextRecordingCollection->createFor(cm);
 
-   return r;
+    return r;
 }
 
 #include <recordingmodel.moc>
