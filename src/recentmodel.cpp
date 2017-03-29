c9bb7a41f01a5773ba60dd6aec798540776b1613
AmarOk <blin.sebastien@enconn.fr>
2017-03-28 22:42:14 -0400

    recentmodel: remove duplicate conversation
    When a new contact is added, if the CM is already present in m_lTopLevelReverted,
    a duplicate node will be created. So, we need to remove the CM from m_hCMsToNodes
    
    Change-Id: I5cf91c5a2a977b51a19c792f3e4b0b7d5c84a2db
    Tuleap: #1549

--- src/recentmodel.cpp
+++ src/recentmodel.cpp
@@ -692,6 +692,9 @@ void RecentModelPrivate::insertNode(RecentViewNode* n, time_t t, bool isNew)
    else
       q_ptr->endInsertRows();
 
+    auto newIdx = q_ptr->index(newPos, 0);
+    q_ptr->selectionModel()->setCurrentIndex(newIdx, QItemSelectionModel::ClearAndSelect);
+
 #if 0
     //Uncomment if there is issues
     qDebug() << "\n\nList:" << m_lTopLevelReverted.size() << isNew;
@@ -724,8 +727,17 @@ void RecentModelPrivate::removeNode(RecentViewNode* n)
 
 void RecentModelPrivate::slotPersonAdded(const Person* p)
 {
-   if (p)
+   if (p) {
+      // if the contact method is already present in m_hCMsToNodes
+      // we need to remove it first.
+      auto cmToRm = p->phoneNumbers()[0];
+      if (m_hCMsToNodes.contains(cmToRm)) {
+          removeNode(m_hCMsToNodes.value(cmToRm));
+          m_hCMsToNodes.remove(cmToRm);
+      }
+
       slotLastUsedTimeChanged(p, p->lastUsedTime());
+   }
 }
 
 void RecentModelPrivate::slotLastUsedTimeChanged(const Person* p, time_t t)
