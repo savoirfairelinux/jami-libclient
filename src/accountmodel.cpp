0108d87710160788604eec22a96c178a1ab01d97
Kateryna Kostiuk <kateryna.kostiuk@savoirfairelinux.com>
2017-03-30 12:16:10 -0400

    AccountModel: separate selected and chosen account
    Selected account is used to display information in preferences. Chosen
    account is the manually user selected account.
    
    Change-Id: I8a86db1b9847c9754a20fd105f7cc288ff1decda
    Tuleap: #1532

--- src/accountmodel.cpp
+++ src/accountmodel.cpp
@@ -55,7 +55,7 @@ QHash<QByteArray,AccountPlaceHolder*> AccountModelPrivate::m_hsPlaceHolder;
 
 AccountModelPrivate::AccountModelPrivate(AccountModel* parent) : QObject(parent),q_ptr(parent),
 m_pIP2IP(nullptr),m_pProtocolModel(nullptr),m_pSelectionModel(nullptr),m_lMimes({RingMimes::ACCOUNT}),
-m_selectedAccount(nullptr), m_lSupportedProtocols {{
+m_lSupportedProtocols {{
    /* SIP  */ false,
    /* RING */ false,
 }}
@@ -243,23 +243,50 @@ QItemSelectionModel* AccountModel::selectionModel() const
    return d_ptr->m_pSelectionModel;
 }
 
+
+QItemSelectionModel* AccountModel::userSelectionModel() const
+{
+   if (!d_ptr->m_pUserSelectionModel)
+      d_ptr->m_pUserSelectionModel = new QItemSelectionModel(const_cast<AccountModel*>(this));
+
+   return d_ptr->m_pUserSelectionModel;
+}
+
 /**
  * returns the select account
  */
 Account*
 AccountModel::selectedAccount() const
 {
-    return d_ptr->m_selectedAccount;
+   auto accIdx = AccountModel::instance().selectionModel()->currentIndex();
+   return AccountModel::instance().getAccountByModelIndex(accIdx);
 }
 
 /**
- * set the select account and emits selectedAccountChanged signal
+ * Helper method to update the selected index in user selection model
  */
 void
-AccountModel::setSelectedAccount(Account* a)
+AccountModel::setUserChosenAccount(Account* account)
 {
-    d_ptr->m_selectedAccount = a;
-    emit selectedAccountChanged(a);
+    if (!account) {
+        return;
+    }
+
+    const auto idx = account->index();
+
+    userSelectionModel()->setCurrentIndex(
+        idx, QItemSelectionModel::ClearAndSelect
+    );
+}
+
+/**
+ * returns the user chosen account
+ */
+Account*
+AccountModel::userChosenAccount() const
+{
+   auto accIdx = AccountModel::instance().userSelectionModel()->currentIndex();
+   return AccountModel::instance().getAccountByModelIndex(accIdx);
 }
 
 QList<Account*> AccountModel::accountsToMigrate() const
