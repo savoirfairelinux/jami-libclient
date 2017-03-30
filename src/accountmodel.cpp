1247626383c8d39562e13f5e0b7a253ce7d60334
Nicolas Jager <nicolas.jager@savoirfairelinux.com>
2017-03-30 10:19:51 -0400

    AccountModel : updateAccounts() cares of userChosenAccount()
    - so far, we didn't take care to check when we remove an account
    if the selected account need to be updated. This patch checks when
    updateAccounts() is called, if userChosenAccount() is still valid.
    If not, it use the first account in the list. If there is no account
    at all, it just set the pointer to nullptr.
    
    Change-Id: I507087d12df48059a697ad1a757e7d4267034c73

--- src/accountmodel.cpp
+++ src/accountmodel.cpp
@@ -573,6 +573,12 @@ void AccountModel::updateAccounts()
          acc->performAction(Account::EditAction::RELOAD);
       }
    }
+
+   // check if the selected account is still alive
+   if (!userChosenAccount()) {
+       setUserChosenAccount((size()) ? d_ptr->m_lAccounts[0] : nullptr);
+   }
+
    emit accountListUpdated();
 } //updateAccounts
 
