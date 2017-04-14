b2d5d55d13aa3b89f6d78912d7dd1713093456c8
Stepan Salenikovich <stepan.salenikovich@savoirfairelinux.com>
2017-04-14 18:11:19 -0400

    fix RecentModel ordering
    UsageStatistics::update() was being called before setLastUsed()
    in the ContactMethod which resulted in the lastUsedChanged signal
    not being emitted and so the RecentModel was not being re-ordered.
    
    Change-Id: I866b213ae97aacd5bd6ef6157fb136a436952b08

--- src/contactmethod.cpp
+++ src/contactmethod.cpp
@@ -588,6 +588,8 @@ void ContactMethod::addCall(Call* call)
    d_ptr->m_Type = ContactMethod::Type::USED;
    d_ptr->m_lCalls << call;
 
+   // call setLastUsed first so that we emit lastUsedChanged()
+   setLastUsed(time);
    //Update the contact method statistics
    d_ptr->m_UsageStats.update(call->startTimeStamp(), call->stopTimeStamp());
    if (d_ptr->m_pAccount)
@@ -602,7 +604,6 @@ void ContactMethod::addCall(Call* call)
    auto time = call->startTimeStamp();
    if (d_ptr->m_pAccount)
        d_ptr->m_pAccount->usageStats.setLastUsed(time);
-   setLastUsed(time);
 
    d_ptr->callAdded(call);
    d_ptr->changed();
