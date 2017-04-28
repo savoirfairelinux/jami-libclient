d5a33f62ddbfc90647ff4cb7fa93085ca1721f55
Emmanuel Lepage Vallee <elv1313@gmail.com>
2017-04-28 04:34:33 -0400

    call: Prevent ambiguous Ring URI from being used in placeCall()
    As of now, unresolved registered names could be passed to placeCall
    with a Ring account. As the URI parser doesn't have the metadata
    required to correctly pick the scheme, it used `sip:`. This isn't
    correct when the account is a Ring account (but the URI is unaware
    of that).
    
    This commit set the correct scheme before calling placeCall.
    
    Change-Id: Ied3cfd1ef7f7171e3bf05d51a245e35e2df95ffb

--- src/call.cpp
+++ src/call.cpp
@@ -1780,6 +1780,14 @@ void CallPrivate::call()
 
     // Warning: m_pDialNumber can become nullptr when linking directly
     URI uri {peerCM->uri()};
+
+    // Better late than never. Set the scheme.
+    if (m_Account && m_Account->protocol() == Account::Protocol::RING &&
+      uri.schemeType() == URI::SchemeType::NONE &&
+      uri.protocolHint() != URI::ProtocolHint::RING) {
+       uri.setSchemeType(URI::SchemeType::RING);
+    }
+
     if (!m_pPeerContactMethod) {
         m_pPeerContactMethod = PhoneDirectoryModel::instance().getNumber(uri, q_ptr->account());
     }
@@ -1795,6 +1803,12 @@ void CallPrivate::call()
     //Refresh peerCM
     peerCM = q_ptr->peerContactMethod();
     peerCM->addCall(q_ptr);
+
+    // dring can print "No suitable account to create outgoing call" if the
+    // URI isn't properly formatted (like sip:foobar).
+    if (m_Account && m_Account->protocol() == Account::Protocol::RING && uri.protocolHint() != URI::ProtocolHint::RING) {
+       qWarning() << "The URI isn't a RingId. It is possible the call wont work" << uri.full();
+    }
 
     m_DringId = CallManager::instance().placeCall(m_Account->id(), uri.full());
 
