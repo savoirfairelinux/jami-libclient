fa00bd1daf01b3882860958c55cf355db8dd93eb
Emmanuel Lepage Vallee <elv1313@gmail.com>
2017-04-28 05:25:54 -0400

    phonedirectory: prevent unlikely null dereference
    There is a code path combination (that should not happen, but is
    reachable) that could cause a null dereference.
    
    [SS: add warning log in case it happens to make it easier to track down]
    
    Change-Id: I1c2c2b5be417e9ec74e0ab157c9aadcd0ec4a613
    Signed-off-by: Stepan Salenikovich <stepan.salenikovich@savoirfairelinux.com>
    Reviewed-by: Stepan Salenikovich <stepan.salenikovich@savoirfairelinux.com>

--- src/phonedirectorymodel.cpp
+++ src/phonedirectorymodel.cpp
@@ -599,7 +599,11 @@ ContactMethod* PhoneDirectoryModel::getNumber(const QString& uri, Person* contac
             d_ptr->m_hDirectory    [extendedUri] = wrap2;
             d_ptr->m_hSortedNumbers[extendedUri] = wrap2;
          }
-         wrap2->numbers << number;
+
+         if (wrap2)
+            wrap2->numbers << number;
+         else
+            qWarning() << "PhoneDirectoryModel: code path should not be reached, wrap2 is nullptr";
       }
 
    }
