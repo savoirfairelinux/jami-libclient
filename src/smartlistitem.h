e8e3118b3db02716558e1720051488f018e01bae
Nicolas Jager <nicolas.jager@savoirfairelinux.com>
2017-08-01 14:32:05 -0400

    add SmartListModel and SmartListItem
    Change-Id: Ia06f874dd1baebb086edd570e52d8d61d73b905b

---
+++ src/smartlistitem.h
@@ -0,0 +1,46 @@
+/****************************************************************************
+ *   Copyright (C) 2017 Savoir-faire Linux                                  *
+ *   Author : Nicolas Jäger <nicolas.jager@savoirfairelinux.com>            *
+ *                                                                          *
+ *   This library is free software; you can redistribute it and/or          *
+ *   modify it under the terms of the GNU Lesser General Public             *
+ *   License as published by the Free Software Foundation; either           *
+ *   version 2.1 of the License, or (at your option) any later version.     *
+ *                                                                          *
+ *   This library is distributed in the hope that it will be useful,        *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of         *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU      *
+ *   Lesser General Public License for more details.                        *
+ *                                                                          *
+ *   You should have received a copy of the GNU General Public License      *
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.  *
+ ***************************************************************************/
+#pragma once
+
+// Std
+#include <string>
+
+enum class SmartListItemType{
+    CONTACT,
+    DEFAULT,
+    COUNT
+};
+
+class SmartListItem {
+    public:
+    ~SmartListItem();
+
+    virtual void setTitle(const std::string) {}; // limiter l'acces seulement la factory devrait le modifier
+    virtual const std::string getTitle() { return "TITLE ERROR"; };
+    SmartListItemType getType(){return _smartListItemType;};
+    
+    
+
+    protected:
+    SmartListItem(SmartListItemType);
+
+    private:
+    std::string* _title;
+    SmartListItemType _smartListItemType;
+    
+};
