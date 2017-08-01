b54736b326f9331ae694d78b896812ef27e4d2d3
Nicolas Jager <nicolas.jager@savoirfairelinux.com>
2017-08-01 16:49:52 -0400

    WIP : smartlistmodel, smartlistitem and contactitem
    Change-Id: I5407f2885bba40f6f20f32e0a14c866dcaa7eb0d

---
+++ src/smartlistmodel.cpp
@@ -0,0 +1,89 @@
+/****************************************************************************
+ *   Copyright (C) 2017 Savoir-faire Linux                                  *
+ *   Author : Nicolas Jäger <nicolas.jager@savoirfairelinux.com>            *
+ *                                                                          *
+ *   This library is free software; you can redistribute it and/or          *
+ *   modify it under the terms of the GNU Lesser General Public             *
+ *   License as published by the Free Software Foundation; either           *
+ *   version 2.1 of the License, or (at your option) any later version.     *
+ *                                                                          *
+ *   This library is distributed in the hope that it will be useful,        *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of         *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU      *
+ *   Lesser General Public License for more details.                        *
+ *                                                                          *
+ *   You should have received a copy of the GNU General Public License      *
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.  *
+ ***************************************************************************/
+
+
+
+
+
+// Parent
+#include "smartlistmodel.h"
+
+// Debug
+#include <qdebug.h>
+
+// Lrc mettre des ""
+#include <contactitem.h>
+#include <accountmodel.h>
+#include <contactmethod.h>
+#include <availableaccountmodel.h>
+
+SmartListModel::SmartListModel(QObject* parent)
+{
+    auto fillsWithContacts = [&] (Account* a) {
+        if (not a) {
+            qDebug() << "no available account selected";
+            return a;
+        }
+
+        auto contacts = a->getContacts();
+
+        // clear the list
+        items.clear();
+
+        // add contacts to the list
+        for (auto c : contacts) {
+            auto contact = new ContactItem();
+            contact->setTitle(c->uri().toUtf8().constData());
+            items.push_back(contact);
+        }
+
+        return a;
+    };
+
+    connect(&AvailableAccountModel::instance(), &AvailableAccountModel::currentDefaultAccountChanged,
+    [this, fillsWithContacts](Account* a)
+    {
+        if (fillsWithContacts(a))
+            emit modelUpdated();
+    });
+
+    // initialise the list
+    fillsWithContacts(AvailableAccountModel::instance().currentDefaultAccount());
+}
+
+SmartListModel::~SmartListModel()
+{
+    // delete the items
+    for (auto item : items)
+        delete item;
+}
+
+std::list<SmartListItem*>
+SmartListModel::getItems()
+{
+    return items;
+}
+
+SmartListModel& SmartListModel::instance()
+{
+    static auto instance = new SmartListModel(QCoreApplication::instance());
+    return *instance;
+}
+
+
+#include <smartlistmodel.moc>
