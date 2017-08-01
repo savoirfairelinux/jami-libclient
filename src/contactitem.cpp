3893b279bf16f0660019a59f1895901cf8bd4d92
Nicolas Jager <nicolas.jager@savoirfairelinux.com>
2017-08-01 16:49:52 -0400

    WIP : smartlistmodel, smartlistitem and contactitem
    Change-Id: I5407f2885bba40f6f20f32e0a14c866dcaa7eb0d

---
+++ src/contactitem.cpp
@@ -0,0 +1,55 @@
+/****************************************************************************
+ *   Copyright (C) 2017 Savoir-faire Linux                                  *
+ *   Author : Nicolas Jäger <nicolas.jager@savoirfairelinux.com>            *
+ *                                                                          *
+ *   This library is free software; you can redistribute it and/or          *
+ *   modify it under the terms of the GNU Lesser General Public             *
+ *   License as published by the Free Software Foundation; either           *
+ *   version 2.1 of the License, or (at your option) any later version.     *
+ *                                                                          *
+ *   This library is distributed in the hope that it will be useful,        *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of         *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU      *
+ *   Lesser General Public License for more details.                        *
+ *                                                                          *
+ *   You should have received a copy of the GNU General Public License      *
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.  *
+ ***************************************************************************/
+
+
+
+// Parent
+#include "contactitem.h"
+
+
+
+// Debug
+#include <qdebug.h>
+
+
+ContactItem::ContactItem()
+: SmartListItem()
+{
+}
+
+ContactItem::~ContactItem()
+{
+}
+
+void
+ContactItem::setTitle(const std::string title)
+{
+    if (not _title)
+        _title = std::unique_ptr<std::string>(new std::string());
+
+    _title->assign(title);
+}
+
+const std::string
+ContactItem::getTitle()
+{
+    return _title->data();
+}
+
+
+#include <contactitem.moc>
