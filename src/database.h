e9cfc02a447a24477151caafadb1af16dcba94d3
Nicolas Jager <nicolas.jager@savoirfairelinux.com>
2017-08-03 08:16:38 -0400

    add DataBase
    Change-Id: Id5893a64928cd1f736794ccbce99ce2ae4adf6a3

---
+++ src/database.h
@@ -0,0 +1,56 @@
+/****************************************************************************
+ *   Copyright (C) 2017 Savoir-faire Linux                                  *
+ *   Author : Nicolas Jäger <nicolas.jager@savoirfairelinux.com>            *
+ *                                                                          *
+ *   This library is free software; you can redistribute it and/or          *
+ *   modify it under the terms of the GNU Lesser General Public             *
+ *   License as published by the Free Software Foundation; either           *
+ *   version 2.1 of the License, or (at your option) any later version.     *
+ *                                                                          *
+ *   This library is distributed in the hope that it will be useful,        *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of         *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU      *
+ *   Lesser General Public License for more details.                        *
+ *                                                                          *
+ *   You should have received a copy of the GNU General Public License      *
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.  *
+ ***************************************************************************/
+#pragma once
+
+// Std
+#include <string>
+#include <memory>
+#include <vector>
+#include <forward_list>
+
+// Qt
+#include <qobject.h>
+#include <QtSql/QSqlQuery>
+
+
+// TEST
+#include <qdebug.h>
+
+constexpr char ringDB[] = "ring.db";
+
+class DataBase : public QObject {
+    Q_OBJECT
+    public:
+    ~DataBase();
+
+    void addMessage(const QString& From, const QString& message);
+    std::forward_list<std::string> getMessages(const QString& author);
+
+    //Singleton
+    static DataBase& instance();
+
+    // signals
+    Q_SIGNALS:
+
+    private:
+    explicit DataBase(QObject* parent = nullptr);
+
+    std::unique_ptr<QSqlQuery> _querry;
+    QSqlDatabase _db;
+
+};
