def34c7f38f31cf4b1d8596cd29753a2456505f9
Nicolas Jager <nicolas.jager@savoirfairelinux.com>
2017-08-21 15:29:38 -0400

    Add stubs and class diagram
    add stubs for :
     - DatabaseManager class.
     - ConversationModel class and Conversation namespace.
     - NewCallModel class and NewCall namespace.
     - ContactModel class and Contact namespace.
     - Message namespace.
     - NewAccountModel.
     - Lrc class, this class is the root class of all models. Instating this
       class will build interfaces between the daemon, client and the
       database.
     - all these class are nested in the lrc namespace.
     - add diagrams in doc folder
    
    Change-Id: Ia06f874dd1baebb086edd570e52d8d61d73b905b

---
+++ src/data/message.h
@@ -0,0 +1,54 @@
+/****************************************************************************
+ *   Copyright (C) 2017 Savoir-faire Linux                                  *
+ *   Author : Nicolas Jäger <nicolas.jager@savoirfairelinux.com>            *
+ *   Author : Sébastien Blin <sebastien.blin@savoirfairelinux.com>          *
+ *                                                                          *
+ *   This library is free software; you can redistribute it and/or          *
+ *   modify it under the terms of the GNU Lesser General Public             *
+ *   License as published by the Free Software Foundation; either           *
+ *   version 2.1 of the License, or (at your option) any later version.     *
+ *                                                                          *
+ *   This library is distributed in the hope that it will be useful,        *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of         *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU      *
+ *   Lesser General Public License for more details.                        *
+ *                                                                          *
+ *   You should have received a copy of the GNU General Public License      *
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.  *
+ ***************************************************************************/
+#pragma once
+
+namespace lrc
+{
+
+namespace message
+{
+
+enum class Type {
+    INVALID,
+    TEXT,
+    CALL,
+    CONTACT
+};
+
+enum class Status {
+    INVALID,
+    SENDING,
+    FAILED,
+    SUCCEED,
+    READ
+};
+
+struct Info
+{
+    std::string uid;
+    std::string body;
+    std::time_t timestamp = 0;
+    Type type = Type::INVALID;
+    Status status = Status::INVALID;
+};
+
+} // namespace message
+
+using MessagesMap = std::map<int, message::Info>;
+} // namespace lrc
