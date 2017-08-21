5f0b90d676ca4b44359a7420bb32fe6a2eeb9238
Nicolas Jager <nicolas.jager@savoirfairelinux.com>
2017-08-21 15:29:38 -0400

    Add stubs and class diagram
    add stubs for :
     - DatabaseManager class.
     - ConversationModel class and Conversation namespace.
     - NewCallModel class and NewCall namespace.
     - ContactModel class and Contact namespace.
     - Message namespace.
     - NewAccountModel.
     - CallbacksHandler, will be used to bind the callbacks from the daemon
       and will emit signals for lrc and client.
     - Lrc class, this class is the root class of all models. Instating this
       class will build interfaces between the daemon, client and the
       database.
     - all these class are nested in the lrc namespace.
     - add diagrams in doc folder
    
    Change-Id: Ia06f874dd1baebb086edd570e52d8d61d73b905b

---
+++ src/data/call.h
@@ -0,0 +1,57 @@
+/****************************************************************************
+ *   Copyright (C) 2017 Savoir-faire Linux                                  *
+ *   Author : Nicolas Jäger <nicolas.jager@savoirfairelinux.com>            *
+ *   Author : Sébastien Blin <sebastien.blin@savoirfairelinux.com>          *
+ *                                                                          *
+ *   This library is free software; you can redistribute it and/or          *
+ *   modify it under the terms of the GNU Lesser General Public             *
+ *   License as published by the Free Software Foundation; either           *
+ *   version 2.1 of the License, or (at your option) any later version.     *
+ *                                                                          *
+ *   This library is distributed in the hope that it will be useful,        *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of         *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU      *
+ *   Lesser General Public License for more details.                        *
+ *                                                                          *
+ *   You should have received a copy of the GNU General Public License      *
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.  *
+ ***************************************************************************/
+#pragma once
+
+// std
+#include <ctime>
+
+namespace lrc
+{
+
+namespace call
+{
+
+enum class Status {
+    INVALID,
+    OUTGOING_REQUESTED,
+    INCOMING_RINGING,
+    OUTGOING_RINGING,
+    CONNECTING,
+    SEARCHING,
+    IN_PROGRESS,
+    PAUSED,
+    PEER_PAUSED,
+    INACTIVE,
+    ENDED,
+    TERMINATING,
+    CONNECTED,
+    AUTO_ANSWERING
+};
+
+struct Info
+{
+    std::string id;
+    std::time_t startTime = 0;
+    Status status = Status::INVALID;
+};
+
+} // namespace call
+
+using CallsInfoMap = std::map<std::string, std::shared_ptr<call::Info>>;
+} // namespace lrc
---
+++ src/newaccountmodel.h
@@ -0,0 +1,58 @@
+/****************************************************************************
+ *   Copyright (C) 2017 Savoir-faire Linux                                  *
+ *   Author : Nicolas Jäger <nicolas.jager@savoirfairelinux.com>            *
+ *   Author : Sébastien Blin <sebastien.blin@savoirfairelinux.com>          *
+ *                                                                          *
+ *   This library is free software; you can redistribute it and/or          *
+ *   modify it under the terms of the GNU Lesser General Public             *
+ *   License as published by the Free Software Foundation; either           *
+ *   version 2.1 of the License, or (at your option) any later version.     *
+ *                                                                          *
+ *   This library is distributed in the hope that it will be useful,        *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of         *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU      *
+ *   Lesser General Public License for more details.                        *
+ *                                                                          *
+ *   You should have received a copy of the GNU General Public License      *
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.  *
+ ***************************************************************************/
+#pragma once
+
+// Std
+#include <memory>
+#include <deque>
+#include <map>
+
+// Qt
+#include <qobject.h>
+
+// Data
+#include "data/account.h"
+
+// Lrc
+#include "typedefs.h"
+
+namespace lrc
+{
+
+class Database;
+class Lrc;
+
+class LIB_EXPORT NewAccountModel : public QObject {
+    Q_OBJECT
+
+    friend class Lrc;
+
+public:
+    ~NewAccountModel();
+    const account::Info& getAccountInfo(const std::string& accountId);
+
+private:
+    explicit NewAccountModel(const Database& database);
+    const Database& database_;
+    AccountsInfoMap accounts_;
+};
+
+using upNewAccountModel = std::unique_ptr<NewAccountModel>;
+
+} // namespace lrc
