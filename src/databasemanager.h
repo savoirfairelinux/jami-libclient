03d1b0502fc430f975bea35bb5b9af695aa902e2
Nicolas Jager <nicolas.jager@savoirfairelinux.com>
2017-08-21 15:29:38 -0400

    Add stubs and class diagram
    add stubs for :
     - DatabaseManager class.
     - ConversationModel class and Conversation namespace.
     - NewCallModel class and NewCall namespace.
     - ContactModel class and Contact namespace.
     - Message namespace.
     - NewAccountModel.
     - Lrc class, this class is the root class of all models. Instating this
       class will build interfaces between the daemon, client and the
       database.
     - all these class are nested in the lrc namespace.
     - add diagrams in doc folder
    
    Change-Id: Ia06f874dd1baebb086edd570e52d8d61d73b905b

---
+++ src/databasemanager.h
@@ -0,0 +1,124 @@
+/****************************************************************************
+ *   Copyright (C) 2017 Savoir-faire Linux                                  *
+ *   Author : Nicolas Jäger <nicolas.jager@savoirfairelinux.com>            *
+ *   Author : Sébastien Blin <sebastien.blin@savoirfairelinux.com>          *
+ *                                                                          *
+ *   This library is free software; you can redistribute it and/or          *
+ *   modify it under the terms of the GNU Lesser General Public             *
+ *   License as published by the Free Software Foundation; either           *
+ *   version 2.1 of the License, or (at your option) any later version.     *
+ *                                                                          *
+ *   This library is distributed in the hope that it will be useful,        *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of         *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU      *
+ *   Lesser General Public License for more details.                        *
+ *                                                                          *
+ *   You should have received a copy of the GNU General Public License      *
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.  *
+ ***************************************************************************/
+#pragma once
+
+// Std
+#include <memory>
+#include <string>
+
+// Qt
+#include <qobject.h>
+#include <QtSql/QSqlQuery>
+
+// Lrc
+#include "message.h"
+#include "typedefs.h"
+#include "namedirectory.h"
+#include "lrc.h"
+
+constexpr char ringDB[] = "ring.db";
+
+class Account;
+
+namespace lrc
+{
+
+class LIB_EXPORT Database : public QObject {
+    Q_OBJECT
+public:
+    ~Database();
+
+    // Messages related
+    /**
+     * Add a message object into the database
+     * @param account account linked to message
+     * @param message the object to add
+     */
+    void addMessage(const std::string& account, const message::Info& message) const;
+    /**
+     * Clear the history of the conversation between account and uid
+     * @param account
+     * @param uid
+     * @param removeContact if we also want to remove the contact
+     */
+    void clearHistory(const std::string& account,
+                      const std::string& uid,
+                      bool removeContact = false) const;
+    /**
+     * @param  account
+     * @param  uid
+     * @return messages from the conversation between account and uid
+     */
+    MessagesMap getAllMessages(const std::string& account, const std::string& uid) const;
+    /**
+     * @param  account
+     * @param  uid
+     * @return the number of unread messages in the conversation between account and uid
+     */
+    unsigned int numberOfUnreads(const std::string& account, const std::string& uid) const;
+    /**
+     * Set a message READ
+     * @param uid of a message
+     */
+    void setMessageRead(int uid) const;
+
+    // Contacts related
+    /**
+     * Add a contact into the database
+     * @param contact a uid
+     * @param payload the VCard of a contact
+     */
+    void addContact(const std::string& contact, const QByteArray& payload) const;
+    /**
+     * @param  uid of a contact
+     * @param  the attribute to search for a contact
+     * @return attribute of a contact
+     */
+    std::string getContactAttribute(const std::string& uid, const std::string& attribute) const;
+
+Q_SIGNALS:
+    /**
+     * Will be emitted each time a message is successfully stored into the database
+     * @param uid the uid of the message
+     * @param account linked to the conversation
+     * @param msg the message added
+     */
+    void messageAdded(int uid, const std::string& account, message::Info msg) const;
+    /**
+     * Will be emitted each time a contact is added into the database
+     * @param uid the uid of the contact
+     */
+    void contactAdded(const std::string& uid) const;
+
+public Q_SLOTS:
+    // NOTE: temporary, will be removed
+    void slotRegisteredNameFound(const Account* account,
+                                 NameDirectory::LookupStatus status,
+                                 const QString& address,
+                                 const QString& name) const;
+
+private:
+    friend Lrc::Lrc();
+    explicit Database();
+    std::unique_ptr<QSqlQuery> query_;
+    QSqlDatabase db_;
+
+};
+
+} // namespace lrc
---
+++ src/newaccountmodel.h
@@ -0,0 +1,54 @@
+/****************************************************************************
+ *   Copyright (C) 2017 Savoir-faire Linux                                  *
+ *   Author : Nicolas Jäger <nicolas.jager@savoirfairelinux.com>            *
+ *   Author : Sébastien Blin <sebastien.blin@savoirfairelinux.com>          *
+ *                                                                          *
+ *   This library is free software; you can redistribute it and/or          *
+ *   modify it under the terms of the GNU Lesser General Public             *
+ *   License as published by the Free Software Foundation; either           *
+ *   version 2.1 of the License, or (at your option) any later version.     *
+ *                                                                          *
+ *   This library is distributed in the hope that it will be useful,        *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of         *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU      *
+ *   Lesser General Public License for more details.                        *
+ *                                                                          *
+ *   You should have received a copy of the GNU General Public License      *
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.  *
+ ***************************************************************************/
+#pragma once
+
+// Std
+#include <memory>
+#include <deque>
+#include <map>
+
+// Qt
+#include <qobject.h>
+
+// Lrc
+#include "typedefs.h"
+#include "databasemanager.h"
+#include "accountinfo.h"
+
+namespace lrc
+{
+
+class LIB_EXPORT NewAccountModel : public QObject {
+    Q_OBJECT
+
+public:
+    explicit NewAccountModel(const DatabaseManager& dbManager);
+
+    ~NewAccountModel();
+    const account::Info& getAccountInfo(const std::string& id);
+
+private:
+
+    const DatabaseManager& dbManager_;
+    AccountsInfo accounts_;
+};
+
+using upNewAccountModel = std::unique_ptr<NewAccountModel>;
+
+} // namespace lrc
