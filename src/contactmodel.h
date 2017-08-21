c6831410bf7c4123083c0023baac4d158c695d2c
Nicolas Jager <nicolas.jager@savoirfairelinux.com>
2017-08-21 15:29:38 -0400

    Add stubs and class diagram
    add stubs for :
     - DatabaseManager class.
     - ConversationModel class and Conversation namespace.
     - NewCallModel class and NewCall namespace.
     - ContactModel class and Contact namespace.
     - Message namespace.
     - NewAccountModel.
     - Lrc class, this class is the root class of all models. Instating this
       class will build interfaces between the daemon, client and the
       database.
     - all these class are nested in the lrc namespace.
     - add diagrams in doc folder
    
    Change-Id: Ia06f874dd1baebb086edd570e52d8d61d73b905b

---
+++ src/contactmodel.h
@@ -0,0 +1,67 @@
+/****************************************************************************
+ *   Copyright (C) 2017 Savoir-faire Linux                                  *
+ *   Author : Nicolas Jäger <nicolas.jager@savoirfairelinux.com>            *
+ *   Author : Sébastien Blin <sebastien.blin@savoirfairelinux.com>          *
+ *                                                                          *
+ *   This library is free software; you can redistribute it and/or          *
+ *   modify it under the terms of the GNU Lesser General Public             *
+ *   License as published by the Free Software Foundation; either           *
+ *   version 2.1 of the License, or (at your option) any later version.     *
+ *                                                                          *
+ *   This library is distributed in the hope that it will be useful,        *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of         *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU      *
+ *   Lesser General Public License for more details.                        *
+ *                                                                          *
+ *   You should have received a copy of the GNU General Public License      *
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.  *
+ ***************************************************************************/
+#pragma once
+
+// Std
+#include <memory>
+
+// Qt
+#include <qobject.h>
+
+// Data
+#include "data/contact.h"
+#include "data/call.h"
+#include "database.h"
+
+namespace lrc
+{
+
+class ContactModel : public QObject {
+    Q_OBJECT
+public:
+    explicit ContactModel(const Database& db, const std::string& accountId);
+    ~ContactModel();
+
+    void addContact(const std::string& uri);
+    void removeContact(const std::string& uri);
+    void sendMessage(const std::string& uri, const std::string& body) const;
+    const contact::Info& getContact(const std::string& uri);
+    const ContactsInfoMap& getAllContacts() const;
+    void nameLookup(const std::string& uri) const;
+    void addressLookup(const std::string& name) const;
+    void setContactPresent(const std::string& uri, bool status);
+
+Q_SIGNALS:
+    void contactsChanged();
+
+private Q_SLOTS:
+    // TODO remove this from here when LRC signals are added
+    void slotContactsAdded(const QString &accountID, const QString &uri, bool confirmed);
+    void slotContactsRemoved(const QString &accountID, const QString &uri, bool status);
+
+private:
+    bool fillsWithContacts();
+
+    ContactsInfoMap contacts_;
+    const Database& db_;
+    const std::string accountId_;
+
+};
+
+}
