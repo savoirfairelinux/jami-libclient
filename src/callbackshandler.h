d36ee1eddacfae5bcd2b9d773dc970acfde99f53
Nicolas Jager <nicolas.jager@savoirfairelinux.com>
2017-08-21 15:29:38 -0400

    Add stubs and class diagram
    add stubs for :
     - DatabaseManager class.
     - ConversationModel class and Conversation namespace.
     - NewCallModel class and NewCall namespace.
     - ContactModel class and Contact namespace.
     - Message namespace.
     - NewAccountModel.
     - CallbacksHandler, will be used to bind the callbacks from the daemon
       and will emit signals for lrc and client.
     - Lrc class, this class is the root class of all models. Instating this
       class will build interfaces between the daemon, client and the
       database.
     - all these class are nested in the lrc namespace.
     - add diagrams in doc folder
    
    Change-Id: Ia06f874dd1baebb086edd570e52d8d61d73b905b

---
+++ src/callbackshandler.h
@@ -0,0 +1,53 @@
+/****************************************************************************
+ *   Copyright (C) 2017 Savoir-faire Linux                                  *
+ *   Author : Nicolas Jäger <nicolas.jager@savoirfairelinux.com>            *
+ *   Author : Sébastien Blin <sebastien.blin@savoirfairelinux.com>          *
+ *                                                                          *
+ *   This library is free software; you can redistribute it and/or          *
+ *   modify it under the terms of the GNU Lesser General Public             *
+ *   License as published by the Free Software Foundation; either           *
+ *   version 2.1 of the License, or (at your option) any later version.     *
+ *                                                                          *
+ *   This library is distributed in the hope that it will be useful,        *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of         *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU      *
+ *   Lesser General Public License for more details.                        *
+ *                                                                          *
+ *   You should have received a copy of the GNU General Public License      *
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.  *
+ ***************************************************************************/
+#pragma once
+
+// Std
+#include <memory>
+
+// Qt
+#include <qobject.h>
+
+// Lrc
+#include "typedefs.h"
+
+namespace lrc
+{
+
+class Lrc;
+
+class LIB_EXPORT CallbacksHandler : public QObject {
+    Q_OBJECT
+
+    friend class Lrc;
+
+public:
+    ~CallbacksHandler();
+
+private:
+    CallbacksHandler();
+
+public Q_SLOTS:
+    void slotNewAccountMessage(const QString& accountId, const QString& from, const QMap<QString,QString>& payloads);
+    void slotNewBuddySubscription(const QString& accountId, const QString& uri, bool status, const QString& message);
+};
+
+using upCallbacksHandler = std::unique_ptr<CallbacksHandler>;
+
+} // namespace lrc
---
+++ src/contactmodel.cpp
@@ -0,0 +1,122 @@
+/****************************************************************************
+ *   Copyright (C) 2017 Savoir-faire Linux                                  *
+ *   Author : Nicolas Jäger <nicolas.jager@savoirfairelinux.com>            *
+ *   Author : Sébastien Blin <sebastien.blin@savoirfairelinux.com>          *
+ *                                                                          *
+ *   This library is free software; you can redistribute it and/or          *
+ *   modify it under the terms of the GNU Lesser General Public             *
+ *   License as published by the Free Software Foundation; either           *
+ *   version 2.1 of the License, or (at your option) any later version.     *
+ *                                                                          *
+ *   This library is distributed in the hope that it will be useful,        *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of         *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU      *
+ *   Lesser General Public License for more details.                        *
+ *                                                                          *
+ *   You should have received a copy of the GNU General Public License      *
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.  *
+ ***************************************************************************/
+#include "contactmodel.h"
+
+// Dbus
+#include "dbus/configurationmanager.h"
+#include "dbus/presencemanager.h"
+
+// Lrc
+#include "availableaccountmodel.h"
+#include "contactmethod.h"
+
+namespace lrc
+{
+
+ContactModel::ContactModel(NewAccountModel& parent, const Database& db, const account::Info& info)
+: parent_(parent)
+, db_(db)
+, owner(info)
+, QObject()
+{
+
+}
+
+ContactModel::ContactModel(const ContactModel& contactModel)
+: contacts_(contactModel.contacts_)
+, db_(contactModel.db_)
+, parent_(contactModel.parent_)
+, owner(contactModel.owner)
+, QObject()
+{
+
+}
+
+ContactModel::~ContactModel()
+{
+
+}
+
+void
+ContactModel::addContact(const std::string& uri)
+{
+
+}
+
+void
+ContactModel::removeContact(const std::string& uri)
+{
+
+}
+
+void
+ContactModel::sendMessage(const std::string& uri, const std::string& body) const
+{
+
+}
+
+const contact::Info&
+ContactModel::getContact(const std::string& uri) const
+{
+    return contact::Info();
+}
+
+const ContactsInfoMap&
+ContactModel::getAllContacts() const
+{
+    return contacts_;
+}
+
+void
+ContactModel::nameLookup(const std::string& uri) const
+{
+
+}
+
+void
+ContactModel::addressLookup(const std::string& name) const
+{
+
+}
+
+void
+ContactModel::setContactPresent(const std::string& uri, bool status)
+{
+
+}
+
+bool
+ContactModel::fillsWithContacts()
+{
+    return false;
+}
+
+void
+ContactModel::slotContactsAdded(const QString &accountID, const QString &uri, bool confirmed)
+{
+
+}
+
+void
+ContactModel::slotContactsRemoved(const QString &accountID, const QString &uri, bool status)
+{
+
+}
+
+} // namespace lrc
