3e491866b978614363716f56e9628ead5cef054d
Nicolas Jager <nicolas.jager@savoirfairelinux.com>
2017-08-21 15:29:38 -0400

    Add stubs and class diagram
    add stubs for :
     - Database class.
     - ConversationModel class and Conversation namespace.
     - NewCallModel class and NewCall namespace.
     - ContactModel class and Contact namespace.
     - Message namespace.
     - NewAccountModel.
     - CallbacksHandler, will be used to bind the callbacks from the daemon
       and will emit signals for lrc and client.
     - Lrc class, this class is the root class of all models. Instating this
       class will build interfaces between the daemon, client and the
       database.
     - all these class are nested in the lrc namespace.
     - all publicly exposed API have been put into lrc::api namespace
     - add diagrams in doc folder
    
    [GR: ci msg additions]
    
    Change-Id: Ia06f874dd1baebb086edd570e52d8d61d73b905b
    Reviewed-by: Guillaume Roguez <guillaume.roguez@savoirfairelinux.com>
    Signed-off-by: Guillaume Roguez <guillaume.roguez@savoirfairelinux.com>

---
+++ src/database.cpp
@@ -0,0 +1,85 @@
+/****************************************************************************
+ *   Copyright (C) 2017 Savoir-faire Linux                                  *
+ *   Author : Nicolas Jäger <nicolas.jager@savoirfairelinux.com>            *
+ *   Author : Sébastien Blin <sebastien.blin@savoirfairelinux.com>          *
+ *                                                                          *
+ *   This library is free software; you can redistribute it and/or          *
+ *   modify it under the terms of the GNU Lesser General Public             *
+ *   License as published by the Free Software Foundation; either           *
+ *   version 2.1 of the License, or (at your option) any later version.     *
+ *                                                                          *
+ *   This library is distributed in the hope that it will be useful,        *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of         *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU      *
+ *   Lesser General Public License for more details.                        *
+ *                                                                          *
+ *   You should have received a copy of the GNU General Public License      *
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.  *
+ ***************************************************************************/
+#include "database.h"
+
+namespace lrc
+{
+
+using namespace api;
+
+Database::Database()
+: QObject()
+{
+
+}
+
+Database::~Database()
+{
+
+}
+
+void
+Database::addMessage(const std::string& accountId, const message::Info& message) const
+{
+
+}
+
+void
+Database::clearHistory(const std::string& accountId, const std::string& uid, bool removeContact) const
+{
+
+}
+
+MessagesMap
+Database::getHistory(const std::string& accountId, const std::string& uid) const
+{
+
+}
+
+std::size_t
+Database::numberOfUnreads(const std::string& accountId, const std::string& uid) const
+{
+
+}
+
+void
+Database::setMessageRead(int uid) const
+{
+
+}
+
+void
+Database::addContact(const std::string& contact, const QByteArray& payload) const
+{
+
+}
+
+std::string
+Database::getContactAttribute(const std::string& uid, const std::string& attribute) const
+{
+
+}
+
+void
+Database::slotRegisteredNameFound(Account const*, NameDirectory::LookupStatus, QString const&, QString const&) const
+{
+
+}
+
+} // namespace lrc
