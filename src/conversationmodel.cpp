29f18a5dd205a9ee76299f2ad497d4e3d9bda78a
Nicolas Jager <nicolas.jager@savoirfairelinux.com>
2017-08-21 15:29:38 -0400

    Add stubs and class diagram
    add stubs for :
     - DatabaseManager class.
     - ConversationModel class and Conversation namespace.
     - NewCallModel class and NewCall namespace.
     - ContactModel class and Contact namespace.
     - Message namespace.
    
    add diagram in doc/Diagram1.dia
    
    Change-Id: Ia06f874dd1baebb086edd570e52d8d61d73b905b

---
+++ src/conversationmodel.cpp
@@ -0,0 +1,90 @@
+/****************************************************************************
+ *   Copyright (C) 2017 Savoir-faire Linux                                  *
+ *   Author : Nicolas Jäger <nicolas.jager@savoirfairelinux.com>            *
+ *   Author : Sébastien Blin <sebastien.blin@savoirfairelinux.com>          *
+ *                                                                          *
+ *   This library is free software; you can redistribute it and/or          *
+ *   modify it under the terms of the GNU Lesser General Public             *
+ *   License as published by the Free Software Foundation; either           *
+ *   version 2.1 of the License, or (at your option) any later version.     *
+ *                                                                          *
+ *   This library is distributed in the hope that it will be useful,        *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of         *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU      *
+ *   Lesser General Public License for more details.                        *
+ *                                                                          *
+ *   You should have received a copy of the GNU General Public License      *
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.  *
+ ***************************************************************************/
+#include "conversationmodel.h"
+
+ConversationModel::ConversationModel(QObject* parent)
+:QObject(parent)
+{
+
+}
+
+ConversationModel::~ConversationModel()
+{
+
+}
+
+const Conversations&
+ConversationModel::getConversations() const
+{
+    return conversations_;
+}
+
+const Conversation::Info&
+ConversationModel::getConversation(const unsigned int row) const
+{
+    return Conversation::Info(nullptr, Contact::Info());
+}
+
+const Conversation::Info&
+ConversationModel::addConversation(const std::string& uri)
+{
+    return Conversation::Info(nullptr, Contact::Info());
+}
+
+void
+ConversationModel::selectConversation(const std::string& uid)
+{
+
+}
+
+void
+ConversationModel::removeConversation(const std::string& uid)
+{
+
+}
+
+void
+ConversationModel::placeCall(const std::string& uid) const
+{
+
+}
+
+void
+ConversationModel::sendMessage(const std::string& uid, const std::string& body) const
+{
+
+}
+
+void
+ConversationModel::setFilter(const std::string&)
+{
+
+}
+
+void
+ConversationModel::addParticipant(const std::string& uid, const::std::string& uri)
+{
+
+}
+
+void
+ConversationModel::cleanHistory(const std::string& uid)
+{
+
+}
