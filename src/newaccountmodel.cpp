7189192e83e13ed805f8fef51e075f509b458f04
Nicolas Jager <nicolas.jager@savoirfairelinux.com>
2017-08-25 09:55:18 -0400

    add getAccountModel() and fix CMakeList.txt
    Change-Id: I260a97e78de7d9e44cd85b1926de4540f8bd922b

---
+++ src/newaccountmodel.cpp
@@ -0,0 +1,50 @@
+/****************************************************************************
+ *   Copyright (C) 2017 Savoir-faire Linux                                  *
+ *   Author : Nicolas Jäger <nicolas.jager@savoirfairelinux.com>            *
+ *   Author : Sébastien Blin <sebastien.blin@savoirfairelinux.com>          *
+ *                                                                          *
+ *   This library is free software; you can redistribute it and/or          *
+ *   modify it under the terms of the GNU Lesser General Public             *
+ *   License as published by the Free Software Foundation; either           *
+ *   version 2.1 of the License, or (at your option) any later version.     *
+ *                                                                          *
+ *   This library is distributed in the hope that it will be useful,        *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of         *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU      *
+ *   Lesser General Public License for more details.                        *
+ *                                                                          *
+ *   You should have received a copy of the GNU General Public License      *
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.  *
+ ***************************************************************************/
+#include "newaccountmodel.h"
+
+#include "dbus/configurationmanager.h" // old
+
+NewAccountModel::NewAccountModel(pDatabaseManager dbManager)
+:QObject(nullptr)
+, dbManager_(dbManager)
+{
+    const QStringList accountIds = configurationManager.getAccountList();
+
+    for (auto id : accountIds) {
+        // first we build all objects contained in the info structure
+        auto callModel = std::make_shared<NewCallModel>(NewCallModel());
+        auto contactModel = std::make_shared<ContactModel>(ContactModel(dbManager_, id.toStdString()));
+        auto conversationModel = std::make_shared<ConversationModel>(ConversationModel());
+        
+        auto info = NewAccount::Info(id.toStdString(), callModel, contactModel, conversationModel);
+        accounts[id] = info;
+    }
+
+}
+
+NewAccountModel::~NewAccountModel()
+{
+
+}
+
+pAccountInfo
+NewAccountModel::getAccountInfo(const std::string& id)
+{
+    return accounts_[id];
+}
