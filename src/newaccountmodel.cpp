6fdf099c8432da68a374fd9a6685443d013e3f44
Sébastien Blin <sebastien.blin@savoirfairelinux.com>
2017-08-28 15:37:37 -0400

    contactmodel: implement model methods
    Fill model with daemon's contacts and add the ability to manage contacts
    from this model and send messages.
    
    /!\ il y a du code a bouger -> chercher [jn] dans contactmodel.cpp
    
    /!\ du code a etait edite dans la fonction fillsWithRINGContacts,
        il faut tester cette fonction.
    
    Change-Id: I79cc1f6bfb6327d92b9f8cd117531c4c194f932d

--- src/newaccountmodel.cpp
+++ src/newaccountmodel.cpp
@@ -197,7 +197,7 @@ NewAccountModelPimpl::addToAccounts(const std::string& accountId)
     addAcountProfileInDb(owner);
     // Init models for this account
     owner.callModel = std::make_unique<NewCallModel>(owner);
-    owner.contactModel = std::make_unique<ContactModel>(owner, database);
+    owner.contactModel = std::make_unique<ContactModel>(owner, database, callbacksHandler);
     owner.conversationModel = std::make_unique<ConversationModel>(owner, database);
     owner.accountModel = &linked;
 }
