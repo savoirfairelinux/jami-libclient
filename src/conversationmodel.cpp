e111a0e4c3e5f10cca3da578b369d731cf99e9a6
Sébastien Blin <sebastien.blin@savoirfairelinux.com>
2017-08-28 16:45:11 -0400

    conversationmodel: add the ability to manage conversations
    Change-Id: I9a22f88afa0add2adaec8047dae5361cee453846

--- src/conversationmodel.cpp
+++ src/conversationmodel.cpp
@@ -22,31 +22,43 @@
 #include <regex>
 
 // LRC
+#include "api/contactmodel.h"
 #include "api/newcallmodel.h"
 #include "api/newaccountmodel.h"
 #include "api/account.h"
-
+#include "callbackshandler.h"
 #include "database.h"
+
+#include "availableaccountmodel.h"
 #include "namedirectory.h"
+
+// Dbus
+#include "dbus/configurationmanager.h"
 
 namespace lrc
 {
 
 using namespace api;
+//~ using ConversationQueue = std::deque<conversation::Info>;
 
 class ConversationModelPimpl : public QObject
 {
     Q_OBJECT
 public:
-    ConversationModelPimpl(const ConversationModel& linked, const Database& db);
+    //~ using ConversationQueue = std::deque<conversation::Info>;
+
+    ConversationModelPimpl(const ConversationModel& linked,
+                           const Database& db,
+                           const CallbacksHandler& callbacksHandler);
+
     ~ConversationModelPimpl();
 
     /**
-     * Search a conversation in conversations_
-     * @param uid the contact to search
-     * @return the index in conversations_
+     * return a conversation index from conversations or -1 if no index is found.
+     * @param uid of the contact to search.
+     * @return an int.
      */
-    int find(const std::string& uid) const;
+    int indexOf(const std::string& uid) const;
     /**
      * Initialize conversations_ and filteredConversations_
      */
@@ -56,9 +68,11 @@ public:
      */
     void sortConversations();
     void search();
+    int find(const std::string& uid) const;
 
     const ConversationModel& linked;
     const Database& db;
+    const CallbacksHandler& callbacksHandler;
 
     ConversationModel::ConversationQueue conversations;
     mutable ConversationModel::ConversationQueue filteredConversations;
@@ -72,9 +86,9 @@ public Q_SLOTS:
 
 };
 
-ConversationModel::ConversationModel(const account::Info& owner, const Database& database)
+ConversationModel::ConversationModel(const account::Info& owner, const Database& database, const CallbacksHandler& callbacksHandler)
 : QObject()
-, pimpl_(std::make_unique<ConversationModelPimpl>(*this, database))
+, pimpl_(std::make_unique<ConversationModelPimpl>(*this, database, callbacksHandler))
 , owner(owner)
 {
 
@@ -94,25 +108,96 @@ ConversationModel::getFilteredConversations() const
 conversation::Info
 ConversationModel::getConversation(const unsigned int row) const
 {
-    return {};
+    if (row >= pimpl_->filteredConversations.size())
+        return conversation::Info();
+    return pimpl_->filteredConversations.at(row);
 }
 
 void
-ConversationModel::addConversation(const std::string& uri) const
+ConversationModel::addConversation(const std::string& uid) const
 {
+    auto conversationIdx = pimpl_->indexOf(uid);
+
+    if (conversationIdx == -1)
+        return;
+
+    auto conversation = pimpl_->conversations.at(conversationIdx);
+
+    if (conversation.participants.empty())
+        return;
+
+    auto contact = conversation.participants.front();
+
+    // Send contact request if non used
+    if (not conversation.isUsed) {
+        if (contact.length() == 0)
+            return;
+
+        owner.contactModel->addContact(contact);
+    }
 
 }
 
 void
 ConversationModel::selectConversation(const std::string& uid)
 {
+    // Get conversation
+    auto conversationIdx = pimpl_->indexOf(uid);
 
+    if (conversationIdx == -1)
+        return;
+
+    auto conversation = pimpl_->conversations.at(conversationIdx);
+    auto participants = conversation.participants;
+
+    // Check if conversation has a valid contact.
+    if (participants.empty() || participants.front().empty())
+        return;
+
+    emit showChatView(conversation);
+    /* TODO
+    if (conversation.call.status == call::Status::INVALID) {
+        emit showChatView(conversation);
+        return;
+    }
+    switch (conversation.call.status) {
+    case call::Status::INCOMING_RINGING:
+    case call::Status::OUTGOING_RINGING:
+    case call::Status::CONNECTING:
+    case call::Status::SEARCHING:
+            // We are currently in a call
+            emit showIncomingCallView(conversation);
+            break;
+        case call::Status::IN_PROGRESS:
+            // We are currently receiving a call
+            emit showCallView(conversation);
+            break;
+        default:
+            // We are not in a call, show the chatview
+            emit showChatView(conversation);
+    }*/
 }
 
 void
 ConversationModel::removeConversation(const std::string& uid)
 {
+    // Get conversation
+    auto i = std::find_if(pimpl_->conversations.begin(), pimpl_->conversations.end(),
+    [uid](const conversation::Info& conversation) {
+      return conversation.uid == uid;
+    });
 
+    if (i == pimpl_->conversations.end())
+        return;
+
+    auto conversation = *i;
+
+    if (conversation.participants.empty())
+        return;
+
+    // Remove contact from daemon
+    auto contact = conversation.participants.front();
+    owner.contactModel->removeContact(contact);
 }
 
 void
@@ -123,7 +208,42 @@ ConversationModel::placeCall(const std::string& uid) const
 void
 ConversationModel::sendMessage(const std::string& uid, const std::string& body) const
 {
+    auto conversationIdx = pimpl_->indexOf(uid);
 
+    if (conversationIdx == -1)
+        return;
+
+    auto conversation = pimpl_->conversations.at(conversationIdx);
+
+    if (conversation.participants.empty())
+        return;
+
+    auto contact = conversation.participants.front();
+    auto account = AvailableAccountModel::instance().currentDefaultAccount(); // TODO replace by linked account
+
+    if (!account || contact.length() == 0)
+        return;
+
+    // Send contact request if non used
+    if (!conversation.isUsed)
+        addConversation(contact);
+
+    // Send message to contact.
+    QMap<QString, QString> payloads;
+    payloads["text/plain"] = body.c_str();
+
+    // TODO change this for group messages
+    auto id = ConfigurationManager::instance().sendTextMessage(account->id(),
+    contact.c_str(), payloads);
+
+    message::Info msg;
+    msg.contact = contact.c_str();
+    msg.body = body;
+    msg.timestamp = std::time(nullptr);
+    msg.type = message::Type::TEXT;
+    msg.status = message::Status::SENDING;
+
+    //~ pimpl_->database.addMessage(account->id().toStdString(), msg);
 }
 
 void
@@ -144,12 +264,19 @@ ConversationModel::clearHistory(const std::string& uid)
 
 }
 
-ConversationModelPimpl::ConversationModelPimpl(const ConversationModel& linked, const Database& db)
+ConversationModelPimpl::ConversationModelPimpl(const ConversationModel& linked,
+                                               const Database& db,
+                                               const CallbacksHandler& callbacksHandler)
 : linked(linked)
 , db(db)
+, callbacksHandler(callbacksHandler)
 {
-
+    initConversations();
+    //~ connect(&database, &Database::messageAdded, this, &ConversationModelPimpl::slotMessageAdded);
+    //~ connect(&*linked.owner.contactModel, &ContactModel::modelUpdated,
+            //~ this, &ConversationModelPimpl::slotContactModelUpdated);
 }
+
 
 ConversationModelPimpl::~ConversationModelPimpl()
 {
@@ -171,7 +298,31 @@ ConversationModelPimpl::search()
 void
 ConversationModelPimpl::initConversations()
 {
+    auto account = AvailableAccountModel::instance().currentDefaultAccount();
 
+    if (!account)
+        return;
+
+    conversations.clear();
+    // Fill conversations
+    for (auto const& contact : linked.owner.contactModel->getAllContacts())
+    {
+        auto contactinfo = contact;
+        conversation::Info conversation;
+        conversation.uid = contactinfo.second->uri;
+        conversation.participants.emplace_back((*contactinfo.second.get()).uri);
+        auto messages = std::map<int, message::Info>(); //database.getHistory(account->id().toStdString(),
+                                             //contactinfo.second->uri);
+        conversation.messages = messages;
+        if (!messages.empty()) {
+            conversation.lastMessageUid = (--messages.end())->first;
+            conversation.isUsed = true;
+        }
+        conversation.accountId = account->id().toStdString();
+        conversations.emplace_front(conversation);
+    }
+    sortConversations();
+    filteredConversations = conversations;
 }
 
 void
@@ -189,15 +340,71 @@ ConversationModelPimpl::slotContactsChanged()
 void
 ConversationModelPimpl::slotMessageAdded(int uid, const std::string& account, const message::Info& msg)
 {
+    auto conversationIdx = indexOf(msg.contact);
 
+    if (conversationIdx == -1)
+        return;
+
+    auto conversation = conversations.at(conversationIdx);
+
+    if (conversation.participants.empty())
+        return;
+
+    if (!conversation.isUsed)
+        conversation.isUsed = true;
+
+    // Add message to conversation
+    conversation.messages.insert(std::pair<int, message::Info>(uid, msg));
+    conversation.lastMessageUid = uid;
+
+    emit linked.newMessageAdded(msg.contact, msg);
+
+    sortConversations();
+    emit linked.modelUpdated();
 }
 
 void
 ConversationModelPimpl::slotRegisteredNameFound(const Account* account, NameDirectory::LookupStatus status,
                                            const QString& address, const QString& name)
 {
-
+    std::sort(conversations.begin(), conversations.end(),
+    [](const conversation::Info& conversationA, const conversation::Info& conversationB)
+    {
+        auto historyA = conversationA.messages;
+        auto historyB = conversationB.messages;
+        // A or B is a new conversation (without INVITE message)
+        if (historyA.empty()) return true;
+        if (historyB.empty()) return false;
+        // Sort by last Interaction
+        try
+        {
+            auto lastMessageA = historyA.at(conversationA.lastMessageUid);
+            auto lastMessageB = historyB.at(conversationB.lastMessageUid);
+            return lastMessageA.timestamp > lastMessageB.timestamp;
+        }
+        catch (const std::exception& e)
+        {
+            qDebug() << "ConversationModel::sortConversations(), can't get lastMessage";
+            return true;
+        }
+    });
 }
+
+int
+ConversationModelPimpl::indexOf(const std::string& uid) const
+{
+    for (unsigned int i = 0; i < conversations.size() ; ++i) {
+        if(conversations.at(i).uid == uid) return i;
+    }
+    return -1;
+}
+
+//~ void
+//~ ConversationModelPimpl::slotContactModelUpdated()
+//~ {
+    //~ initConversations();
+    //~ emit linked.modelUpdated();
+//~ }
 
 } // namespace lrc
 
