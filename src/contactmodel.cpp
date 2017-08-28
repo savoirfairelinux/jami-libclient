16ac2c71dc2705a046d46ae4313bf5939fe7180b
Sébastien Blin <sebastien.blin@savoirfairelinux.com>
2017-08-28 15:37:37 -0400

    contactmodel: implement model methods
    Fill model with daemon's contacts and add the ability to manage contacts
    from this model and send messages.
    
    Change-Id: I766f526734e3fbd0d3c30f9094ff44323b1ea0b9

--- src/contactmodel.cpp
+++ src/contactmodel.cpp
@@ -18,10 +18,14 @@
  ***************************************************************************/
 #include "contactmodel.h"
 
+// std
+#include <stdexcept>
+
+// lrc
 #include "availableaccountmodel.h"
 #include "dbus/configurationmanager.h"
-#include "dbus/presencemanager.h"
 #include "contactmethod.h"
+#include "phonedirectorymodel.h"
 
 namespace lrc
 {
@@ -29,6 +33,13 @@ namespace lrc
 ContactModel::ContactModel(const Database& db, const std::string& accountId)
 : QObject(), db_(db), accountId_(accountId)
 {
+    fillsWithContacts();
+
+    // TODO move into LRC
+    connect(&ConfigurationManager::instance(), &ConfigurationManagerInterface::contactAdded, this,
+            &ContactModel::slotContactsAdded);
+    connect(&ConfigurationManager::instance(), &ConfigurationManagerInterface::contactRemoved, this,
+            &ContactModel::slotContactsRemoved);
 
 }
 
@@ -40,25 +51,47 @@ ContactModel::~ContactModel()
 void
 ContactModel::addContact(const std::string& uri)
 {
-
+    // Add contact to daemon
+    ConfigurationManager::instance().addContact(QString(accountId_.c_str()),
+    QString(uri.c_str()));
+    // TODO connect signal from LRC
 }
 
 void
 ContactModel::removeContact(const std::string& uri)
 {
-
+    // Remove contact from daemon contacts
+    ConfigurationManager::instance().removeContact(QString(accountId_.c_str()), QString(uri.c_str()), false);
+    // TODO do this when daemon emit contactRemoved
+    contacts_.erase(uri);
 }
 
 void
 ContactModel::sendMessage(const std::string& uri, const std::string& body) const
 {
+    // Send message
+    QMap<QString, QString> payloads;
+    payloads["text/plain"] = body.c_str();
+    unsigned int id = ConfigurationManager::instance().sendTextMessage(QString(accountId_.c_str()), uri.c_str(), payloads);
 
+    // Store it into the database
+    message::Info msg;
+    msg.uid = std::to_string(id);
+    msg.body = body;
+    msg.timestamp = std::time(nullptr);
+    msg.type = message::Type::TEXT;
+    msg.status = message::Status::SENDING;
+    db_.addMessage(accountId_, msg);
 }
 
 const contact::Info&
 ContactModel::getContact(const std::string& uri)
 {
-    return contact::Info();
+    auto contact = contacts_.find(uri);
+    if (contact == contacts_.end()) {
+        throw std::out_of_range("ContactModel::getContact, invalid uri");
+    }
+    return *contact->second.get();
 }
 
 const ContactsInfoMap&
@@ -70,37 +103,110 @@ ContactModel::getAllContacts() const
 void
 ContactModel::nameLookup(const std::string& uri) const
 {
-
+    // TODO
 }
 
 void
 ContactModel::addressLookup(const std::string& name) const
 {
-
+    // TODO
 }
 
 void
 ContactModel::setContactPresent(const std::string& uri, bool status)
 {
-
+    if (contacts_.find(uri) != contacts_.end()) {
+        contacts_[uri]->isPresent = status;
+    }
 }
 
 bool
 ContactModel::fillsWithContacts()
 {
-    return false;
+    auto account = AccountModel::instance().getById(accountId_.c_str());
+    if (not account) {
+        qDebug() << "ContactModel::fillsWithContacts(), nullptr";
+    }
+    // TODO improve this
+    if (account->protocol() != Account::Protocol::RING) {
+        qDebug() << "fillsWithContacts, account is not a RING account";
+        return false;
+    }
+
+    auto contacts = account->getContacts();
+
+    // Clear the list
+    contacts_.clear();
+
+    auto type = contact::Type::RING;
+
+    // Add contacts to the list
+    for (auto c : contacts) {
+        auto uri = c->uri().toStdString();
+        auto avatar = db_.getContactAttribute(uri, "photo");
+        auto registeredName = c->registeredName().toStdString();
+        auto alias = c->bestName().toStdString();
+        auto isTrusted = false; // TODO: handle trust
+        auto isPresent = c->isPresent();
+
+        auto contact = std::make_shared<contact::Info>();
+        contact->uri = uri;
+        contact->avatar = avatar;
+        contact->registeredName = registeredName;
+        contact->alias = alias;
+        contact->isTrusted = isTrusted;
+        contact->isPresent = isPresent;
+        contact->type = type;
+
+        contacts_[uri] = contact;
+    }
+
+    return true;
 }
 
 void
 ContactModel::slotContactsAdded(const QString &accountID, const QString &uri, bool confirmed)
 {
+    auto account = AccountModel::instance().getById(accountId_.c_str());
+    if (not account) {
+        qDebug() << "ContactModel::slotContactsAdded(), nullptr";
+    }
+    // TODO disconnect signal from LRC
+    if (contacts_.find(uri.toStdString()) == contacts_.end()) {
+        auto cm = PhoneDirectoryModel::instance().getNumber(uri, account);
+        auto contact = std::make_shared<contact::Info>();
+        contact->uri = uri.toStdString();
+        contact->avatar = db_.getContactAttribute(uri.toStdString(), "photo");
+        contact->registeredName = cm->registeredName().toStdString();
+        contact->alias =  cm->bestName().toStdString();
+        contact->isTrusted = false;
+        contact->isPresent = cm->isPresent();
+        contact->type = contact::Type::RING; // TODO
 
+        contacts_[uri.toStdString()] = contact;
+
+        // Add to database
+        message::Info msg;
+        msg.uid = uri.toStdString();
+        msg.body = "";
+        msg.timestamp = std::time(nullptr);
+        msg.type = message::Type::CONTACT;
+        msg.status = message::Status::SUCCEED;
+        db_.addMessage(accountId_, msg);
+        emit contactsChanged();
+    }
 }
 
 void
 ContactModel::slotContactsRemoved(const QString &accountID, const QString &uri, bool status)
 {
-
+    if (accountId_ != accountID.toStdString()) return;
+    // TODO disconnect signal from LRC
+    if (contacts_.find(uri.toStdString()) != contacts_.end()) {
+        db_.clearHistory(accountId_, uri.toStdString(), true);
+        contacts_.erase(uri.toStdString());
+        emit contactsChanged();
+    }
 }
 
 } // namespace lrc
