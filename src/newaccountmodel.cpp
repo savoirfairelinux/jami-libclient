68d44fae3317b717204493b872c29462c1637641
Sébastien Blin <sebastien.blin@savoirfairelinux.com>
2017-08-28 16:45:11 -0400

    conversationmodel: add the ability to manage conversations
    Change-Id: I9a22f88afa0add2adaec8047dae5361cee453846

--- src/newaccountmodel.cpp
+++ src/newaccountmodel.cpp
@@ -104,7 +104,7 @@ NewAccountModelPimpl::NewAccountModelPimpl(NewAccountModel& linked,
         owner.profile.type = details["Account.type"] == "RING" ? contact::Type::RING : contact::Type::SIP;
         owner.callModel = std::make_unique<NewCallModel>(owner);
         owner.contactModel = std::make_unique<ContactModel>(owner, database, callbacksHandler);
-        owner.conversationModel = std::make_unique<ConversationModel>(owner, database);
+        owner.conversationModel = std::make_unique<ConversationModel>(owner, database, callbacksHandler);
         owner.accountModel = &linked;
         addAcountProfileInDb(owner);
     }
