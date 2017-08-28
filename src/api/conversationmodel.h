bd897d76c3e1d8b329ccbcf9852f57fdb474a140
Sébastien Blin <sebastien.blin@savoirfairelinux.com>
2017-08-28 16:45:11 -0400

    conversationmodel: add the ability to manage conversations
    Change-Id: I9a22f88afa0add2adaec8047dae5361cee453846

--- src/api/conversationmodel.h
+++ src/api/conversationmodel.h
@@ -32,12 +32,14 @@
 
 // Data
 #include "api/conversation.h"
+#include "api/contact.h"
 
 namespace lrc
 {
 
-class Database;
+class CallbacksHandler;
 class ConversationModelPimpl;
+class Database;
 
 namespace api
 {
@@ -53,30 +55,31 @@ public:
 
     const account::Info& owner;
 
-    ConversationModel(const account::Info& owner, const Database& database);
+    ConversationModel(const account::Info& owner, Database& db, const CallbacksHandler& callbacksHandler);
     ~ConversationModel();
 
     const ConversationQueue& getFilteredConversations() const;
     conversation::Info getConversation(unsigned int row) const;
-    void addConversation(const std::string& uri) const;
-    void removeConversation(const std::string& uid);
+    void addConversation(const std::string& uid) const;
+    void removeConversation(const std::string& uid, bool banned=false);
     void selectConversation(const std::string& uid);
     void placeCall(const std::string& uid) const;
     void sendMessage(const std::string& uid, const std::string& body) const;
     void setFilter(const std::string& filter);
+    void setFilter(const contact::Type& filter = lrc::api::contact::Type::INVALID);
     void addParticipant(const std::string& uid, const::std::string& uri);
     void clearHistory(const std::string& uid);
 
 Q_SIGNALS:
-    void newMessageAdded(const std::string& uid, const message::Info& msg);
+    void newMessageAdded(const std::string& uid, const message::Info& msg) const;
     void conversationUpdated(unsigned int row);
     void modelUpdated() const;
     void newContactAdded(const std::string& uri);
     void incomingCallFromItem(const unsigned int row);
 
-    void showChatView(const conversation::Info& conversationInfo);
-    void showCallView(const conversation::Info& conversationInfo);
-    void showIncomingCallView(const conversation::Info& conversationInfo);
+    void showChatView(const conversation::Info& conversationInfo); // TO MOVE
+    void showCallView(const conversation::Info& conversationInfo);// TO MOVE
+    void showIncomingCallView(const conversation::Info& conversationInfo);// TO MOVE
 
 private:
     std::unique_ptr<ConversationModelPimpl> pimpl_;
