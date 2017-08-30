ce24a527ca895f39332dcfd4870fddcfcc56ec46
Nicolas Jager <nicolas.jager@savoirfairelinux.com>
2017-08-30 09:49:16 -0400

    travaux sur les call
    Change-Id: If7abae0c6dbaf27acf6b97953e042f93c1ef5d5f

--- src/conversationmodel.cpp
+++ src/conversationmodel.cpp
@@ -84,6 +84,7 @@ public Q_SLOTS:
     void slotMessageAdded(int uid, const std::string& accountId, const message::Info& msg);
     void registeredNameFound(const Account* account, NameDirectory::LookupStatus status,
                              const QString& address, const QString& name);
+    void slotIncomingCall(const std::string& callId, const std::string& fromId);
 
 };
 
@@ -153,7 +154,7 @@ ConversationModel::addConversation(const std::string& uid) const
     if (conversationIdx == -1)
         return;
 
-    auto conversation = pimpl_->conversations.at(conversationIdx);
+    auto& conversation = pimpl_->conversations.at(conversationIdx);
 
     if (conversation.participants.empty())
         return;
@@ -180,35 +181,44 @@ ConversationModel::selectConversation(const std::string& uid)
     if (conversationIdx == -1)
         return;
 
-    auto conversation = pimpl_->conversations.at(conversationIdx);
+    auto& conversation = pimpl_->conversations.at(conversationIdx);
     auto participants = conversation.participants;
 
     // Check if conversation has a valid contact.
     if (participants.empty())
         return;
 
-    emit showChatView(conversation);
-    /* TODO
-    if (conversation.call.status == call::Status::INVALID) {
+    //emit showChatView(conversation);
+    try  {
+        auto call = owner.callModel->getCall(conversation.callId);
+        switch (call.status) {
+            case call::Status::INCOMING_RINGING:
+            case call::Status::OUTGOING_RINGING:
+            case call::Status::CONNECTING:
+            case call::Status::SEARCHING:
+                // We are currently in a call
+                emit showIncomingCallView(conversation);
+                break;
+            case call::Status::PAUSED:
+            case call::Status::PEER_PAUSED:
+            case call::Status::CONNECTED:
+            case call::Status::IN_PROGRESS:
+                // We are currently receiving a call
+                emit showCallView(conversation);
+                break;
+            case call::Status::INVALID:
+            case call::Status::OUTGOING_REQUESTED:
+            case call::Status::INACTIVE:
+            case call::Status::ENDED:
+            case call::Status::TERMINATING:
+            case call::Status::AUTO_ANSWERING:
+            default:
+                // We are not in a call, show the chatview
+                emit showChatView(conversation);
+        }
+    } catch (const std::out_of_range&) {
         emit showChatView(conversation);
-        return;
     }
-    switch (conversation.call.status) {
-    case call::Status::INCOMING_RINGING:
-    case call::Status::OUTGOING_RINGING:
-    case call::Status::CONNECTING:
-    case call::Status::SEARCHING:
-            // We are currently in a call
-            emit showIncomingCallView(conversation);
-            break;
-        case call::Status::IN_PROGRESS:
-            // We are currently receiving a call
-            emit showCallView(conversation);
-            break;
-        default:
-            // We are not in a call, show the chatview
-            emit showChatView(conversation);
-    }*/
 }
 
 void
@@ -223,7 +233,7 @@ ConversationModel::removeConversation(const std::string& uid, bool banned)
     if (i == pimpl_->conversations.end())
         return;
 
-    auto conversation = *i;
+    auto& conversation = *i;
 
     if (conversation.participants.empty())
         return;
@@ -234,8 +244,21 @@ ConversationModel::removeConversation(const std::string& uid, bool banned)
 }
 
 void
-ConversationModel::placeCall(const std::string& uid) const
+ConversationModel::placeCall(const std::string& uid)
 {
+    auto conversationIdx = pimpl_->indexOf(uid);
+
+    if (conversationIdx == -1)
+        return;
+
+    auto& conversation = pimpl_->conversations.at(conversationIdx);
+
+    auto url = conversation.participants.front();
+    if (owner.contact.type == contact::Type::RING) {
+        url = "ring:" + url;
+    }
+    conversation.callId = owner.callModel->createCall(url);
+    emit showIncomingCallView(conversation);
 }
 
 void
@@ -246,7 +269,7 @@ ConversationModel::sendMessage(const std::string& uid, const std::string& body) 
     if (conversationIdx == -1)
         return;
 
-    auto conversation = pimpl_->conversations.at(conversationIdx);
+    auto& conversation = pimpl_->conversations.at(conversationIdx);
     if (conversation.participants.empty())
         return;
 
@@ -388,6 +411,8 @@ ConversationModelPimpl::ConversationModelPimpl(const ConversationModel& linked,
     connect(&database, &Database::messageAdded, this, &ConversationModelPimpl::slotMessageAdded);
     connect(&*linked.owner.contactModel, &ContactModel::modelUpdated,
             this, &ConversationModelPimpl::slotContactModelUpdated);
+    connect(&*linked.owner.callModel, &NewCallModel::newIncomingCall,
+            this, &ConversationModelPimpl::slotIncomingCall);
 }
 
 ConversationModelPimpl::~ConversationModelPimpl()
@@ -583,6 +608,22 @@ ConversationModelPimpl::slotContactModelUpdated()
 {
     initConversations();
     emit linked.modelUpdated();
+}
+
+void
+ConversationModelPimpl::slotIncomingCall(const std::string& fromId, const std::string& callId)
+{
+    auto conversationIdx = indexOf(fromId);
+
+    if (conversationIdx != -1) {
+        auto& conversation = conversations.at(conversationIdx);
+
+        qDebug() << "Add call to conversation with " << fromId.c_str();
+        conversation.callId = callId;
+        emit linked.showIncomingCallView(conversation);
+    } else {
+        // TODO we receive a conversation from a non contact. How do we want to show this?
+    }
 }
 
 } // namespace api
--- src/newcallmodel.cpp
+++ src/newcallmodel.cpp
@@ -16,9 +16,22 @@
  *   You should have received a copy of the GNU General Public License      *
  *   along with this program.  If not, see <http://www.gnu.org/licenses/>.  *
  ***************************************************************************/
+
 #include "api/newcallmodel.h"
 
+#include "callbackshandler.h"
+#include "api/conversationmodel.h"
 #include "api/newaccountmodel.h"
+#include "dbus/callmanager.h"
+#include "private/videorenderermanager.h"
+#include "video/renderer.h"
+
+// Ring daemon
+#include <media_const.h>
+
+// Qt
+#include <QObject>
+#include <QString>
 
 namespace lrc
 {
@@ -26,70 +39,180 @@ namespace lrc
 namespace api
 {
 
-class NewCallModelPimpl
+class NewCallModelPimpl: public QObject
 {
 public:
-    NewCallModelPimpl(NewAccountModel& parent);
+    NewCallModelPimpl(const NewCallModel& linked, NewAccountModel& parent, const CallbacksHandler& callbackHandler);
     ~NewCallModelPimpl();
 
     void sendMessage(const std::string& callId, const std::string& body) const;
 
     CallInfoMap calls;
     NewAccountModel& parent;
+    const CallbacksHandler& callbackHandler;
+    const NewCallModel& linked;
+
+public Q_SLOTS:
+    void slotIncomingCall(const std::string& accountId, const std::string& callId, const std::string& fromId);
+    void slotCallStateChanged(const std::string& callId, const std::string &state, int code);
+
 };
 
-NewCallModel::NewCallModel(NewAccountModel& parent, const account::Info& info)
+NewCallModel::NewCallModel(NewAccountModel& parent, const CallbacksHandler& callbackHandler, const account::Info& info)
 : owner(info)
-, pimpl_(std::make_unique<NewCallModelPimpl>(parent))
+, pimpl_(std::make_unique<NewCallModelPimpl>(*this, parent, callbackHandler))
 {
 
 }
 
 NewCallModel::~NewCallModel()
 {
-
 }
 
 const call::Info&
-NewCallModel::createCall(const std::string& contactUri)
+NewCallModel::getCall(const std::string& uid) const
 {
-
+    if (pimpl_->calls.find(uid) == pimpl_->calls.end()) {
+        throw std::out_of_range("No call at UID " + uid);
+    }
+    return *pimpl_->calls[uid];
 }
+
+std::string
+NewCallModel::humanReadableStatus(const call::Status& status)
+{
+    switch(status)
+    {
+    case call::Status::PAUSED:
+        return tr("Hold").toStdString();
+    case call::Status::IN_PROGRESS:
+        return tr("Talking").toStdString();
+    case call::Status::INVALID:
+        return tr("ERROR").toStdString();
+    case call::Status::OUTGOING_REQUESTED:
+        return tr("Outgoing requested").toStdString();
+    case call::Status::INCOMING_RINGING:
+        return tr("Incoming").toStdString();
+    case call::Status::OUTGOING_RINGING:
+        return tr("Calling").toStdString();
+    case call::Status::CONNECTING:
+        return tr("Connecting").toStdString();
+    case call::Status::SEARCHING:
+        return tr("Searching").toStdString();
+    case call::Status::PEER_PAUSED:
+        return tr("Hold").toStdString();
+    case call::Status::INACTIVE:
+        return tr("Inactive").toStdString();
+    case call::Status::ENDED:
+        return tr("Finished").toStdString();
+    case call::Status::TERMINATING:
+        return tr("Finished").toStdString();
+    case call::Status::CONNECTED:
+        return tr("Communication established").toStdString();
+    case call::Status::AUTO_ANSWERING:
+        return tr("Auto answering").toStdString();
+        break;
+    }
+}
+
+const std::string
+NewCallModel::createCall(const std::string& url)
+{
+    // do not use auto here (QDBusPendingReply<QString>)
+    QString callId = CallManager::instance().placeCall(owner.id.c_str(), url.c_str());
+
+    if (callId.isEmpty())
+        qDebug() << "no call placed between (account :" << owner.id.c_str() << ", contact :" << url.c_str() << ")";
+
+    auto callInfo = std::make_shared<call::Info>();
+    callInfo->id = callId.toStdString();
+    callInfo->status =  call::Status::SEARCHING;
+    pimpl_->calls[callId.toStdString()] = callInfo;
+
+    return callId.toStdString();
+}
+
+void
+NewCallModel::accept(const std::string& callId) const
+{
+    CallManager::instance().accept(callId.c_str());
+}
+
 
 void
 NewCallModel::hangUp(const std::string& callId) const
 {
-
+    CallManager::instance().hangUp(callId.c_str());
 }
 
 void
 NewCallModel::togglePause(const std::string& callId) const
 {
-
+    auto call = pimpl_->calls.find(callId);
+    if (call == pimpl_->calls.end()) return;
+    switch(pimpl_->calls[callId]->status)
+    {
+    case call::Status::PAUSED:
+        CallManager::instance().unhold(callId.c_str());
+        break;
+    case call::Status::IN_PROGRESS:
+        CallManager::instance().hold(callId.c_str());
+        break;
+    case call::Status::INVALID:
+    case call::Status::OUTGOING_REQUESTED:
+    case call::Status::INCOMING_RINGING:
+    case call::Status::OUTGOING_RINGING:
+    case call::Status::CONNECTING:
+    case call::Status::SEARCHING:
+    case call::Status::PEER_PAUSED:
+    case call::Status::INACTIVE:
+    case call::Status::ENDED:
+    case call::Status::TERMINATING:
+    case call::Status::CONNECTED:
+    case call::Status::AUTO_ANSWERING:
+        break;
+    }
 }
 
 void
-NewCallModel::toggleMedia(const std::string& callId, const Media media) const
+NewCallModel::toggleMedia(const std::string& callId, const NewCallModel::Media media, bool flag) const
 {
+    switch(media)
+    {
+    case NewCallModel::Media::AUDIO:
+        CallManager::instance().muteLocalMedia(callId.c_str(),
+                                               DRing::Media::Details::MEDIA_TYPE_AUDIO,
+                                               flag);
+        break;
 
+    case NewCallModel::Media::VIDEO:
+        CallManager::instance().muteLocalMedia(callId.c_str(),
+                                               DRing::Media::Details::MEDIA_TYPE_AUDIO,
+                                               flag);
+        break;
+
+    case NewCallModel::Media::NONE:
+    default:
+        break;
+    }
 }
 
 void
 NewCallModel::toggleRecoringdAudio(const std::string& callId) const
 {
-
+    CallManager::instance().toggleRecording(callId.c_str());
 }
 
 void
 NewCallModel::setQuality(const std::string& callId, const double quality) const
 {
-
+    qDebug() << "setQuality, isn't yet implemented";
 }
 
 void
 NewCallModel::transfer(const std::string& callId, const std::string& to) const
 {
-
+    qDebug() << "transfer, isn't yet implemented";
 }
 
 void
@@ -104,10 +227,23 @@ NewCallModel::removeParticipant(const std::string& callId, const std::string& pa
 
 }
 
-NewCallModelPimpl::NewCallModelPimpl(NewAccountModel& p)
-: parent(p)
+Video::Renderer*
+NewCallModel::getRenderer(const std::string& callId) const
 {
+   #ifdef ENABLE_VIDEO
+   return VideoRendererManager::instance().getRenderer(callId);
+   #else
+   return nullptr;
+   #endif
+}
 
+NewCallModelPimpl::NewCallModelPimpl(const NewCallModel& linked, NewAccountModel& p, const CallbacksHandler& callbackHandler)
+: linked(linked)
+, parent(p)
+, callbackHandler(callbackHandler)
+{
+    connect(&callbackHandler, &CallbacksHandler::incomingCall, this, &NewCallModelPimpl::slotIncomingCall);
+    connect(&callbackHandler, &CallbacksHandler::callStateChanged, this, &NewCallModelPimpl::slotCallStateChanged);
 }
 
 NewCallModelPimpl::~NewCallModelPimpl()
@@ -118,7 +254,52 @@ NewCallModelPimpl::~NewCallModelPimpl()
 void
 NewCallModelPimpl::sendMessage(const std::string& callId, const std::string& body) const
 {
+    // cette fonction doit etre appellé depuis le sendmessage de conversation
+    // la fonction sendmessage de conversation doit etre friend
+    //
+    QMap<QString, QString> payloads;
+    payloads["text/plain"] = body.c_str();
 
+    CallManager::instance().sendTextMessage(callId.c_str(), payloads, true /* not used */);
+
+    // l'ajout du message dans la db doit se faire depuis conversatoin
+}
+
+void
+NewCallModelPimpl::slotIncomingCall(const std::string& accountId, const std::string& callId, const std::string& fromId)
+{
+    if (linked.owner.id != accountId) return;
+
+    qDebug() << "NewCallModelPimpl::slotIncomingCall";
+
+    auto callInfo = std::make_shared<call::Info>();
+    callInfo->id = callId;
+    callInfo->status =  call::Status::INCOMING_RINGING;
+    calls[callId] = callInfo;
+
+    emit linked.newIncomingCall(fromId, callId);
+}
+
+void
+NewCallModelPimpl::slotCallStateChanged(const std::string& callId, const std::string& state, int code)
+{
+    if (calls.find(callId) != calls.end()) {
+        if (state == "CONNECTING") {
+            calls[callId]->status = call::Status::CONNECTING;
+        } else if (state == "RINGING") {
+            calls[callId]->status = call::Status::OUTGOING_RINGING;
+        } else if (state == "HUNGUP") {
+            calls[callId]->status = call::Status::TERMINATING;
+        } else if (state == "OVER") {
+            calls[callId]->status = call::Status::ENDED;
+        } else if (state == "INACTIVE") {
+            calls[callId]->status = call::Status::INACTIVE;
+        } else if (state == "CURRENT") {
+            calls[callId]->status = call::Status::IN_PROGRESS;
+        }
+        qDebug() << "slotCallStateChanged, call:" << callId.c_str() << " - state: " << state.c_str();
+        emit linked.callStatusChanged(callId);
+    }
 }
 
 } // namespace api
