5b9173d830451e18366594e9eb9adebc33956fcb
Sébastien Blin <sebastien.blin@savoirfairelinux.com>
2017-08-31 12:51:18 -0400

    database: implement methods.
    - createTables() : create the first tables when a new db is created.
    
    - storeVersion() : add the version to the db. It will allow to figure
    if the local db needs to be updated.
    
    - insertInto(), update(), deleteFrom() and select() allow to handle
    data from the database.
    
    - ring.db is created inside standard folder.
    
    Change-Id: I979eb02dc3f36279188b177c6b432c91272e3696

--- src/newaccountmodel.cpp
+++ src/newaccountmodel.cpp
@@ -34,6 +34,9 @@ using namespace api;
 
 class NewAccountModelPimpl
 {
+    //~ friend class Dm;
+    //~ friend class Db;
+
 public:
     NewAccountModelPimpl(const Database& database);
     ~NewAccountModelPimpl();
@@ -42,10 +45,78 @@ public:
     NewAccountModel::AccountInfoMap accounts;
 };
 
+/**
+ * The following namespace is used to separate visualy the sources used to update the data for the model.
+ * We should NEVER have some information stored in two differents sources.
+ */
+
+namespace source
+{
+class Dm{ // daemon
+    public:
+    Dm(){};
+    virtual ~Dm() = 0;
+    static void getRingAccountsInfo(){ qDebug() << "!1!";};
+};
+
+class Db{ // database
+    public:
+    Db(){};
+    virtual ~Db() = 0;
+    static void getAccountsInfo(const Database& database);
+};
+
+} // namespace source
+
+using namespace source;
+
+void
+Db::getAccountsInfo(const Database& database)
+{
+    qDebug() << "!2!";
+
+    // insert into :
+    std::map<std::string, std::string> bindCol1 = {{":col1" , "col1"}, {":col2", "col2"}};
+    std::map<std::string, std::string> bindsSet1 = {{":col1" , "alphonse"}, {":col2", "nono"}};
+    auto n = database.insertInto("tests", bindCol1, bindsSet1);
+    qDebug() << "my last insert : " << n;
+    
+    // update :
+    std::string set2 = "col1=:newvalue";
+    std::map<std::string, std::string> bindSet2 = {{":newvalue" , "ATA"}};
+    std::string where2 = "col1=:oldvalue";
+    std::map<std::string, std::string> bindWhere2 = {{":oldvalue" , "qwert"}};
+    database.update("tests", set2, bindSet2, where2, bindWhere2);
+
+    // select :
+    std::string select3 = "*";
+    std::string where3 = "col1=:name";
+    std::map<std::string, std::string> binds3 = {{":name", "TOTO"}};
+    auto result = database.select(select3, "tests", where3, binds3);
+    qDebug() << " voici les resultats";
+    int j=0;
+    for (auto&e : result.payloads){
+        qDebug() << j << e.c_str();
+        j++;
+    }
+    qDebug() << "fin";
+    
+    // delete :
+    std::string where4 = "col2=:location";
+    std::map<std::string, std::string> binds4 = {{":location", "TITI"}};
+    database.deleteFrom("tests", where4, binds4);
+
+}
+
+using namespace source;
+
+
 NewAccountModel::NewAccountModel(const Database& database)
 : QObject()
 , pimpl_(std::make_unique<NewAccountModelPimpl>(database))
 {
+    qDebug() << "@YO3@";
+
 }
 
 NewAccountModel::~NewAccountModel()
@@ -67,7 +138,9 @@ NewAccountModel::getAccountInfo(const std::string& accountId)
 NewAccountModelPimpl::NewAccountModelPimpl(const Database& database)
 : database(database)
 {
-
+    qDebug() << "@YO4@";
+    Db::getAccountsInfo(database);
+    Dm::getRingAccountsInfo();
 }
 
 NewAccountModelPimpl::~NewAccountModelPimpl()
