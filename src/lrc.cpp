2f70eadf6e999d92996af63ae265ad43a6635805
Nicolas Jager <nicolas.jager@savoirfairelinux.com>
2017-08-31 13:58:26 -0400

    Lrc: implementation
    Change-Id: Id38179ea9271e906f52a2c4ace64b3860c8bebc4

--- src/lrc.cpp
+++ src/lrc.cpp
@@ -32,16 +32,16 @@ class LrcPimpl
 {
 
 public:
-    LrcPimpl();
+    LrcPimpl(const Lrc& linked);
 
-    std::unique_ptr<Lrc> parent;
+    const Lrc& linked;
     std::unique_ptr<Database> database;
     std::unique_ptr<NewAccountModel> accountModel;
     std::unique_ptr<CallbacksHandler> callbackHandler;
 };
 
 Lrc::Lrc()
-: lrcPipmpl_(std::make_unique<LrcPimpl>())
+: lrcPipmpl_(std::make_unique<LrcPimpl>(*this))
 {
 }
 
@@ -49,15 +49,26 @@ Lrc::~Lrc()
 {
 }
 
-NewAccountModel&
-Lrc::getAccountModel()
+const NewAccountModel&
+Lrc::getAccountModel() const
 {
     return *lrcPipmpl_->accountModel;
 }
 
-LrcPimpl::LrcPimpl()
+LrcPimpl::LrcPimpl(const Lrc& linked)
+: linked(linked)
 {
+    // ⚠️ take care of the order ⚠️
+    // we need to bind objects, so we have to be sure to never use something not yet instanced
+
+    // create callback model.
+    callbackHandler = std::make_unique<CallbacksHandler>();
+
+    // create the database.
     database = std::make_unique<Database>();
+
+    // create account model.
+    accountModel = std::make_unique<NewAccountModel>(*database);
 }
 
 } // namespace lrc
