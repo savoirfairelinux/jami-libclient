92dfd59961bec4ec87862497d71629ddbbffa317
Sébastien Blin <sebastien.blin@savoirfairelinux.com>
2017-08-31 12:51:18 -0400

    database: implement methods.
    - createTables() : create the first tables when a new db is created.
    
    - storeVersion() : add the version to the db. It will allow to figure
    if the local db needs to be updated.
    
    - insertInto(), update(), deleteFrom() and select() allow to handle
    data from the database.
    
    - ring.db is created inside standard folder.
    
    Change-Id: I979eb02dc3f36279188b177c6b432c91272e3696
    Reviewed-by: Guillaume Roguez <guillaume.roguez@savoirfairelinux.com>
    Signed-off-by: Guillaume Roguez <guillaume.roguez@savoirfairelinux.com>

--- src/database.h
+++ src/database.h
@@ -1,7 +1,8 @@
 /****************************************************************************
  *   Copyright (C) 2017 Savoir-faire Linux                                  *
- *   Author : Nicolas Jäger <nicolas.jager@savoirfairelinux.com>            *
- *   Author : Sébastien Blin <sebastien.blin@savoirfairelinux.com>          *
+ *   Author:  Nicolas Jäger <nicolas.jager@savoirfairelinux.com>            *
+ *   Author:  Sébastien Blin <sebastien.blin@savoirfairelinux.com>          *
+ *   Author: Guillaume Roguez <guillaume.roguez@savoirfairelinux>           *
  *                                                                          *
  *   This library is free software; you can redistribute it and/or          *
  *   modify it under the terms of the GNU Lesser General Public             *
@@ -21,57 +22,195 @@
 // Std
 #include <memory>
 #include <string>
+#include <stdexcept>
 
 // Qt
 #include <qobject.h>
 #include <QtSql/QSqlQuery>
 
-// Lrc
-#include "namedirectory.h"
-
-class Account; // TODO: move this class into lrc ns
-
 namespace lrc
 {
 
-namespace api { namespace message { struct Info; }}
-
+/**
+  *  @brief Class that communicates with the database.
+  *  @note not thread safe.
+  */
 class Database : public QObject {
     Q_OBJECT
 
 public:
-    using MessagesMap = std::map<int, api::message::Info>;
-
-    static constexpr auto ringDB = "ring.db"; // TODO: set path correctly for tests and release.
-
+    /**
+     * Create a database on the user system.
+     * @exception QueryError database query error.
+     */
     Database();
+
     ~Database();
 
-    // Messages related
-    void addMessage(const std::string& accountId, const api::message::Info& message) const;
-    void clearHistory(const std::string& accountId,
-                      const std::string& uid,
-                      bool removeContact = false) const;
-    MessagesMap getHistory(const std::string& accountId, const std::string& uid) const;
-    std::size_t numberOfUnreads(const std::string& accountId, const std::string& uid) const;
-    void setMessageRead(int uid) const;
+    /**
+     * A structure wich contains result(s) returned by a database query.
+     */
+    struct Result {
+        int nbrOfCols = -1; ///< store the number of columns returned.
+        /**
+         * if nbrOfCols equals three and if the size of payloads equals six,
+         * means two rows of data over three columns.
+         */
+        std::vector<std::string> payloads; ///< store the values.
+    };
 
-    // Contacts related
-    void addContact(const std::string& contact, const QByteArray& payload) const;
-    std::string getContactAttribute(const std::string& uid, const std::string& attribute) const;
+    /**
+     * Generic database query exception.
+     * details() returns more information, could be empty.
+     */
+    class QueryError : public std::runtime_error {
+    public:
+        explicit QueryError(const QSqlQuery& query);
+        virtual std::string details() { return {}; }
 
-Q_SIGNALS:
-    void messageAdded(int uid, const std::string& accountId, const api::message::Info& msg) const;
-    void contactAdded(const std::string& uid) const;
+        const QSqlQuery query;
+    };
 
-private Q_SLOTS:
-    void slotRegisteredNameFound(const Account* account,
-                                 NameDirectory::LookupStatus status,
-                                 const QString& address,
-                                 const QString& name) const;
+    /**
+     * Exception on database insert operation.
+     * details() returns more information.
+     */
+    class QueryInsertError final : public QueryError {
+    public:
+        explicit QueryInsertError(const QSqlQuery& query,
+                                  const std::string& table,
+                                  const std::map<std::string, std::string>& bindCol,
+                                  const std::map<std::string, std::string>& bindsSet);
+        std::string details() override;
+
+        const std::string table;
+        const std::map<std::string, std::string> bindCol;
+        const std::map<std::string, std::string> bindsSet;
+    };
+
+    /**
+     * Exception on database update operation.
+     * details() returns more information.
+     */
+    class QueryUpdateError final : public QueryError {
+    public:
+        explicit QueryUpdateError(const QSqlQuery& query,
+                                  const std::string& table,
+                                  const std::string& set,
+                                  const std::map<std::string, std::string>& bindsSet,
+                                  const std::string& where,
+                                  const std::map<std::string, std::string>& bindsWhere);
+        std::string details() override;
+
+        const std::string table;
+        const std::string set;
+        const std::map<std::string, std::string> bindsSet;
+        const std::string where;
+        const std::map<std::string, std::string> bindsWhere;
+    };
+
+    /**
+     * Exception on database select operation.
+     * details() returns more information.
+     */
+    class QuerySelectError final : public QueryError {
+    public:
+        explicit QuerySelectError(const QSqlQuery& query,
+                                  const std::string& select,
+                                  const std::string& table,
+                                  const std::string& where,
+                                  const std::map<std::string, std::string>& bindsWhere);
+        std::string details() override;
+
+        const std::string select;
+        const std::string table;
+        const std::string where;
+        const std::map<std::string, std::string> bindsWhere;
+    };
+
+    /**
+     * Exception on database delete operation.
+     * details() returns more information.
+     */
+    class QueryDeleteError final : public QueryError {
+    public:
+        explicit QueryDeleteError(const QSqlQuery& query,
+                                  const std::string& table,
+                                  const std::string& where,
+                                  const std::map<std::string, std::string>& bindsWhere);
+        std::string details() override;
+
+        const std::string table;
+        const std::string where;
+        const std::map<std::string, std::string> bindsWhere;
+    };
+
+    /**
+     * Insert value(s) inside a table.
+     * @param table where to perfom the action on.
+     * @param bindCol binds column(s) and identifier(s). The key is the identifier, it should begin by ':'.
+     *        The value is the name of the column from the table.
+     * @param bindsSet binds value(s) and identifier(s). The key is the identifier, it should begin by ':'.
+     *        The value is the value to store.
+     * @return signed integer representing the index of last inserted element. -1 if nothing inserted.
+     * @exception QueryInsertError insert query failed.
+     *
+     * @note usually the identifiers has to be the same between bindCol and bindsSet
+     */
+    int insertInto(const std::string& table,
+                   const std::map<std::string, std::string>& bindCol,
+                   const std::map<std::string, std::string>& bindsSet);
+    /**
+     * Update value(s) inside a table.
+     * @param table where to perfom the action on.
+     * @param set defines which column(s), using identifier(s), will be updated.
+     * @param bindsSet specifies the value(s) to set, using the identifier(s). The key is the identifier, it should
+     *        begin by ':'. The value is value to set.
+     * @param where defines the conditional to update, using identifier(s).
+     * @param bindsWhere specifies the value(s) to test using the identifier(s). The key is the identifier, it should
+     *        begin by ':'. The value is the value test.
+     * @exception QueryUpdateError update query failed.
+     *
+     * @note usually, identifiers between set and bindsSet, are equals. The same goes between where and bindsWhere.
+     */
+    void update(const std::string& table,
+                const std::string& set,
+                const std::map<std::string, std::string>& bindsSet,
+                const std::string& where,
+                const std::map<std::string, std::string>& bindsWhere);
+    /**
+     * Delete rows from a table.
+     * @param table where to perfom the action on.
+     * @param where defines the conditional to update, using identifier(s).
+     * @param bindsWhere specifies the value(s) to test using the identifier(s). The key is the identifier, it should
+     *        begin by ':'. The value is the value test.
+     * @exception QueryDeleteError delete query failed.
+     *
+     * @note usually, identifiers between where and bindsWhere, are equals.
+     */
+    void deleteFrom(const std::string& table,
+                    const std::string& where,
+                    const std::map<std::string, std::string>& bindsWhere);
+    /**
+     * Select data from table.
+     * @param select column(s) to select.e
+     * @param table where to perfom the action on.
+     * @param where defines the conditional to select, using identifier(s).
+     * @param bindsWhere specifies the value(s) to test using the identifier(s). The key is the identifier, it should
+     *        begin by ':'. The value is the value to test.
+     * @return Database::Result which contains the result(s).
+     * @exception QuerySelectError select query failed.
+     *
+     * @note usually, identifiers between where and bindsWhere, are equals.
+     */
+    Database::Result select(const std::string& select,
+                            const std::string& table,
+                            const std::string& where,
+                            const std::map<std::string, std::string>& bindsWhere);
 
 private:
-    std::unique_ptr<QSqlQuery> query_;
+    void createTables();
+    void storeVersion(const std::string& version);
     QSqlDatabase db_;
 };
 
