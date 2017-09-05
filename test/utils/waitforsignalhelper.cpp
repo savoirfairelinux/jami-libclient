dcf2e3a09a9d6b794a87907cf962d67c02b3811d
Sébastien Blin <sebastien.blin@savoirfairelinux.com>
2017-09-05 16:28:58 -0400

    tests: add tests for LRC (TODO split)
    Change-Id: I587d29657b2f5b37eb3b9372a5caa247ea75abb9

---
+++ test/utils/waitforsignalhelper.cpp
@@ -0,0 +1,30 @@
+#include "waitforsignalhelper.h"
+
+#include <QTimer>
+
+WaitForSignalHelper::WaitForSignalHelper(QObject& object, const char* signal)
+: timeout_(false)
+{
+    connect(&object, signal, &eventLoop_, SLOT(quit()));
+}
+
+bool
+WaitForSignalHelper::wait(unsigned int timeoutMs)
+{
+    QTimer timeoutHelper;
+    if (timeoutMs != 0) {
+        timeoutHelper.setInterval(timeoutMs);
+        timeoutHelper.start();
+        connect(&timeoutHelper, SIGNAL(timeout()), this, SLOT(timeout()));
+    }
+    timeout_ = false;
+    eventLoop_.exec();
+    return timeout_;
+}
+
+void
+WaitForSignalHelper::timeout()
+{
+    timeout_ = true;
+    eventLoop_.quit();
+}
