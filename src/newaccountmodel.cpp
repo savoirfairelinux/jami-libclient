28176bf1e1983be18fe8a37d88ac3211db6ebd21
Nicolas Jager <nicolas.jager@savoirfairelinux.com>
2017-09-13 12:38:24 -0400

    NewAccountModel: implementation
    - this class allows to get accounts informations. Each account has
    his own NewCallModel, ContactModel and ConversationModel.
    
    - getAccountList() returns a list of account ids.
    
    - getAccountInfo() returns informations about an account.
    
    - references to the NewAccountModel called parents were removed from
    the constructor because account::Info& is already storing a reference
    to NewAccountModel.
    
    Change-Id: Ib429520ec6751201e1bc6a45c9dc8d54c76e428a

--- src/newaccountmodel.cpp
+++ src/newaccountmodel.cpp
@@ -1,7 +1,7 @@
 /****************************************************************************
  *   Copyright (C) 2017 Savoir-faire Linux                                  *
- *   Author : Nicolas Jäger <nicolas.jager@savoirfairelinux.com>            *
- *   Author : Sébastien Blin <sebastien.blin@savoirfairelinux.com>          *
+ *   Author: Nicolas Jäger <nicolas.jager@savoirfairelinux.com>            *
+ *   Author: Sébastien Blin <sebastien.blin@savoirfairelinux.com>          *
  *                                                                          *
  *   This library is free software; you can redistribute it and/or          *
  *   modify it under the terms of the GNU Lesser General Public             *
@@ -24,27 +24,73 @@
 #include "api/contactmodel.h"
 #include "api/conversationmodel.h"
 #include "api/account.h"
-
+#include "authority/databasehelper.h"
+#include "callbackshandler.h"
 #include "database.h"
+
+#include "accountmodel.h"
+
+// Dbus
+#include "dbus/configurationmanager.h"
 
 namespace lrc
 {
 
 using namespace api;
 
-class NewAccountModelPimpl
+class NewAccountModelPimpl: public QObject
 {
 public:
-    NewAccountModelPimpl(const Database& database);
+    NewAccountModelPimpl(NewAccountModel& linked,
+                         Database& database,
+                         const CallbacksHandler& callbackHandler);
     ~NewAccountModelPimpl();
 
-    const Database& database;
+    NewAccountModel& linked;
+    const CallbacksHandler& callbacksHandler;
+    Database& database;
     NewAccountModel::AccountInfoMap accounts;
+    /**
+     * Add the profile information from an account to the db.
+     * @param info
+     */
+    void addAcountProfileInDb(const account::Info& info);
+    /**
+     * Add the profile information from an account to the db then add it to accounts.
+     * @param accountId
+     * @note this method get details for an account from the daemon.
+     */
+    void addToAccounts(const std::string& accountId);
+
+    std::vector<std::string> newAccountsId;
+
+public Q_SLOTS:
+    /**
+     * Emit accountStatusChanged.
+     * @param accountId
+     * @param status
+     */
+    void slotAccountStatusChanged(const std::string& accountID, const api::account::Status status);
+    /**
+     * Emit accountAdded.
+     * @param account
+     */
+    void slotAccountAdded(Account* account);
+    /**
+     * Emit accountRemoved.
+     * @param account
+     */
+    void slotAccountRemoved(Account* account);
+    /**
+     * Emit accountAdded.
+     * @param account
+     */
+    void slotAccountUpdated();
 };
 
-NewAccountModel::NewAccountModel(const Database& database)
+NewAccountModel::NewAccountModel(Database& database, const CallbacksHandler& callbacksHandler)
 : QObject()
-, pimpl_(std::make_unique<NewAccountModelPimpl>(database))
+, pimpl_(std::make_unique<NewAccountModelPimpl>(*this, database, callbacksHandler))
 {
 }
 
@@ -52,27 +98,136 @@ NewAccountModel::~NewAccountModel()
 {
 }
 
-const std::vector<std::string>
+std::vector<std::string>
 NewAccountModel::getAccountList() const
 {
-    return {};
+    std::vector<std::string> accountsId;
+
+    for(auto const& accountInfo: pimpl_->accounts)
+        accountsId.emplace_back(accountInfo.first);
+
+    return accountsId;
 }
 
 const account::Info&
-NewAccountModel::getAccountInfo(const std::string& accountId)
+NewAccountModel::getAccountInfo(const std::string& accountId) const
 {
-    return pimpl_->accounts[accountId];
+    auto accountInfo = pimpl_->accounts.find(accountId);
+    if (accountInfo == pimpl_->accounts.end())
+        throw std::out_of_range("NewAccountModel::getAccountInfo, can't find " + accountId);
+
+    return accountInfo->second;
 }
 
-NewAccountModelPimpl::NewAccountModelPimpl(const Database& database)
-: database(database)
+NewAccountModelPimpl::NewAccountModelPimpl(NewAccountModel& linked,
+                                           Database& database,
+                                           const CallbacksHandler& callbacksHandler)
+: linked(linked)
+, callbacksHandler(callbacksHandler)
+, database(database)
 {
+    const QStringList accountIds = ConfigurationManager::instance().getAccountList();
 
+    for (auto& id : accountIds)
+        addToAccounts(id.toStdString());
+
+    connect(&callbacksHandler, &CallbacksHandler::accountStatusChanged, this, &NewAccountModelPimpl::slotAccountStatusChanged);
+
+    // NOTE: because we still use the legacy LRC for configuration, we are still using old signals
+    connect(&AccountModel::instance(), &AccountModel::accountAdded, this,  &NewAccountModelPimpl::slotAccountAdded);
+    connect(&AccountModel::instance(), &AccountModel::accountRemoved, this,  &NewAccountModelPimpl::slotAccountRemoved);
+    connect(&AccountModel::instance(), &AccountModel::accountListUpdated, this,  &NewAccountModelPimpl::slotAccountUpdated);
 }
 
 NewAccountModelPimpl::~NewAccountModelPimpl()
 {
 
+}
+
+void
+NewAccountModelPimpl::addAcountProfileInDb(const account::Info& info)
+{
+    auto returnFromDb = database.select("uri",
+                                        "profiles",
+                                        "uri=:uri",
+                                        {{":uri", info.profileInfo.uri}});
+    if (returnFromDb.payloads.empty()) {
+        // Profile is not in db, add it.
+        std::string type = info.profileInfo.type == profile::Type::RING ? "RING" : "SIP";
+        database.insertInto("profiles",
+                             {{":uri", "uri"}, {":alias", "alias"}, {":photo", "photo"},
+                              {":type", "type"}, {":status", "status"}},
+                             {{":uri", info.profileInfo.uri}, {":alias", info.profileInfo.alias}, {":photo", ""},
+                              {":type", type}, {":status", ""}});
+    }
+}
+
+void
+NewAccountModelPimpl::slotAccountStatusChanged(const std::string& accountID, const api::account::Status status)
+{
+    auto accountInfo = accounts.find(accountID);
+        if (accountInfo == accounts.end())
+            addToAccounts(accountID);
+
+    accountInfo->second.status = status;
+
+    emit linked.accountStatusChanged(accountID);
+}
+
+void
+NewAccountModelPimpl::addToAccounts(const std::string& accountId)
+{
+    QMap<QString, QString> details = ConfigurationManager::instance().getAccountDetails(accountId.c_str());
+    const MapStringString volatileDetails = ConfigurationManager::instance().getVolatileAccountDetails(accountId.c_str());
+
+    // Init profile
+    auto& item = *(accounts.emplace(accountId, account::Info()).first);
+    auto& owner = item.second;
+    owner.id = accountId;
+    owner.enabled = details["Account.enable"] == QString("true");
+    // TODO get avatar;
+    owner.profileInfo.type = details["Account.type"] == "RING" ? profile::Type::RING : profile::Type::SIP;
+    owner.profileInfo.alias = details["Account.alias"].toStdString();
+    owner.registeredName = owner.profileInfo.type == profile::Type::RING ?
+                                   volatileDetails["Account.registredName"].toStdString() : owner.profileInfo.alias;
+    owner.profileInfo.uri = (owner.profileInfo.type == profile::Type::RING and details["Account.username"].contains("ring:")) ?
+                        details["Account.username"].toStdString().substr(std::string("ring:").size())
+                        : details["Account.username"].toStdString();
+    // Add profile into database
+    addAcountProfileInDb(owner);
+    // Init models for this account
+    owner.callModel = std::make_unique<NewCallModel>(owner);
+    owner.contactModel = std::make_unique<ContactModel>(owner, database);
+    owner.conversationModel = std::make_unique<ConversationModel>(owner, database);
+    owner.accountModel = &linked;
+}
+
+void
+NewAccountModelPimpl::slotAccountAdded(Account* account)
+{
+    newAccountsId.emplace_back(account->id().toStdString());
+    // NOTE: The account is not correct yet. we need to wait for accountListUpdated
+}
+
+void
+NewAccountModelPimpl::slotAccountUpdated()
+{
+    if (newAccountsId.empty()) return;
+    // Accounts are now ready
+    for (const auto& accountId: newAccountsId) {
+        addToAccounts(accountId);
+        emit linked.accountAdded(accountId);
+    }
+    newAccountsId.clear();
+}
+
+void
+NewAccountModelPimpl::slotAccountRemoved(Account* account)
+{
+    auto accountId = account->id().toStdString();
+    authority::database::removeAccount(database, accounts[accountId].profileInfo.uri);
+    accounts.erase(accountId);
+    emit linked.accountRemoved(accountId);
 }
 
 } // namespace lrc
