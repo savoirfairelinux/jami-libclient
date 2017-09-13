0f77df2d703c756178ae62372c1d072ba850ea4e
Nicolas Jager <nicolas.jager@savoirfairelinux.com>
2017-09-13 12:38:24 -0400

    NewAccountModel: implementation
    - this class allows to get accounts informations. Each account has
    his own NewCallModel, ContactModel and ConversationModel.
    
    - getAccountList() returns a list of account ids.
    
    - getAccountInfo() returns informations about an account.
    
    - references to the NewAccountModel called parents were removed from
    the constructor because account::Info& is already storing a reference
    to NewAccountModel.
    
    Change-Id: Ib429520ec6751201e1bc6a45c9dc8d54c76e428a

--- src/api/newaccountmodel.h
+++ src/api/newaccountmodel.h
@@ -33,6 +33,7 @@
 namespace lrc
 {
 
+class CallbacksHandler;
 class Database;
 class NewAccountModelPimpl;
 
@@ -45,11 +46,19 @@ class LIB_EXPORT NewAccountModel : public QObject {
 public:
     using AccountInfoMap = std::map<std::string, account::Info>;
 
-    NewAccountModel(const Database& database);
+    NewAccountModel(const Database& database, const CallbacksHandler& callbackHandler);
     ~NewAccountModel();
-
+    /**
+     * get a list of all acountId.
+     * @return a std::vector<std::string>.
+     */
     const std::vector<std::string> getAccountList() const;
-    const account::Info& getAccountInfo(const std::string& accountId);
+    /**
+     * get account informations associated to an accountId.
+     * @param accountId.
+     * @return a const account::Info& structure.
+     */
+    const api::account::Info& getAccountInfo(const std::string& accountId) const;
 
 private:
     std::unique_ptr<NewAccountModelPimpl> pimpl_;
