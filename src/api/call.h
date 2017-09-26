8f5d78dd0de375e49370a92397b026deccc62f8b
Nicolas Jager <nicolas.jager@savoirfairelinux.com>
2017-09-26 10:53:59 -0400

    api changes
    - change lrc::api::message for lrc::api::interaction
      Rationale: message is only a part of different interactions possibly
      present in the historic.
    
    - add lrc::api::profile namespace.
      Rationale: information stored by daemon or database
      about accounts or contacts accessible through profile. Other
      informations will be considered volatile.
    
    - lrc::api::account::Info changes:
      * accountModel a non-owned pointer than unique_ptr
      * add members: registeredName, enabled, status and profileInfo
    
    - lrc::api::call::Info changes:
      * add members: peer, audioMuted and videoMuted
    
    - lrc::api::contact::Info changes:
      * remove members: uri, avatar, alias, type
      * add member: profileInfo
    
    - lrc::api::conversation::Info changes:
      * messages -> interactions
      * remove isUsed
    
    - all ::Info: add functions to convert Type and Status to string and vice versa.
    
    - minor file header fixes.
    
    [GR: ci-msg refactor]
    
    Change-Id: Iba46194eb29ff394481663bef4ca07dc4bd899f6
    Reviewed-by: Guillaume Roguez <guillaume.roguez@savoirfairelinux.com>

--- src/api/call.h
+++ src/api/call.h
@@ -1,7 +1,7 @@
 /****************************************************************************
  *   Copyright (C) 2017 Savoir-faire Linux                                  *
- *   Author : Nicolas Jäger <nicolas.jager@savoirfairelinux.com>            *
- *   Author : Sébastien Blin <sebastien.blin@savoirfairelinux.com>          *
+ *   Author: Nicolas Jäger <nicolas.jager@savoirfairelinux.com>             *
+ *   Author: Sébastien Blin <sebastien.blin@savoirfairelinux.com>           *
  *                                                                          *
  *   This library is free software; you can redistribute it and/or          *
  *   modify it under the terms of the GNU Lesser General Public             *
@@ -21,6 +21,9 @@
 // std
 #include <string>
 #include <ctime>
+
+// Qt
+#include <QObject>
 
 namespace lrc
 {
@@ -48,11 +51,52 @@ enum class Status {
     AUTO_ANSWERING
 };
 
+static const std::string
+StatusToString(const call::Status& status)
+{
+    switch(status)
+    {
+    case call::Status::PAUSED:
+        return QObject::tr("Hold").toStdString();
+    case call::Status::IN_PROGRESS:
+        return QObject::tr("Talking").toStdString();
+    case call::Status::INVALID:
+        return QObject::tr("ERROR").toStdString();
+    case call::Status::OUTGOING_REQUESTED:
+        return QObject::tr("Outgoing requested").toStdString();
+    case call::Status::INCOMING_RINGING:
+        return QObject::tr("Incoming").toStdString();
+    case call::Status::OUTGOING_RINGING:
+        return QObject::tr("Calling").toStdString();
+    case call::Status::CONNECTING:
+        return QObject::tr("Connecting").toStdString();
+    case call::Status::SEARCHING:
+        return QObject::tr("Searching").toStdString();
+    case call::Status::PEER_PAUSED:
+        return QObject::tr("Hold").toStdString();
+    case call::Status::INACTIVE:
+        return QObject::tr("Inactive").toStdString();
+    case call::Status::ENDED:
+        return QObject::tr("Finished").toStdString();
+    case call::Status::TERMINATING:
+        return QObject::tr("Finished").toStdString();
+    case call::Status::CONNECTED:
+        return QObject::tr("Communication established").toStdString();
+    case call::Status::AUTO_ANSWERING:
+        return QObject::tr("Auto answering").toStdString();
+        break;
+    }
+}
+
+
 struct Info
 {
     std::string id;
     std::time_t startTime = 0;
     Status status = Status::INVALID;
+    std::string peer;
+    bool audioMuted = false;
+    bool videoMuted = false;
 };
 
 } // namespace call
