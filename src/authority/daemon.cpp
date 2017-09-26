bfbbb8c30d504a58b5573ecd69ce032625605e66
Nicolas Jager <nicolas.jager@savoirfairelinux.com>
2017-09-26 12:51:17 -0400

    add namespace authority
    - authority namespace is used to help when we work inside the lrc.
    it makes more visibles which source of information has the authority
    over some data.
    
    Change-Id: I25a5d27b154376fe6211ff01b0e9e138bd782f43

---
+++ src/authority/daemon.cpp
@@ -0,0 +1,49 @@
+// Lrc
+#include "daemon.h"
+
+namespace lrc
+{
+
+namespace authority
+{
+
+namespace daemon
+{
+
+void addContact(const api::account::Info& owner, const std::string& uri)
+{
+    ConfigurationManager::instance().addContact(QString(owner.id.c_str()),
+    QString(uri.c_str()));
+}
+
+void addToContacts(const api::account::Info& owner, api::contact::Info& contactInfo)
+{
+    ConfigurationManager::instance().addContact(QString(owner.id.c_str()),
+    QString(contactInfo.uri.c_str()));
+}
+
+void removeContact(const api::account::Info& owner, const std::string& contactUri, bool banned)
+{
+    ConfigurationManager::instance().removeContact(QString(owner.id.c_str()),
+    QString(contactUri.c_str()), banned);
+}
+
+void addContactFromPending(const api::account::Info& owner, const std::string& contactUri)
+{
+    ConfigurationManager::instance().acceptTrustRequest(QString(owner.id.c_str()),
+    QString(contactUri.c_str()));
+}
+
+void discardFromPending(const api::account::Info& owner, const std::string& contactUri)
+{
+    ConfigurationManager::instance().discardTrustRequest(
+        QString(owner.id.c_str()),
+        QString(contactUri.c_str())
+    );
+}
+
+} // namespace daemon
+
+} // namespace authority
+
+} // namespace lrc
