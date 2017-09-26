03cc8f9e730c3ad1352e0946924a1bcee9581abd
Nicolas Jager <nicolas.jager@savoirfairelinux.com>
2017-09-26 10:53:59 -0400

    api: edit data namespaces
    - after working on the WIP branch we need to get these edits before
    the ones for the models. This patch edits the following namespaces:
    account, contact, conversation and messages.
    
    - add functions to convert Type and Status to string and vice versa.
    
    - edit Info structures, remove unused members and add some needed.
    
    - add profile namespace. Information stored by daemon or database
    about accounts or contacts will be accessible through profile. Other
    informations will be considered volatile.
    
    - message namespace is renamed in interaction namespace. The reason,
    is message is only a part of the different interactions that could
    be present in the historic.
    
    Change-Id: Iba46194eb29ff394481663bef4ca07dc4bd899f6

--- src/api/account.h
+++ src/api/account.h
@@ -1,7 +1,7 @@
 /****************************************************************************
  *   Copyright (C) 2017 Savoir-faire Linux                                  *
- *   Author : Nicolas Jäger <nicolas.jager@savoirfairelinux.com>            *
- *   Author : Sébastien Blin <sebastien.blin@savoirfairelinux.com>          *
+ *   Author: Nicolas Jäger <nicolas.jager@savoirfairelinux.com>            *
+ *   Author: Sébastien Blin <sebastien.blin@savoirfairelinux.com>          *
  *                                                                          *
  *   This library is free software; you can redistribute it and/or          *
  *   modify it under the terms of the GNU Lesser General Public             *
@@ -18,8 +18,12 @@
  ***************************************************************************/
 #pragma once
 
+// Std
 #include <string>
 #include <memory>
+
+// Data
+#include "profile.h"
 
 namespace lrc
 {
@@ -41,14 +45,40 @@ enum class Type {
     SIP
 };
 
+enum class Status {
+    INVALID,
+    INITIALIZING,
+    UNREGISTERED,
+    TRYING,
+    REGISTERED
+};
+
+static account::Status
+StringToStatus(const std::string& type)
+{
+    if (type == "INITIALIZING")
+        return account::Status::INITIALIZING;
+    else if (type == "UNREGISTERED")
+        return account::Status::UNREGISTERED;
+    else if (type == "TRYING")
+        return account::Status::TRYING;
+    else if (type == "REGISTERED")
+        return account::Status::REGISTERED;
+    else
+        return account::Status::INVALID;
+}
+
 struct Info
 {
     std::string id;
-    Type type = account::Type::INVALID;
+    std::string registeredName;
+    bool enabled;
+    Status status = account::Status::INVALID;
+    profile::Info profileInfo;
     std::unique_ptr<lrc::api::NewCallModel> callModel;
     std::unique_ptr<lrc::api::ContactModel> contactModel;
     std::unique_ptr<lrc::api::ConversationModel> conversationModel;
-    std::unique_ptr<lrc::api::NewAccountModel> accountModel;
+    NewAccountModel* accountModel {nullptr};
 };
 
 } // namespace account
