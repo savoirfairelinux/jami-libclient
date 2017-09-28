a8d72f43682bfb191d5672401f5795a5ad5f446a
Nicolas Jager <nicolas.jager@savoirfairelinux.com>
2017-09-28 15:24:53 -0400

    BehaviorController: implementation
    - this class is an helper for clients. Clients can connect
    signals from this class to easily update their views.
    
    - for now, this class has the following signals:
      - showChatView: the client should open the chat view.
      - showCallView: the client should open the call view.
      - showIncomingCallView: the client should open the incoming call
        view.
    
    Change-Id: I9f51859c08aeadacf46d3c828dc9ab012c19b8f1

--- src/conversationmodel.cpp
+++ src/conversationmodel.cpp
@@ -23,6 +23,7 @@
 #include <algorithm>
 
 // LRC
+#include "api/behaviorcontroller.h"
 #include "api/contactmodel.h"
 #include "api/newcallmodel.h"
 #include "api/newaccountmodel.h"
@@ -51,7 +52,8 @@ class ConversationModelPimpl : public QObject
 public:
     ConversationModelPimpl(const ConversationModel& linked,
                            Database& db,
-                           const CallbacksHandler& callbacksHandler);
+                           const CallbacksHandler& callbacksHandler,
+                           const BehaviorController& behaviorController);
 
     ~ConversationModelPimpl();
 
@@ -103,6 +105,7 @@ public:
     Database& db;
     const CallbacksHandler& callbacksHandler;
     const std::string accountProfileId;
+    const BehaviorController& behaviorController;
 
     // contains all conversations
     ConversationModel::ConversationQueue conversations;
@@ -167,9 +170,12 @@ public Q_SLOTS:
 
 };
 
-ConversationModel::ConversationModel(const account::Info& owner, Database& db, const CallbacksHandler& callbacksHandler)
+ConversationModel::ConversationModel(const account::Info& owner,
+                                     Database& db,
+                                     const CallbacksHandler& callbacksHandler,
+                                     const BehaviorController& behaviorController)
 : QObject()
-, pimpl_(std::make_unique<ConversationModelPimpl>(*this, db, callbacksHandler))
+, pimpl_(std::make_unique<ConversationModelPimpl>(*this, db, callbacksHandler, behaviorController))
 , owner(owner)
 {
 
@@ -272,14 +278,14 @@ ConversationModel::selectConversation(const std::string& uid) const
             case call::Status::CONNECTING:
             case call::Status::SEARCHING:
                 // We are currently in a call
-                // fully functional behaviour not implemented in this patch
+                emit pimpl_->behaviorController.showIncomingCallView(owner.id, conversation);
                 break;
             case call::Status::PAUSED:
             case call::Status::PEER_PAUSED:
             case call::Status::CONNECTED:
             case call::Status::IN_PROGRESS:
                 // We are currently receiving a call
-                // fully functional behaviour not implemented in this patch
+                emit pimpl_->behaviorController.showCallView(owner.id, conversation);
                 break;
             case call::Status::INVALID:
             case call::Status::OUTGOING_REQUESTED:
@@ -289,11 +295,10 @@ ConversationModel::selectConversation(const std::string& uid) const
             case call::Status::AUTO_ANSWERING:
             default:
                 // We are not in a call, show the chatview
-                // fully functional behaviour not implemented in this patch
-                break;
+                emit pimpl_->behaviorController.showChatView(owner.id, conversation);
         }
     } catch (const std::out_of_range&) {
-        // fully functional behaviour not implemented in this patch
+        emit pimpl_->behaviorController.showChatView(owner.id, conversation);
     }
 }
 
@@ -357,7 +362,7 @@ ConversationModel::placeCall(const std::string& uid) const
             return;
         conversation = pimpl_->conversations.at(conversationIdx);
     }
-    // fully functional behaviour not implemented in this patch
+    emit pimpl_->behaviorController.showIncomingCallView(owner.id, conversation);
 }
 
 void
@@ -461,12 +466,14 @@ ConversationModel::clearHistory(const std::string& uid)
 
 ConversationModelPimpl::ConversationModelPimpl(const ConversationModel& linked,
                                                Database& db,
-                                               const CallbacksHandler& callbacksHandler)
+                                               const CallbacksHandler& callbacksHandler,
+                                               const BehaviorController& behaviorController)
 : linked(linked)
 , db(db)
 , callbacksHandler(callbacksHandler)
 , typeFilter(profile::Type::INVALID)
 , accountProfileId(database::getProfileId(db, linked.owner.profileInfo.uri))
+, behaviorController(behaviorController)
 {
     initConversations();
 
@@ -710,7 +717,7 @@ ConversationModelPimpl::slotIncomingCall(const std::string& fromId, const std::s
 
     qDebug() << "Add call to conversation with " << fromId.c_str();
     conversation.callId = callId;
-    // fully functional behaviour not implemented in this patch
+    emit behaviorController.showIncomingCallView(linked.owner.id, conversation);
 }
 
 void
