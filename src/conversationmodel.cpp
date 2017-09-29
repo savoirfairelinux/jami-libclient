3b088acb8b4384d1c86623fa07704250f100c803
Sébastien Blin <sebastien.blin@savoirfairelinux.com>
2017-09-29 16:11:52 -0400

    api: implement methods for conferences.
    Change-Id: Ia90efc9ebae41698c42a349b0227b0d1450f35c7

--- src/conversationmodel.cpp
+++ src/conversationmodel.cpp
@@ -157,6 +157,7 @@ public Q_SLOTS:
                                std::string& from,
                                std::map<std::string,std::string> payloads);
     void slotIncomingCallMessage(const std::string& callId, const std::string& from, const std::string& body);
+    void slotCallAddedToConference(const std::string& callId, const std::string& confId);
 
 };
 
@@ -328,6 +329,33 @@ ConversationModel::placeCall(const std::string& uid) const
         return;
     }
 
+    // Disallow multiple call
+    if (!conversation.callId.empty()) {
+        try  {
+            auto call = owner.callModel->getCall(conversation.callId);
+            switch (call.status) {
+                case call::Status::INCOMING_RINGING:
+                case call::Status::OUTGOING_RINGING:
+                case call::Status::CONNECTING:
+                case call::Status::SEARCHING:
+                case call::Status::PAUSED:
+                case call::Status::PEER_PAUSED:
+                case call::Status::CONNECTED:
+                case call::Status::IN_PROGRESS:
+                case call::Status::OUTGOING_REQUESTED:
+                case call::Status::AUTO_ANSWERING:
+                    return;
+                case call::Status::INVALID:
+                case call::Status::INACTIVE:
+                case call::Status::ENDED:
+                case call::Status::TERMINATING:
+                default:
+                    break;
+            }
+        } catch (const std::out_of_range&) {
+        }
+    }
+
     auto convId = uid;
     auto accountId = pimpl_->accountProfileId;
 
@@ -429,11 +457,18 @@ ConversationModel::setFilter(const profile::Type& filter)
 }
 
 void
-ConversationModel::addParticipant(const std::string& uid, const::std::string& uri)
+ConversationModel::addParticipant(const std::string& uidSrc, const std::string& uidDest)
 {
-    Q_UNUSED(uid)
-    Q_UNUSED(uri)
-    // TODO when conferences.will be implemented
+    auto conversationSrcIdx = pimpl_->indexOf(uidSrc);
+    auto conversationDestIdx = pimpl_->indexOf(uidDest);
+    if (conversationSrcIdx == -1 || conversationDestIdx == -1)
+        return;
+    auto& conversationSrc = pimpl_->conversations[conversationSrcIdx];
+    auto& conversationDest = pimpl_->conversations[conversationDestIdx];
+    // NOTE: To create a conference, we must be in call for now.
+    if (conversationSrc.callId.empty() || conversationDest.callId.empty())
+        return;
+    owner.callModel->addParticipant(conversationSrc.callId, conversationDest.callId);
 }
 
 void
@@ -498,6 +533,10 @@ ConversationModelPimpl::ConversationModelPimpl(const ConversationModel& linked,
             &lrc::api::NewCallModel::callEnded,
             this,
             &ConversationModelPimpl::slotCallEnded);
+    connect(&*linked.owner.callModel,
+            &lrc::api::NewCallModel::callAddedToConference,
+            this,
+            &ConversationModelPimpl::slotCallAddedToConference);
 }
 
 
@@ -753,24 +792,19 @@ void
 ConversationModelPimpl::addCallMessage(const std::string& callId, const std::string& body)
 {
     // Get conversation
-    auto i = std::find_if(
-    conversations.begin(), conversations.end(),
-    [callId](const conversation::Info& conversation) {
-      return conversation.callId == callId;
-    });
-
-    if (i == conversations.end()) return;
-
-    auto& conversation = *i;
-    auto uid = conversation.uid;
-    auto msg = interaction::Info({accountProfileId, body, std::time(nullptr),
-                            interaction::Type::CALL, interaction::Status::SUCCEED});
-    int msgId = database::addMessageToConversation(db, accountProfileId, conversation.uid, msg);
-    conversation.interactions.emplace(msgId, msg);
-    conversation.lastMessageUid = msgId;
-    emit linked.newUnreadMessage(conversation.uid, msg);
-    sortConversations();
-    emit linked.modelSorted();
+    for (auto& conversation: conversations) {
+        if (conversation.callId == callId) {
+            auto uid = conversation.uid;
+            auto msg = interaction::Info({accountProfileId, body, std::time(nullptr),
+                                    interaction::Type::CALL, interaction::Status::SUCCEED});
+            int msgId = database::addMessageToConversation(db, accountProfileId, conversation.uid, msg);
+            conversation.interactions.emplace(msgId, msg);
+            conversation.lastMessageUid = msgId;
+            emit linked.newUnreadMessage(conversation.uid, msg);
+            sortConversations();
+            emit linked.modelSorted();
+        }
+    }
 }
 
 
@@ -819,6 +853,18 @@ ConversationModelPimpl::addIncomingMessage(const std::string& from, const std::s
     sortConversations();
     emit linked.modelSorted();
 }
+
+void
+ConversationModelPimpl::slotCallAddedToConference(const std::string& callId, const std::string& confId)
+{
+    for (auto& conversation: conversations) {
+        if (conversation.callId == callId) {
+            conversation.callId = confId;
+            emit linked.selectConversation(conversation.uid);
+        }
+    }
+}
+
 
 } // namespace lrc
 
