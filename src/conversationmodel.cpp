aab0751791918b94c61137882aa1a43c41a5f371
Nicolas Jager <nicolas.jager@savoirfairelinux.com>
2017-10-13 09:47:04 -0400

    [WIP] fix for conferences
    Change-Id: Ia2e7985757c8ea8076858e16e48946ecc56acf7a

--- src/conversationmodel.cpp
+++ src/conversationmodel.cpp
@@ -187,6 +187,11 @@ public Q_SLOTS:
      * @param confId
      */
     void slotCallAddedToConference(const std::string& callId, const std::string& confId);
+    /**
+     * Listen from CallbacksHandler when a conference is deleted.
+     * @param callId
+     */
+    void slotConferenceRemoved(const std::string& callId);
 
 };
 
@@ -295,8 +300,12 @@ ConversationModel::selectConversation(const std::string& uid) const
 
     auto& conversation = pimpl_->conversations.at(conversationIdx);
     try  {
-        auto call = owner.callModel->getCall(conversation.callId);
-        switch (call.status) {
+        if (not conversation.confId.empty()) {
+            emit pimpl_->behaviorController.showCallView(owner.id, conversation);
+            qDebug() << "   @@@  " << conversation.confId.c_str();
+        } else {
+            auto call = owner.callModel->getCall(conversation.callId);
+            switch (call.status) {
             case call::Status::INCOMING_RINGING:
             case call::Status::OUTGOING_RINGING:
             case call::Status::CONNECTING:
@@ -309,6 +318,7 @@ ConversationModel::selectConversation(const std::string& uid) const
             case call::Status::CONNECTED:
             case call::Status::IN_PROGRESS:
                 // We are currently receiving a call
+                qDebug() << " %%%%%%%%%%%%%%%%%%%%% TEST";
                 emit pimpl_->behaviorController.showCallView(owner.id, conversation);
                 break;
             case call::Status::INVALID:
@@ -320,6 +330,7 @@ ConversationModel::selectConversation(const std::string& uid) const
             default:
                 // We are not in a call, show the chatview
                 emit pimpl_->behaviorController.showChatView(owner.id, conversation);
+            }
         }
     } catch (const std::out_of_range&) {
         emit pimpl_->behaviorController.showChatView(owner.id, conversation);
@@ -600,6 +611,10 @@ ConversationModelPimpl::ConversationModelPimpl(const ConversationModel& linked,
             &lrc::api::NewCallModel::callAddedToConference,
             this,
             &ConversationModelPimpl::slotCallAddedToConference);
+    connect(&callbacksHandler,
+            &CallbacksHandler::conferenceRemoved,
+            this,
+            &ConversationModelPimpl::slotConferenceRemoved);
 }
 
 ConversationModelPimpl::~ConversationModelPimpl()
@@ -955,7 +970,7 @@ ConversationModelPimpl::slotCallAddedToConference(const std::string& callId, con
 {
     for (auto& conversation: conversations) {
         if (conversation.callId == callId) {
-            conversation.callId = confId;
+            conversation.confId = confId;
             dirtyConversations = true;
             emit linked.selectConversation(conversation.uid);
         }
@@ -1009,6 +1024,49 @@ ConversationModelPimpl::slotUpdateInteractionStatus(const std::string& accountId
             }
         }
     }
+}
+
+void
+ConversationModel::hangUpFor(const std::string& uid)
+{
+    auto conversationIdx = pimpl_->indexOf(uid);
+    if (conversationIdx == -1)
+        return;
+
+    auto& conversationInfo = pimpl_->conversations.at(conversationIdx);
+
+    if (not conversationInfo.confId.empty()) {
+        owner.callModel->hangUp(conversationInfo.confId);
+        conversationInfo.confId = "";
+    }
+    else if (not conversationInfo.callId.empty()) {
+        owner.callModel->hangUp(conversationInfo.callId);
+        conversationInfo.callId = "";
+    }
+}
+
+void
+ConversationModelPimpl::slotConferenceRemoved(const std::string& confId)
+{
+    // Get conversation
+    auto i = std::find_if(
+        conversations.begin(), conversations.end(),
+        [confId](const conversation::Info& conversation) {
+            return conversation.confId == confId;
+        });
+
+    if (i == conversations.end())
+        return;
+
+    auto& conversationInfo = *i;
+    auto uid = conversationInfo.uid;
+
+
+    if (not conversationInfo.confId.empty())
+        conversationInfo.confId = "";
+
+    linked.selectConversation(uid);
+
 }
 
 } // namespace lrc
