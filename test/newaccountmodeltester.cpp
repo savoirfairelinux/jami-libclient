e76b7b6702e1cb7717e4105a19d2fc1ccc0e8432
Nicolas Jager <nicolas.jager@savoirfairelinux.com>
2017-11-29 11:32:09 -0500

    test: add NewAccountModelTester
    - this class is a stub.
    
    Change-Id: Ie585f3ca375443f968fbaa4cf6a02bc4d15dbfeb

---
+++ test/newaccountmodeltester.cpp
@@ -0,0 +1,54 @@
+/****************************************************************************
+ *   Copyright (C) 2017 Savoir-faire Linux                                  *
+ *   Author: Nicolas Jäger <nicolas.jager@savoirfairelinux.com>             *
+ *                                                                          *
+ *   This library is free software; you can redistribute it and/or          *
+ *   modify it under the terms of the GNU Lesser General Public             *
+ *   License as published by the Free Software Foundation; either           *
+ *   version 2.1 of the License, or (at your option) any later version.     *
+ *                                                                          *
+ *   This library is distributed in the hope that it will be useful,        *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of         *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU      *
+ *   Lesser General Public License for more details.                        *
+ *                                                                          *
+ *   You should have received a copy of the GNU General Public License      *
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.  *
+ ***************************************************************************/
+#include "newaccountmodeltester.h"
+
+// Qt
+#include <QString>
+#include "utils/waitforsignalhelper.h"
+
+// lrc
+#include "api/lrc.h"
+
+namespace ring
+{
+namespace test
+{
+
+CPPUNIT_TEST_SUITE_REGISTRATION(NewAccountModelTester);
+
+NewAccountModelTester::NewAccountModelTester()
+: lrc_(new lrc::api::Lrc())
+{
+
+}
+
+void
+NewAccountModelTester::setUp()
+{
+
+}
+
+
+void
+NewAccountModelTester::tearDown()
+{
+
+}
+
+} // namespace test
+} // namespace ring
