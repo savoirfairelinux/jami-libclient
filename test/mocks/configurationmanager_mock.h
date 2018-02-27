9159174e69997262cdcdb564ab72dfa3c08ec1c1
Sébastien Blin <sebastien.blin@savoirfairelinux.com>
2018-02-27 16:45:12 -0500

    account: link to the new exportToFile method
    Change-Id: I78d37f8f125f40d92cee11ac7e95ffc5fa44dfb3
    Reviewed-by: Philippe Gorley <philippe.gorley@savoirfairelinux.com>

--- test/mocks/configurationmanager_mock.h
+++ test/mocks/configurationmanager_mock.h
@@ -97,6 +97,13 @@ public Q_SLOTS: // METHODS
         return false;
     }
 
+    bool exportOnFile(const QString& accountId, const QString& destinationPath)
+    {
+        Q_UNUSED(accountId)
+        Q_UNUSED(password)
+        return false;
+    }
+
     MapStringString getKnownRingDevices(const QString& accountId)
     {
         Q_UNUSED(accountId)
