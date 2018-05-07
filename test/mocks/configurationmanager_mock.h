75c4633dc01b1a7b00948f8da72a9da74a0b8c74
Sébastien Blin <sebastien.blin@savoirfairelinux.com>
2018-05-07 16:37:01 -0400

    tests: correct license headers
    Licensess in the mocks directory are incorrect. Change the license
    to GPLv3 and fix authors.
    
    Change-Id: I9a121f4ba4c615d40184c5f77b7eb479cfddc982

--- test/mocks/configurationmanager_mock.h
+++ test/mocks/configurationmanager_mock.h
@@ -1,21 +1,25 @@
-/******************************************************************************
-*   Copyright (C) 2014-2018 Savoir-faire Linux                                 *
-*   Author : Philippe Groarke <philippe.groarke@savoirfairelinux.com>        *
-*   Author : Alexandre Lision <alexandre.lision@savoirfairelinux.com>        *
-*                                                                            *
-*   This library is free software; you can redistribute it and/or            *
-*   modify it under the terms of the GNU Lesser General Public               *
-*   License as published by the Free Software Foundation; either             *
-*   version 2.1 of the License, or (at your option) any later version.       *
-*                                                                            *
-*   This library is distributed in the hope that it will be useful,          *
-*   but WITHOUT ANY WARRANTY; without even the implied warranty of           *
-*   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU        *
-*   Lesser General Public License for more details.                          *
-*                                                                            *
-*   You should have received a copy of the Lesser GNU General Public License *
-*   along with this program.  If not, see <http://www.gnu.org/licenses/>.    *
-*****************************************************************************/
+/*
+ *  Copyright (C) 2017-2018 Savoir-faire Linux Inc.
+ *
+ *  Authors: Sébastien Blin <sebastien.blin@savoirfairelinux.com>
+ *           Guillaume Roguez <guillaume.roguez@savoirfairelinux.com>
+ *           Anthony Léonard <anthony.leonard@savoirfairelinux.com>
+ *           Hugo Lefeuvre <hugo.lefeuvre@savoirfairelinux.com>
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA.
+ */
 #pragma once
 
 #include <QtCore/QObject>
