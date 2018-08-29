9ccb2bce9f47214f3f388ba88cd5c451b2e3b446
Hugo Lefeuvre <hugo.lefeuvre@savoirfairelinux.com>
2018-08-29 12:16:35 -0400

    ringing timeout: implement let a message feature
    Change-Id: I03882ada77366332443b835760034c8e370d9694

--- src/conversationmodel.cpp
+++ src/conversationmodel.cpp
@@ -430,6 +430,14 @@ ConversationModel::selectConversation(const std::string& uid) const
     if (not conversation.confId.empty()) {
         emit pimpl_->behaviorController.showCallView(owner.id, conversation);
     } else if (conversation.callId.empty()) {
+        if (!conversation.previousCallId.empty()) {
+            auto previousCall = owner.callModel->getCall(conversation.previousCallId);
+            // TODO this breaks clients without send a message capability
+            if (previousCall.status == call::Status::ENDED_RECORDING_MESSAGE) {
+                return;
+            }
+        }
+
         emit pimpl_->behaviorController.showChatView(owner.id, conversation);
     } else {
         try  {
@@ -447,6 +455,9 @@ ConversationModel::selectConversation(const std::string& uid) const
             case call::Status::IN_PROGRESS:
                 // We are currently receiving a call
                 emit pimpl_->behaviorController.showCallView(owner.id, conversation);
+                break;
+            case call::Status::PEER_BUSY:
+                emit pimpl_->behaviorController.showLetMessageView(owner.id, conversation);
                 break;
             default: // INVALID, INACTIVE, ENDED, TERMINATING
                 // We are not in a call, show the chatview
@@ -523,6 +534,7 @@ ConversationModelPimpl::placeCall(const std::string& uid, bool isAudioOnly)
                 case call::Status::INVALID:
                 case call::Status::INACTIVE:
                 case call::Status::ENDED:
+                case call::Status::PEER_BUSY:
                 case call::Status::TERMINATING:
                 default:
                     break;
