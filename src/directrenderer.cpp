a860d3cd3f1d2cdeb5ac9eaa0f89b5b66332f3ba
Sébastien Blin <sebastien.blin@savoirfairelinux.com>
2018-10-18 23:15:38 -0400

    api: add new media api
    Migrate all media API to new models.
    
    Gitlab: #394
    Change-Id: Ida00feb0afefa4f0ce39091811da12097f03ba4d

--- src/directrenderer.cpp
+++ src/directrenderer.cpp
@@ -16,6 +16,8 @@
  *   You should have received a copy of the GNU General Public License      *
  *   along with this program.  If not, see <http://www.gnu.org/licenses/>.  *
  ***************************************************************************/
+#ifdef ENABLE_LIBWRAP
+
 #include "directrenderer.h"
 
 #include <QtCore/QDebug>
@@ -146,3 +148,5 @@ Video::Renderer::ColorSpace Video::DirectRenderer::colorSpace() const
 }
 
 #include <directrenderer.moc>
+
+#endif
--- src/newaccountmodel.cpp
+++ src/newaccountmodel.cpp
@@ -29,9 +29,9 @@
 
 // new LRC
 #include "api/lrc.h"
-#include "api/newcallmodel.h"
 #include "api/contactmodel.h"
 #include "api/conversationmodel.h"
+#include "api/newcallmodel.h"
 #include "api/newcodecmodel.h"
 #include "api/newdevicemodel.h"
 #include "api/behaviorcontroller.h"
