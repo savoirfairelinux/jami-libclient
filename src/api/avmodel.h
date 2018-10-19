c8eb650a20b928008f35cc7626286bac4a6afca6
Sébastien Blin <sebastien.blin@savoirfairelinux.com>
2018-10-18 23:15:38 -0400

    api: add new media api
    Change-Id: Ida00feb0afefa4f0ce39091811da12097f03ba4d

--- src/api/avmodel.h
+++ src/api/avmodel.h
@@ -1,6 +1,7 @@
 /****************************************************************************
  *   Copyright (C) 2018 Savoir-faire Linux                                  *
  *   Author: Hugo Lefeuvre <hugo.lefeuvre@savoirfairelinux.com>             *
+ *   Author: Sébastien Blin <sebastien.blin@savoirfairelinux.com>           *
  *                                                                          *
  *   This library is free software; you can redistribute it and/or          *
  *   modify it under the terms of the GNU Lesser General Public             *
@@ -18,18 +19,23 @@
 #pragma once
 
 // std
+#include <map>
 #include <memory>
 #include <string>
+#include <vector>
 
 // Qt
 #include <qobject.h>
 
 // LRC
+#include "api/account.h"
+#include "api/newvideo.h"
 #include "typedefs.h"
 
 namespace lrc
 {
 
+class CallbacksHandler;
 class AVModelPimpl;
 
 namespace api
@@ -38,8 +44,110 @@ namespace api
 class LIB_EXPORT AVModel : public QObject {
     Q_OBJECT
 public:
-    AVModel();
+    AVModel(const CallbacksHandler& callbacksHandler);
     ~AVModel();
+
+    /**
+     * Get if hardware decoding is enabled
+     * @return hardware decoding enabled
+     */
+    bool getDecodingAccelerated() const;
+    /**
+     * Enable/disable hardware decoding
+     * @param if hardware decoding enabled
+     */
+    void setDecodingAccelerated(bool accelerate);
+
+    /**
+     * Get video devices
+     * @return list of devices
+     */
+    std::vector<std::string> getDevices() const;
+    /**
+     * Retrieve current default video device
+     * @return current default video device name
+     */
+    std::string getDefaultDeviceName() const;
+    /**
+     * Set new default video device
+     * @param name of the device
+     */
+    void setDefaultDevice(const std::string& name);
+    /**
+     * Retrieve current framerate/resolution/etc of a device
+     * @param name of the device
+     * @return settings of the device
+     */
+    video::Settings getDeviceSettings(const std::string& name) const;
+    /**
+     * Set device settings
+     * @param video::Settings
+     */
+    void setDeviceSettings(video::Settings& settings);
+    /**
+     * Retrieve all framerate/resolution/etc possibilities of a device
+     * @param name of the device
+     * @return possibilities of the device
+     */
+    video::Capabilities getDeviceCapabilities(const std::string& name) const;
+
+    /**
+     * Get supported audio managers
+     * @return supported audio managers
+     */
+    std::vector<std::string> getSupportedAudioManagers() const;
+    /**
+     * Get current audio manager
+     * @return current audio manager
+     */
+    std::string getAudioManager() const;
+    /**
+     * Get current audio outputs
+     * @return audio outputs
+     */
+    std::vector<std::string> getAudioOutputDevices() const;
+    /**
+     * Get current audio inputs
+     * @return audio inputs
+     */
+    std::vector<std::string> getAudioInputDevices() const;
+    /**
+     * Get current ringtone device
+     * @return current ringtone device
+     */
+    std::string getRingtoneDevice() const;
+    /**
+     * Get current output device
+     * @return current output device
+     */
+    std::string getOutputDevice() const;
+    /**
+     * Get current input device
+     * @return current input device
+     */
+    std::string getInputDevice() const;
+    /**
+     * Set current audio manager
+     * @param name of the new audio manager
+     * @return if the operation is successful
+     */
+    bool setAudioManager(const std::string& name);
+    /**
+     * Set current ringtone device
+     * @param name of the new ringtone device
+     */
+    void setRingtoneDevice(const std::string& name);
+    /**
+     * Set current output device
+     * @param name of the new output device
+     */
+    void setOutputDevice(const std::string& name);
+    /**
+     * Set current input device
+     * @param name of the new input device
+     */
+    void setInputDevice(const std::string& name);
+
     /**
      * Stop local record at given path
      * @param path
@@ -50,6 +158,13 @@ public:
      * @param audioOnly
      */
     std::string startLocalRecorder(const bool& audioOnly) const;
+
+    void startPreview();
+    void stopPreview();
+
+Q_SIGNALS:
+    void rendererStarted(const std::string& id);
+    void rendererStopped(const std::string& id);
 
 private:
     std::unique_ptr<AVModelPimpl> pimpl_;
