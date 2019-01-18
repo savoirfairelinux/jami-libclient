5026200d3cbc59951a671ad50e99141df0c7412b
Andreas Traczyk <andreas.traczyk@savoirfairelinux.com>
2019-01-18 15:39:56 -0500

    audio: bind to audio metering api
    Change-Id: I1b88605c4dd058babe98b323b5c247cb63b4a676

--- src/callbackshandler.cpp
+++ src/callbackshandler.cpp
@@ -206,6 +206,12 @@ CallbacksHandler::CallbacksHandler(const Lrc& parent)
             this,
             &CallbacksHandler::slotDeviceEvent,
             Qt::QueuedConnection);
+
+    connect(&ConfigurationManager::instance(),
+            &ConfigurationManagerInterface::audioMeterReceived,
+            this,
+            &CallbacksHandler::slotAudioMeterReceived,
+            Qt::QueuedConnection);
 }
 
 CallbacksHandler::~CallbacksHandler()
@@ -493,5 +499,10 @@ CallbacksHandler::slotDeviceEvent()
     emit deviceEvent();
 }
 
+void
+CallbacksHandler::slotAudioMeterReceived(const QString& id, float level)
+{
+    emit parent.getBehaviorController().audioLevelReceived(id.toStdString(), level);
+}
 
 } // namespace lrc
