09f817ee3242dfc5465548cd7bbd499d5fd3b7a8
Kateryna Kostiuk <kateryna.kostiuk@savoirfairelinux.com>
2019-03-28 16:40:19 -0400

    conversation: cancel message
    - support cancel message
    - remove interaction status synchronization on start. Now we could
    have status SENDING for long time, it will be updated when receiving
    signal from daemon.
    
    Change-Id: I8a2ffe5dd90e6d4175fc4ac4191666c0751ced7c

--- src/qtwrapper/configurationmanager_wrap.h
+++ src/qtwrapper/configurationmanager_wrap.h
@@ -621,6 +621,14 @@ public Q_SLOTS: // METHODS
         return DRing::getMessageStatus(id);
     }
 
+    int getMessageStatus(const QString&  accountID, uint64_t id) {
+        return DRing::getMessageStatus(accountID.toStdString(), id);
+    }
+
+    bool cancelMessage(const QString&  accountID, uint64_t id) {
+        return DRing::cancelMessage(accountID.toStdString(), id);
+    }
+
     void connectivityChanged() {
         DRing::connectivityChanged();
     }
