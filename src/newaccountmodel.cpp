2121e7e46e0f7579e66c45a722ba5000365fbf06
Mingrui Zhang <mingrui.zhang@savoirfairelinux.com>
2019-05-28 09:43:37 -0400

    config: add accountPeerDiscovery parameter
    Change-Id: I451f02093da629e552482d34cd28aab274e2c4db

--- src/newaccountmodel.cpp
+++ src/newaccountmodel.cpp
@@ -672,6 +672,7 @@ account::Info::fromDetails(const MapStringString& details)
     confProperties.proxyServer                          = toStdString(details[ConfProperties::PROXY_SERVER]);
     confProperties.proxyPushToken                       = toStdString(details[ConfProperties::PROXY_PUSH_TOKEN]);
     confProperties.peerDiscovery                        = toBool(details[ConfProperties::DHT_PEER_DISCOVERY]);
+    confProperties.accountDiscovery                     = toBool(details[ConfProperties::ACCOUNT_PEER_DISCOVERY]);
     // Audio
     confProperties.Audio.audioPortMax                   = toInt(details[ConfProperties::Audio::PORT_MAX]);
     confProperties.Audio.audioPortMin                   = toInt(details[ConfProperties::Audio::PORT_MIN]);
@@ -770,6 +771,7 @@ account::ConfProperties_t::toDetails() const
     details[ConfProperties::PROXY_SERVER]               = toQString(this->proxyServer);
     details[ConfProperties::PROXY_PUSH_TOKEN]           = toQString(this->proxyPushToken);
     details[ConfProperties::DHT_PEER_DISCOVERY]         = toQString(this->peerDiscovery);
+    details[ConfProperties::ACCOUNT_PEER_DISCOVERY]     = toQString(this->accountDiscovery);
     // Audio
     details[ConfProperties::Audio::PORT_MAX]            = toQString(this->Audio.audioPortMax);
     details[ConfProperties::Audio::PORT_MIN]            = toQString(this->Audio.audioPortMin);
