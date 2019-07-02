cb2c1f3806485b61198e4b54bedf45b102fdb537
Mingrui Zhang <mingrui.zhang@savoirfairelinux.com>
2019-07-02 14:45:21 -0400

    lrc: add sip status code message map and get method
    - let the client retrieve corresponding messages
    
    Change-Id: I09e697043274cca495a19440b75c31e6900264c2

--- src/api/newcallmodel.h
+++ src/api/newcallmodel.h
@@ -192,6 +192,13 @@ public:
      */
     static void hangupCallsAndConferences();
 
+    /**
+     * Extract Status Message From Status Map
+     * @param statusCode
+     * @return status message
+     */
+    static std::string getSIPCallStatusString(const short& statusCode);
+
 Q_SIGNALS:
     /**
      * Emitted when a call state changes
