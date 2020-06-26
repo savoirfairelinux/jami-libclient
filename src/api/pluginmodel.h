9bd3a9cc91c9202745bbd6c7fb737cee5c8b5bd7
agsantos <aline.gondimsantos@savoirfairelinux.com>
2020-06-26 13:46:55 -0400

    plugin: add plugin dbus interface
    Change-Id: I5de694b3fe7ebb177c3667cb9feb4aa4950fea7b

---
+++ src/api/pluginmodel.h
@@ -0,0 +1,76 @@
+/****************************************************************************
+ *    Copyright (C) 2018-2020 Savoir-faire Linux Inc.                                  *
+ *   Author: Hugo Lefeuvre <hugo.lefeuvre@savoirfairelinux.com>             *
+ *   Author: Sébastien Blin <sebastien.blin@savoirfairelinux.com>           *
+ *                                                                          *
+ *   This library is free software; you can redistribute it and/or          *
+ *   modify it under the terms of the GNU Lesser General Public             *
+ *   License as published by the Free Software Foundation; either           *
+ *   version 2.1 of the License, or (at your option) any later version.     *
+ *                                                                          *
+ *   This library is distributed in the hope that it will be useful,        *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of         *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU      *
+ *   Lesser General Public License for more details.                        *
+ *                                                                          *
+ *   You should have received a copy of the GNU General Public License      *
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.  *
+ ***************************************************************************/
+#pragma once
+
+// std
+#include <memory>
+#include <string>
+#include <vector>
+
+// Qt
+#include <qobject.h>
+
+// LRC
+#include "typedefs.h"
+
+namespace lrc
+{
+
+class PluginModelPimpl;
+
+namespace api
+{
+
+class LIB_EXPORT PluginModel : public QObject {
+    Q_OBJECT
+public:
+    PluginModel();
+    ~PluginModel();
+
+    /**
+     * Enable/disable plugins
+     * @param if plugin enabled
+     */
+    void setPluginsEnabled(bool enable);
+
+    /**
+     * Get if plugins are enabled
+     * @return plugins enabled
+     */
+    bool getPluginsEnabled() const;
+
+    /**
+     * Install plugin
+     * @return plugin installed
+     */
+    bool installPlugin(const QString& jplPath, bool force);
+
+    /**
+     * Uninstall plugin
+     * @return plugin uninstalled
+     */
+    bool uninstallPlugin(const QString& rootPath);
+
+// Q_SIGNALS:
+private:
+    std::unique_ptr<PluginModelPimpl> pimpl_;
+};
+
+} // namespace api
+} // namespace lrc
