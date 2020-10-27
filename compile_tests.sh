e918dbbfacb64f88e446029e2e0ec30e9558be56
ababi <albert.babi@savoirfairelinux.com>
2020-10-27 13:47:48 +0100

    misc: cqfd support for client and tests
    Change-Id: I7fe20fd550586f2a40bbb65db1af4eeec8d5e155

---
+++ compile_tests.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+# Build lrc, client-qt and pass tests
+
+# Get number of CPU available
+cpuCount=$(nproc || echo -n 4)
+
+# Project directories
+topDir=$(pwd)/..
+echo "Project root dir: "${topDir}
+
+installDir=$topDir/install
+daemonDir=$topDir/daemon
+lrcDir=$topDir/lrc
+clientDir=$topDir/client-qt
+
+# Build lrc
+cd ${lrcDir}
+mkdir -p build
+cd build
+echo "Building lrc in "$PWD
+cmake .. -DCMAKE_INSTALL_PREFIX=$installDir/lrc \
+      -DRING_INCLUDE_DIR=$daemonDir/src/dring \
+      -DRING_XML_INTERFACES_DIR=$daemonDir/bin/dbus
+make
+make install
+
+# Build client and tests
+cd $clientDir
+mkdir -p build
+cd build
+echo "Building client in "$PWD
+pandoc -f markdown -t html5 -o ../changelog.html ../changelog.md
+cmake ..
+make -j${cpuCount}
+
+# Pass Tests
+cd tests
+./unittests
+./qml_tests -input $clientDir/tests/qml
