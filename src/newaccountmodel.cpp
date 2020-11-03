8f282fe14fb183e7a6e3b7de4b2ed81fd612b68c
Ming Rui Zhang <mingrui.zhang@savoirfairelinux.com>
2020-11-03 10:51:38 -0500

    newaccountmodel: remove "ring:" prefix introduced by prepend function
    Gitlab: #446
    Change-Id: If2b685e1c8ed26d564ac0604f4d5b9575499db11

--- src/newaccountmodel.cpp
+++ src/newaccountmodel.cpp
@@ -239,8 +239,7 @@ NewAccountModel::setAccountConfig(const QString& accountId,
                                         ? QString(ProtocolNames::RING)
                                         : QString(ProtocolNames::SIP);
     if (accountInfo.profileInfo.type == profile::Type::RING) {
-        details[ConfProperties::USERNAME] = accountInfo.profileInfo.uri.prepend(
-            (accountInfo.profileInfo.type == profile::Type::RING) ? "ring:" : "");
+        details[ConfProperties::USERNAME] = accountInfo.profileInfo.uri;
     } else if (accountInfo.profileInfo.type == profile::Type::SIP) {
         VectorMapStringString finalCred;
 
@@ -735,7 +734,9 @@ NewAccountModelPimpl::addToAccounts(const QString& accountId, std::shared_ptr<Da
 
     // Init models for this account
     newAccInfo.accountModel = &linked;
-    newAccInfo.callModel = std::make_unique<NewCallModel>(newAccInfo, callbacksHandler, behaviorController);
+    newAccInfo.callModel = std::make_unique<NewCallModel>(newAccInfo,
+                                                          callbacksHandler,
+                                                          behaviorController);
     newAccInfo.contactModel = std::make_unique<ContactModel>(newAccInfo,
                                                              *db,
                                                              callbacksHandler,
