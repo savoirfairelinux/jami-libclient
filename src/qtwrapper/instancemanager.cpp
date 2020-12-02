fd315a7a6fc4a8b68ff5265ca0c7a5c34de764b5
Kateryna Kostiuk <kateryna.kostiuk@savoirfairelinux.com>
2020-12-02 15:17:13 -0500

    swarm: link API
    Change-Id: I5dcdc95d659cc722159146bfb713a9be7672c07c

--- src/qtwrapper/instancemanager.cpp
+++ src/qtwrapper/instancemanager.cpp
@@ -37,7 +37,8 @@ InstanceManagerInterface::InstanceManagerInterface()
     using DRing::ConfigurationSignal;
     using DRing::PresenceSignal;
     using DRing::DataTransferSignal;
-    using DRing::DebugSignal;
+    using DRing::ConfigurationSignal;
+    using DRing::ConversationSignal;
 
 #ifdef ENABLE_VIDEO
     using DRing::VideoSignal;
@@ -57,6 +58,7 @@ InstanceManagerInterface::InstanceManagerInterface()
 #ifdef ENABLE_VIDEO
     registerSignalHandlers(VideoManager::instance().videoHandlers);
 #endif
+    registerSignalHandlers(ConfigurationManager::instance().conversationsHandlers);
 
     if (!DRing::start())
         printf("Error initializing daemon\n");
