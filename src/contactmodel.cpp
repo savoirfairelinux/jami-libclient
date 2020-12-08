efad3d4ef8870c1b14b3d90d35e195ba65078f14
Ming Rui Zhang <mingrui.zhang@savoirfairelinux.com>
2020-12-08 10:28:47 -0500

    vcard: use UTF-8 codec when receiving contact profile
    Gitlab: #450
    Change-Id: I2e9bd8dbf08f8bc1e7a01e9d16795381f58f054f

--- src/contactmodel.cpp
+++ src/contactmodel.cpp
@@ -1161,6 +1161,8 @@ ContactModelPimpl::slotProfileReceived(const QString& accountId,
     if (!vCardFile.open(QIODevice::ReadOnly | QIODevice::Text))
         return;
     QTextStream in(&vCardFile);
+    in.setCodec("UTF-8");
+
     auto vCard = in.readAll();
     vCardFile.remove();
 
