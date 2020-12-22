dc10b243e56a41de74c07ecc59ff9a016e7155b8
Sébastien Blin <sebastien.blin@savoirfairelinux.com>
2020-12-22 14:53:45 -0500

    conversationmodel: tmp fix
    Change-Id: I4c6d749f10cf1ad36720cdbc7721c651844530dd

--- src/api/conversationmodel.h
+++ src/api/conversationmodel.h
@@ -298,7 +298,7 @@ public:
      * @param conversationId conversation's id
      * @return conversation info
      */
-    MapStringString getConversationInfos(const QString& conversationId);
+    MapStringString conversationInfos(const QString& conversationId);
     /**
      * create a new swarm conversation
      * @param participants  conversation's participants
@@ -310,7 +310,7 @@ public:
      * @param conversationId conversation's id
      * @param info
      */
-    void updateConversationInfo(const QString& conversationId, MapStringString info);
+    void updateConversationInfos(const QString& conversationId, MapStringString info);
 
 Q_SIGNALS:
     /**
