c0cc6ccc23716706261dd2df60325389bb8857e0
Jenkins <jenkins@ring-packaging.cx>
2021-01-04 16:17:38 -0500

    i18n: automatic bump
    Change-Id: Ifbb0293571f936ff74b19ca8c377d22a4557955b

--- translations/lrc_nb.ts
+++ translations/lrc_nb.ts
@@ -462,7 +462,7 @@
     <message>
         <location filename="../src/chatview.cpp" line="65"/>
         <source>Waiting for contact</source>
-        <translation type="unfinished"/>
+        <translation>Venter på kontakt</translation>
     </message>
     <message>
         <location filename="../src/chatview.cpp" line="66"/>
