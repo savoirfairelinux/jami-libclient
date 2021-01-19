6684eff30be209402be1a0ea6179f46710f2f0bc
ababi <albert.babi@savoirfairelinux.com>
2021-01-19 11:53:32 +0100

    chatview: follow desgin changes for invitations
    Gitlab: #241
    
    Change-Id: Iab347bac67e1e658e8d42ae49ddd0387f080e6a4

--- src/web-chatview/chatview.js
+++ src/web-chatview/chatview.js
@@ -39,6 +39,10 @@ const unbanButton = document.getElementById("unbanButton")
 const acceptButton = document.getElementById("acceptButton")
 const refuseButton = document.getElementById("refuseButton")
 const blockButton = document.getElementById("blockButton")
+const acceptConvButton = document.getElementById("acceptConvButton")
+const refuseConvButton = document.getElementById("refuseConvButton")
+const blockConvButton = document.getElementById("blockConvButton")
+
 const callButtons = document.getElementById("callButtons")
 const sendButton = document.getElementById("sendButton")
 sendButton.style.display = "none"
@@ -56,12 +60,19 @@ const messageBarInput = document.getElementById("message")
 const messageBarContainer = document.getElementById("message_bar_container")
 const addToConvButton = document.getElementById("addToConversationsButton")
 const invitation = document.getElementById("invitation")
+const invitationConversation = document.getElementById("invitation_conversation")
 const inviteImage = document.getElementById("invite_image")
+const inviteConvImage = document.getElementById("invite_conv_image")
+
 const navbar = document.getElementById("navbar")
-const invitationText = document.getElementById("text")
+const invitationText = document.getElementById("invitation_text")
+const joinText = document.getElementById("join_text")
+const invitationNoteText = document.getElementById("invitation_note")
+
 var   messages = document.getElementById("messages")
 var   sendContainer = document.getElementById("data_transfer_send_container")
 var   wrapperOfNavbar = document.getElementById("wrapperOfNavbar")
+var   typing_indicator_container_div = document.getElementById("typing_indicator_container")
 
 /* States: allows us to avoid re-doing something if it isn't meaningful */
 var displayLinksEnabled = true
@@ -181,6 +192,9 @@ function set_titles() {
         acceptButton.title = i18nStringData["Accept"]
         refuseButton.title = i18nStringData["Refuse"]
         blockButton.title = i18nStringData["Block"]
+        acceptConvButton.innerHTML = i18nStringData["Accept"]
+        refuseConvButton.innerHTML = i18nStringData["Refuse"]
+        blockConvButton.innerHTML = i18nStringData["Block"]
     } else {
         backButton.title = i18n.gettext("Hide chat view")
         placeCallButton.title = i18n.gettext("Place video call")
@@ -343,6 +357,63 @@ function update_chatview_frame(accountEnabled, banned, temporary, alias, bestid)
     navbar.style.display = ""
 }
 
+
+/**
+ * Hide or show invitation to a conversation.
+ *
+ * Invitation is hidden if no contactAlias/invalid alias is passed.
+ * Otherwise, invitation div is updated.
+ *
+ * @param contactAlias
+ * @param contactId
+ */
+/* exported showConversationInvitation */
+function showInvitation(contactAlias, contactId) {
+    console.error("show", contactAlias, contactId)
+    if (!contactAlias) {
+        if (hasInvitation) {
+            hasInvitation = false
+            invitationConversation.style.visibility = ""
+        }
+    } else {
+        if (!inviteImage.classList.contains("sender_image")) {
+            inviteImage.classList.add("sender_image")
+        }
+        if (use_qt) {
+            if (!inviteImage.classList.contains(`sender_image_${contactId}`)) {
+                inviteImage.classList.add(`sender_image_${contactId}`)
+            }
+        } else {
+            const className = `sender_image_${contactId}`.replace(/@/g, "_").replace(/\./g, "_")
+            if (!inviteImage.classList.contains(className)) {
+                inviteImage.classList.add(className)
+            }
+        }
+        invitationText.innerHTML = contactAlias + " " + (use_qt ?
+            i18nStringData["has sent you a conversation request."] :
+            i18n.sprintf(i18n.gettext("%s has sent you a conversation request."), contactAlias))
+            + "<br/>"
+
+        joinText.innerHTML = "<b>" + (use_qt ?
+            i18nStringData["Hello, do you want to join the conversation?"] :
+            i18n.gettext("Hello, do you want to join the conversation?"))
+            + "</b><br/>"
+
+        invitationNoteText.innerHTML = (use_qt ?
+            i18nStringData["Note: you can automatically accept this invitation by sending a message."] :
+            i18n.gettext("Note: you can automatically accept this invitation by sending a message."))
+            + "<br/>"
+
+        //typing_indicator_container_div.style.visibility = "hidden"
+        messages.style.visibility = "hidden"
+        hasInvitation = true
+        invitationConversation.style.visibility = "visible"
+    }
+}
+
+
+
+
 /**
  * Hide or show invitation.
  *
@@ -353,7 +424,7 @@ function update_chatview_frame(accountEnabled, banned, temporary, alias, bestid)
  * @param contactId
  */
 /* exported showInvitation */
-function showInvitation(contactAlias, contactId) {
+function showInvitation2(contactAlias, contactId) {
     if (!contactAlias) {
         if (hasInvitation) {
             hasInvitation = false
@@ -2521,7 +2592,7 @@ function setTheme(theme) {
     root.setAttribute("style", "\
         --jami-light-blue: rgba(59, 193, 211, 0.3);\
         --jami-dark-blue: #003b4e;\
-        --jami-green: #219d55;\
+        --jami-green: #1ed0a8;\
         --jami-green-hover: #1f8b4c;\
         --jami-red: #dc2719;\
         --jami-red-hover: #b02e2c;\
