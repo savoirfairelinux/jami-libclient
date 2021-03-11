c1f8df96c5637d6c17eb77f5be233902da7b2c1a
kkostiuk <kateryna.kostiuk@savoirfairelinux.com>
2021-03-11 16:31:10 -0500

    swarm: transfer model per account
    Change-Id: Iac73a4ae43fec2228e04f3156239ca9a4ebeab85

--- src/newaccountmodel.cpp
+++ src/newaccountmodel.cpp
@@ -29,6 +29,7 @@
 #include "api/newcodecmodel.h"
 #include "api/newdevicemodel.h"
 #include "api/behaviorcontroller.h"
+#include "api/datatransfermodel.h"
 #include "authority/storagehelper.h"
 #include "callbackshandler.h"
 #include "database.h"
