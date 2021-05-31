34ab3fb46f1368285b62f9f93b4d24affb086f56
agsantos <aline.gondimsantos@savoirfairelinux.com>
2021-05-31 17:03:56 -0400

    conference: use participantInfo struct
    Change-Id: I972a8db3e7215c1d45f084d4dc6554dcccd33c2c

--- src/newcallmodel.cpp
+++ src/newcallmodel.cpp
@@ -750,8 +750,12 @@ NewCallModelPimpl::initCallFromDaemon()
             callInfo->videoMuted = details["VIDEO_MUTED"] == "true";
             callInfo->audioMuted = details["AUDIO_MUTED"] == "true";
             callInfo->type = call::Type::DIALOG;
+
+            callInfo->participantsInfos.clear();
             VectorMapStringString infos = CallManager::instance().getConferenceInfos(callId);
-            callInfo->participantsInfos = infos;
+            for (const auto& item : infos) {
+                callInfo->participantsInfos.push_back(call::ParticipantInfo(item, callId));
+            }
             calls.emplace(callId, std::move(callInfo));
             // NOTE/BUG: the videorenderer can't know that the client has restarted
             // So, for now, a user will have to manually restart the medias until
@@ -788,8 +792,13 @@ NewCallModelPimpl::initConferencesFromDaemon()
         if (!isForThisAccount)
             break;
         callInfo->type = call::Type::CONFERENCE;
+
+        callInfo->participantsInfos.clear();
         VectorMapStringString infos = CallManager::instance().getConferenceInfos(callId);
-        callInfo->participantsInfos = infos;
+        for (const auto& item : infos) {
+            callInfo->participantsInfos.push_back(call::ParticipantInfo(item, callId));
+        }
+
         calls.emplace(callId, std::move(callInfo));
     }
 }
@@ -903,8 +912,8 @@ NewCallModel::isModerator(const QString& confId, const QString& uri)
                            : false;
     if (!isModerator && call->second->participantsInfos.size() != 0) {
         for (const auto& participant : call->second->participantsInfos) {
-            if (participant["uri"] == uriToCheck) {
-                isModerator = participant["isModerator"] == "true";
+            if (participant.uri == uriToCheck) {
+                isModerator = participant.isModerator;
                 break;
             }
         }
@@ -1227,15 +1236,19 @@ NewCallModelPimpl::slotOnConferenceInfosUpdated(const QString& confId,
 
     qDebug() << "New conference layout received for call " << confId;
 
+    it->second->participantsInfos.clear();
+    for (auto& item : infos) {
+        it->second->participantsInfos.push_back(call::ParticipantInfo(item, confId));
+    }
+
     // if Jami, remove @ring.dht
-    it->second->participantsInfos = infos;
     for (auto& i : it->second->participantsInfos) {
-        i["uri"].replace("@ring.dht", "");
-        if (i["uri"].isEmpty()) {
+        i.uri.replace("@ring.dht", "");
+        if (i.uri.isEmpty()) {
             if (it->second->type == call::Type::CONFERENCE) {
-                i["uri"] = linked.owner.profileInfo.uri;
+                i.uri = linked.owner.profileInfo.uri;
             } else {
-                i["uri"] = it->second->peerUri.replace("ring:", "");
+                i.uri = it->second->peerUri.replace("ring:", "");
             }
         }
     }
@@ -1292,10 +1305,16 @@ NewCallModelPimpl::slotConferenceCreated(const QString& confId)
     callInfo->status = call::Status::IN_PROGRESS;
     callInfo->type = call::Type::CONFERENCE;
     callInfo->startTime = std::chrono::steady_clock::now();
-    callInfo->participantsInfos = CallManager::instance().getConferenceInfos(confId);
     callInfo->videoMuted = confVideoMuted;
+
+    callInfo->participantsInfos.clear();
+    VectorMapStringString infos = CallManager::instance().getConferenceInfos(confId);
+    for (auto& item : infos) {
+        callInfo->participantsInfos.push_back(call::ParticipantInfo(item, confId));
+    }
+
     for (auto& i : callInfo->participantsInfos)
-        i["uri"].replace("@ring.dht", "");
+        i.uri.replace("@ring.dht", "");
     calls[confId] = callInfo;
 
     foreach (const auto& call, callList) {
