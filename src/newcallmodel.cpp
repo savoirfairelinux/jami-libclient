61d613c408bfe5220e227cd66d7b0c31e9e8a54b
agsantos <aline.gondimsantos@savoirfairelinux.com>
2021-06-14 13:35:43 -0400

    multistream: verify if videoMuted in slotIncomingCallWithMedia
    Change-Id: Ie88bed382f1a13d8297128f0f6d73353269e7444

--- src/newcallmodel.cpp
+++ src/newcallmodel.cpp
@@ -988,11 +988,16 @@ NewCallModelPimpl::slotIncomingCallWithMedia(const QString& accountId,
     callInfo->status = call::Status::INCOMING_RINGING;
     callInfo->type = call::Type::DIALOG;
     callInfo->isAudioOnly = true;
+    callInfo->videoMuted = true;
     for (const auto& item : mediaList) {
-        if (item[MediaAttributeKey::MEDIA_TYPE]
-            == MediaAttributeValue::VIDEO) {
+        if (item[MediaAttributeKey::MEDIA_TYPE] == MediaAttributeValue::VIDEO) {
             callInfo->isAudioOnly = false;
-            break;
+            if (item[MediaAttributeKey::LABEL] == "video_0"
+                && item[MediaAttributeKey::ENABLED] == "true"
+                && item[MediaAttributeKey::MUTED] == "false") {
+                callInfo->videoMuted = false;
+                break;
+            }
         }
     }
     callInfo->mediaList = mediaList;
@@ -1129,21 +1134,23 @@ NewCallModelPimpl::slotMediaNegotiationStatus(const QString& callId,
     }
 
     callInfo->isAudioOnly = true;
+    callInfo->videoMuted = true;
     for (const auto& item : mediaList) {
         if (item[MediaAttributeKey::MEDIA_TYPE]
             == MediaAttributeValue::VIDEO) {
         	if (item[MediaAttributeKey::ENABLED] == "true") {
                 callInfo->isAudioOnly = false;
             }
-            callInfo->videoMuted = item[MediaAttributeKey::MUTED] == "true"
-                                    || item[MediaAttributeKey::ENABLED] == "false";
+            callInfo->videoMuted = item[MediaAttributeKey::LABEL] == "video_0"
+                                   && (item[MediaAttributeKey::MUTED] == "true"
+                                    || item[MediaAttributeKey::ENABLED] == "false");
         }
         if (item[MediaAttributeKey::MEDIA_TYPE]
             == MediaAttributeValue::AUDIO) {
-            callInfo->audioMuted = item[MediaAttributeKey::MUTED] == "true";
+            callInfo->audioMuted = item[MediaAttributeKey::LABEL] == "audio_0"
+                                   && item[MediaAttributeKey::MUTED] == "true";
         }
     }
-
     callInfo->mediaList = mediaList;
     if (callInfo->status == call::Status::IN_PROGRESS)
         emit linked.callInfosChanged(linked.owner.id, callId);
