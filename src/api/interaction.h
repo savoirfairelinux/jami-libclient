217eb5df9bf9685a17e5a963c7c2a90d9043090d
Trevor Tabah <trevor.tabah@savoirfairelinux.com>
2021-06-30 13:37:17 -0400

    messagelistmodel: derive from QAbstractListModel
    This patch allows the client to use a listview to display chat
    messages.
    
    Gitlab: #479
    
    Change-Id: I45ccbdbebe212299c4d477b69218e273014a3bd2

--- src/api/interaction.h
+++ src/api/interaction.h
@@ -1,4 +1,4 @@
-/****************************************************************************
+﻿/****************************************************************************
  *    Copyright (C) 2017-2021 Savoir-faire Linux Inc.                                  *
  *   Author: Nicolas Jäger <nicolas.jager@savoirfairelinux.com>             *
  *   Author: Sébastien Blin <sebastien.blin@savoirfairelinux.com>           *
@@ -247,6 +247,8 @@ struct Info
     Status status = Status::INVALID;
     bool isRead = false;
     MapStringString commit;
+    QVariantMap linkPreviewInfo;
+    bool linkified = false;
 
     Info() {}
 
