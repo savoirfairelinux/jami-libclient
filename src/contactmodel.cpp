ec30a0aee490a2cc4b9ca3ee35a91df35168133c
Ming Rui Zhang <mingrui.zhang@savoirfairelinux.com>
2021-09-08 10:32:53 -0400

    wip: Qt 6.2 test migration
    Change-Id: I9b73a37139d4ed2b185ffa3f6237979b8b143d71

--- src/contactmodel.cpp
+++ src/contactmodel.cpp
@@ -1207,7 +1207,6 @@ ContactModelPimpl::slotProfileReceived(const QString& accountId,
     if (!vCardFile.open(QIODevice::ReadOnly | QIODevice::Text))
         return;
     QTextStream in(&vCardFile);
-    in.setCodec("UTF-8");
 
     auto vCard = in.readAll();
 
