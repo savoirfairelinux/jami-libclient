savoirfairelinux/jami-client-qt

build: add macOS
- adapt build for macOS
- fix video on macOS

Change-Id: Ie325f3cd2955248c26ca858d37fcdc0d88c2e70d

src/distantrenderer.cpp
@@ -104,10 +104,18 @@ DistantRenderer::paint(QPainter* painter)
         if (distantImage) {
             painter->setRenderHint(QPainter::Antialiasing);
             painter->setRenderHint(QPainter::SmoothPixmapTransform);
+#if defined(__APPLE__)
 
+            auto scaledDistant = distantImage
+                                     ->scaled(size().toSize(),
+                                              Qt::KeepAspectRatio,
+                                              Qt::SmoothTransformation)
+                                     .rgbSwapped();
+#else
             auto scaledDistant = distantImage->scaled(size().toSize(),
                                                       Qt::KeepAspectRatio,
                                                       Qt::SmoothTransformation);
+#endif
             auto tempScaledWidth = static_cast<int>(scaledWidth_ * 1000);
             auto tempScaledHeight = static_cast<int>(scaledHeight_ * 1000);
             auto tempXOffset = xOffset_;

savoirfairelinux/jami-libclient

src/distantrenderer.cpp
@@ -104,10 +104,18 @@ DistantRenderer::paint(QPainter* painter)
         if (distantImage) {
             painter->setRenderHint(QPainter::Antialiasing);
             painter->setRenderHint(QPainter::SmoothPixmapTransform);
+#if defined(Q_OS_MACOS)
 
+            auto scaledDistant = distantImage
+                                     ->scaled(size().toSize(),
+                                              Qt::KeepAspectRatio,
+                                              Qt::SmoothTransformation)
+                                     .rgbSwapped();
+#else
             auto scaledDistant = distantImage->scaled(size().toSize(),
                                                       Qt::KeepAspectRatio,
                                                       Qt::SmoothTransformation);
+#endif
             auto tempScaledWidth = static_cast<int>(scaledWidth_ * 1000);
             auto tempScaledHeight = static_cast<int>(scaledHeight_ * 1000);
             auto tempXOffset = xOffset_;