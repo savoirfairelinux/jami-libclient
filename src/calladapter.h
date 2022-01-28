savoirfairelinux/jami-client-qt

recording: show message when peer starts / stops recording the call
Gitlab: #160

Change-Id: Id8125c56145cc661941f445c2f52b73fd983c97d

src/calladapter.h
@@ -96,6 +96,7 @@ signals:
                        bool isSIP,
                        bool isConferenceCall,
                        const QString& bestName);
+    void remoteRecordingChanged(const QString& label, bool state);
 
 public slots:
     void slotShowIncomingCallView(const QString& accountId,
@@ -117,6 +118,7 @@ private:
     QMetaObject::Connection onParticipantsChangedConnection_;
     QMetaObject::Connection closeIncomingCallPageConnection_;
     QMetaObject::Connection appStateChangedConnection_;
+    QMetaObject::Connection remoteRecordingChangedConnection_;
 
     /*
      * For Call Overlay

savoirfairelinux/jami-libclient

src/calladapter.h
@@ -96,6 +96,7 @@ signals:
                        bool isSIP,
                        bool isConferenceCall,
                        const QString& bestName);
+    void remoteRecordingChanged(const QStringList& peers, bool state);
 
 public slots:
     void slotShowIncomingCallView(const QString& accountId,
@@ -117,6 +118,7 @@ private:
     QMetaObject::Connection onParticipantsChangedConnection_;
     QMetaObject::Connection closeIncomingCallPageConnection_;
     QMetaObject::Connection appStateChangedConnection_;
+    QMetaObject::Connection remoteRecordingChangedConnection_;
 
     /*
      * For Call Overlay