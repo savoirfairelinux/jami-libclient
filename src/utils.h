savoirfairelinux/jami-client-qt

migration: use image provider to show avatar image
1. Use avatarimageprovider
2. Remove redundant base64 code

Change-Id: I2a2517890e95b4a9f9a363fbea2251d6d5dd1c8f

src/utils.h
@@ -100,11 +100,9 @@ bool getReplyMessageBox(QWidget* widget, const QString& title, const QString& te
  * Image manipulation
  */
 static const QSize defaultAvatarSize {128, 128};
-QString getContactImageString(const QString& accountId, const QString& uid);
+QImage contactPhotoFromBase64ByteArray(const QByteArray& data, const QString& type);
+QImage contactPhoto(const QString& contactUri, const QSize& requestedSize = defaultAvatarSize);
 QImage getCirclePhoto(const QImage original, int sizePhoto);
-QImage conversationPhoto(const QString& convUid,
-                         const lrc::api::account::Info& accountInfo,
-                         bool filtered = false);
 QColor getAvatarColor(const QString& canonicalUri);
 QImage fallbackAvatar(const QString& canonicalUriStr,
                       const QString& letterStr = QString(),

savoirfairelinux/jami-libclient

src/utils.h
@@ -100,11 +100,9 @@ bool getReplyMessageBox(QWidget* widget, const QString& title, const QString& te
  * Image manipulation
  */
 static const QSize defaultAvatarSize {128, 128};
-QString getContactImageString(const QString& accountId, const QString& uid);
+QImage contactPhotoFromBase64(const QByteArray& data, const QString& type);
+QImage contactPhoto(const QString& contactUri, const QSize& size = defaultAvatarSize);
 QImage getCirclePhoto(const QImage original, int sizePhoto);
-QImage conversationPhoto(const QString& convUid,
-                         const lrc::api::account::Info& accountInfo,
-                         bool filtered = false);
 QColor getAvatarColor(const QString& canonicalUri);
 QImage fallbackAvatar(const QString& canonicalUriStr,
                       const QString& letterStr = QString(),
@@ -123,6 +121,7 @@ QImage cropImage(const QImage& img);
 QPixmap pixmapFromSvg(const QString& svg_resource, const QSize& size);
 QImage setupQRCode(QString ringID, int margin);
 bool isImage(const QString& fileExt);
+QString generateUid();
 
 /*
  * Misc