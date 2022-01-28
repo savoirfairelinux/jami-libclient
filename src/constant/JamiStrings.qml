savoirfairelinux/jami-client-qt

misc: update version name and credits
Change-Id: I2a34d613f3bccd5f16c333a6a4fa2eed94dc28fa

src/constant/JamiStrings.qml
@@ -35,7 +35,7 @@ Item {
     // AboutPopUp
     property string version: qsTr("Version") + (UpdateManager.isCurrentVersionBeta() ? " (BETA)" : "")
     property string companyDeclarationYear: "© 2015-2021 Savoir-faire Linux Inc."
-    property string slogan: "Together"
+    property string slogan: "Mekhenty"
     property string declaration: qsTr("Jami is a free software for universal communication which respects the freedom and privacy of its users.")
     property string credits: qsTr("Credits")
 

savoirfairelinux/jami-libclient

src/constant/JamiStrings.qml
@@ -35,7 +35,7 @@ Item {
     // AboutPopUp
     property string version: qsTr("Version") + (UpdateManager.isCurrentVersionBeta() ? " (BETA)" : "")
     property string companyDeclarationYear: "© 2015-2021 Savoir-faire Linux Inc."
-    property string slogan: "Together"
+    property string slogan: "Maloya"
     property string declaration: qsTr("Jami is a free software for universal communication which respects the freedom and privacy of its users.")
     property string credits: qsTr("Credits")
 