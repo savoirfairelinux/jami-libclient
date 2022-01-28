savoirfairelinux/jami-client-qt

avadapter: consider devicePixelRatio and screen layout when sharing screens
Gitlab: #395

Change-Id: Ic34a084fb9dc5a6605e3242ae7823db35e01a0d8

src/avadapter.h
@@ -70,6 +70,9 @@ protected:
     Q_INVOKABLE void stopAudioMeter(bool async);
 
 private:
+    // Get screens arrangement rect relative to primary screen.
+    const QRect getScreensArrangementRect();
+
     // Get current callId from current selected conv id.
     QString getCurrentCallId();
 

savoirfairelinux/jami-libclient

src/avadapter.h
@@ -70,6 +70,9 @@ protected:
     Q_INVOKABLE void stopAudioMeter(bool async);
 
 private:
+    // Get screens arrangement rect relative to primary screen.
+    const QRect getAllScreensBoundingRect();
+
     // Get current callId from current selected conv id.
     QString getCurrentCallId();
 