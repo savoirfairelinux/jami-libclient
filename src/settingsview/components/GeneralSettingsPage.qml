savoirfairelinux/jami-client-qt

tooltip: add tool tips to qt client
Change-Id: I7aeb96f4c8a09a8b8c0b8e6534079e3fa3965941

src/settingsview/components/GeneralSettingsPage.qml
@@ -256,6 +256,8 @@ Rectangle {
                             labelText: "Enable desktop notifications"
                             fontPointSize: 11
 
+                            tooltipText: qsTr("toggle enable notifications")
+
                             onSwitchToggled: {
                                 slotSetNotifications(checked)
                             }
@@ -269,6 +271,8 @@ Rectangle {
                             labelText: "Keep minimize on close"
                             fontPointSize: 11
 
+                            tooltipText: qsTr("toggle keep minimized on close")
+
                             onSwitchToggled: {
                                 slotSetClosedOrMin(checked)
                             }
@@ -281,6 +285,8 @@ Rectangle {
 
                             labelText: "Run on Startup"
                             fontPointSize: 11
+
+                            tooltipText: qsTr("toggle run application on system startup")
 
                             onSwitchToggled: {
                                 slotSetRunOnStartUp(checked)
@@ -387,6 +393,7 @@ Rectangle {
 
                             labelText: "Always record calls"
                             fontPointSize: 11
+                            tooltipText: qsTr("toggle always record calls")
 
                             onSwitchToggled: {
                                 slotAlwaysRecordingClicked(checked)
@@ -400,6 +407,8 @@ Rectangle {
 
                             labelText: "Record preview video for a call"
                             fontPointSize: 11
+
+                            tooltipText: qsTr("toggle record preview video for a call")
 
                             onSwitchToggled: {
                                 slotRecordPreviewClicked(checked)
@@ -587,6 +596,8 @@ Rectangle {
                             labelText: "Check for updates automatically"
                             fontPointSize: 11
 
+                            tooltipText: qsTr("toggle automatic updates")
+
                             onSwitchToggled: {
                                 slotSetUpdateAutomatic(checked)
                             }
@@ -614,6 +625,8 @@ Rectangle {
 
                                 text: "Check for updates now"
                                 fontPointSize: 10
+
+                                toolTipText: qsTr("Check for updates now")
 
                                 onClicked: {
                                     checkForUpdateSlot()
@@ -648,6 +661,8 @@ Rectangle {
 
                                 text: "Install the latest beta version"
                                 fontPointSize: 10
+
+                                toolTipText: qsTr("Install the newest beta version")
 
                                 onClicked: {
                                     installBetaSlot()

savoirfairelinux/jami-libclient

src/settingsview/components/GeneralSettingsPage.qml
@@ -194,7 +194,8 @@ Rectangle {
                 source: "qrc:/images/icons/ic_arrow_back_24px.svg"
                 backgroundColor: "white"
                 onExitColor: "white"
-
+                toolTipText: qsTr("Toggle to display side panel")
+                hoverEnabled: true
                 visible: mainViewWindow.sidePanelHidden
 
                 onClicked: {
@@ -273,6 +274,8 @@ Rectangle {
                             labelText: desktopNotificationsElidedText.elidedText
                             fontPointSize: JamiTheme.settingsFontSize
 
+                            tooltipText: qsTr("toggle enable notifications")
+
                             onSwitchToggled: {
                                 slotSetNotifications(checked)
                             }
@@ -292,6 +295,8 @@ Rectangle {
                             labelText: keepMinimizeElidedText.elidedText
                             fontPointSize: JamiTheme.settingsFontSize
 
+                            tooltipText: qsTr("toggle keep minimized on close")
+
                             onSwitchToggled: {
                                 slotSetClosedOrMin(checked)
                             }
@@ -310,6 +315,8 @@ Rectangle {
 
                             labelText: runOnStartupElidedText.elidedText
                             fontPointSize: JamiTheme.settingsFontSize
+
+                            tooltipText: qsTr("toggle run application on system startup")
 
                             onSwitchToggled: {
                                 slotSetRunOnStartUp(checked)
@@ -359,6 +366,7 @@ Rectangle {
                                 icon.height: 24
                                 icon.width: 24
 
+                                toolTipText: qsTr("Press to choose download folder path")
                                 text: downloadPath
                                 fontPointSize: JamiTheme.buttonFontSize
 
@@ -529,6 +537,7 @@ Rectangle {
                                 icon.height: 24
                                 icon.width: 24
 
+                                toolTipText: qsTr("Press to choose record folder path")
                                 text: recordPath
                                 fontPointSize: JamiTheme.buttonFontSize
 
@@ -571,6 +580,8 @@ Rectangle {
                             labelText: autoUpdateText.elidedText
                             fontPointSize: JamiTheme.settingsFontSize
 
+                            tooltipText: qsTr("toggle automatic updates")
+
                             onSwitchToggled: {
                                 slotSetUpdateAutomatic(checked)
                             }
@@ -596,7 +607,8 @@ Rectangle {
 
                             radius: height / 2
 
-                            text: qsTr("Check for updates now")
+                            toolTipText: qsTr("Check for updates now")
+                            text: qsTr("Updates")
                             fontPointSize: JamiTheme.buttonFontSize
 
                             onClicked: {
@@ -617,7 +629,8 @@ Rectangle {
 
                             radius: height / 2
 
-                            text: "Install the latest beta version"
+                            toolTipText: qsTr("Install the latest beta version")
+                            text: qsTr("Beta Install")
                             fontPointSize: JamiTheme.buttonFontSize
 
                             onClicked: {