savoirfairelinux/jami-client-qt

videocallpage: handle conference infos
Change-Id: I4cfc7c9c525c66f4e483089864bec059c388a1bd

src/mainview/components/CallStackView.qml
@@ -157,9 +157,15 @@ Rectangle {
         }
 
         function onCallStatusChanged(status, accountId, convUid) {
-            if (responsibleConvUid === convUid
-                    && responsibleAccountId === accountId) {
+            if (responsibleConvUid === convUid && responsibleAccountId === accountId) {
                 outgoingCallPage.callStatusPresentation = status
+            }
+        }
+
+        function onUpdateParticipantsInfos(infos, accountId, callId) {
+            var responsibleCallId = ClientWrapper.utilsAdaptor.getCallId(responsibleAccountId, responsibleConvUid)
+            if (responsibleCallId === callId) {
+                videoCallPage.handleParticipantsInfos(infos)
             }
         }
     }
@@ -196,6 +202,8 @@ Rectangle {
                 videoCallPage.parent = callStackMainView
                 VideoCallFullScreenWindowContainerCreation.closeVideoCallFullScreenWindowContainer()
             }
+
+            videoCallPage.handleParticipantsInfos(CallAdapter.getConferencesInfos())
         }
     }
 

savoirfairelinux/jami-libclient

src/mainview/components/CallStackView.qml
@@ -157,9 +157,15 @@ Rectangle {
         }
 
         function onCallStatusChanged(status, accountId, convUid) {
-            if (responsibleConvUid === convUid
-                    && responsibleAccountId === accountId) {
+            if (responsibleConvUid === convUid && responsibleAccountId === accountId) {
                 outgoingCallPage.callStatusPresentation = status
+            }
+        }
+
+        function onUpdateParticipantsInfos(infos, accountId, callId) {
+            var responsibleCallId = ClientWrapper.utilsAdaptor.getCallId(responsibleAccountId, responsibleConvUid)
+            if (responsibleCallId === callId) {
+                videoCallPage.handleParticipantsInfo(infos)
             }
         }
     }
@@ -198,6 +204,8 @@ Rectangle {
                 videoCallPage.parent = callStackMainView
                 VideoCallFullScreenWindowContainerCreation.closeVideoCallFullScreenWindowContainer()
             }
+
+            videoCallPage.handleParticipantsInfo(CallAdapter.getConferencesInfos())
         }
     }
 