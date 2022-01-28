savoirfairelinux/jami-client-qt

pluginpreferences: code change to handle windows errors
Change-Id: I885a8d8287511d6769d3c33a255c5216f12c6cac

src/settingsview/components/PluginListPreferencesView.qml
@@ -70,7 +70,8 @@ Rectangle {
     }
 
     function getSize(pluginId, show){
-        size = 50 * ClientWrapper.pluginModel.getPluginPreferences(pluginId).length
+        preferenceItemListModel.pluginId = pluginId
+        size = 50 * preferenceItemListModel.numPreferences
         if (show) {
             height = 200 + size            
             pluginPreferenceView.height = size

savoirfairelinux/jami-libclient

src/settingsview/components/PluginListPreferencesView.qml
@@ -70,7 +70,8 @@ Rectangle {
     }
 
     function getSize(pluginId, show){
-        size = 50 * ClientWrapper.pluginModel.getPluginPreferences(pluginId).length
+        preferenceItemListModel.pluginId = pluginId
+        size = 50 * preferenceItemListModel.preferencesCount
         if (show) {
             height = 200 + size
             pluginPreferenceView.height = size