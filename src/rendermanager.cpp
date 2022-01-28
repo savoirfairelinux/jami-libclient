savoirfairelinux/jami-client-qt

distantrenderer: update frame on rendering stopped and switch
A regression was introduced in c472fe83aeb9a0ed712abad1dfa8b5baf1a60053
where the distantRenderingStopped and setRendererId don't update
the current rendered frame

Change-Id: I5aa477e89ae2031ffd282a2f02ae363dffef569a
GitLab: #269

src/rendermanager.cpp
@@ -184,6 +184,7 @@ FrameWrapper::slotRenderingStopped(const QString& id)
     }
 
     emit renderingStopped(id);
+    QObject::disconnect(renderConnections_.stopped);
 }
 
 RenderManager::RenderManager(AVModel& avModel)
@@ -268,6 +269,11 @@ RenderManager::addDistantRenderer(const QString& id)
                                                              [this](const QString& id) {
                                                                  emit distantFrameUpdated(id);
                                                              });
+        distantConnectionMap_[id].stopped = QObject::connect(dfw.get(),
+                                                             &FrameWrapper::renderingStopped,
+                                                             [this](const QString& id) {
+                                                                 emit distantRenderingStopped(id);
+                                                             });
 
         /*
          * Connect FrameWrapper to avmodel.
@@ -301,6 +307,7 @@ RenderManager::removeDistantRenderer(const QString& id)
         if (dcIt != distantConnectionMap_.end()) {
             QObject::disconnect(dcIt->second.started);
             QObject::disconnect(dcIt->second.updated);
+            QObject::disconnect(dcIt->second.stopped);
         }
 
         /*

savoirfairelinux/jami-libclient

src/rendermanager.cpp
@@ -268,6 +268,11 @@ RenderManager::addDistantRenderer(const QString& id)
                                                              [this](const QString& id) {
                                                                  Q_EMIT distantFrameUpdated(id);
                                                              });
+        distantConnectionMap_[id].stopped = QObject::connect(dfw.get(),
+                                                             &FrameWrapper::renderingStopped,
+                                                             [this](const QString& id) {
+                                                                 Q_EMIT distantRenderingStopped(id);
+                                                             });
 
         /*
          * Connect FrameWrapper to avmodel.
@@ -301,6 +306,7 @@ RenderManager::removeDistantRenderer(const QString& id)
         if (dcIt != distantConnectionMap_.end()) {
             QObject::disconnect(dcIt->second.started);
             QObject::disconnect(dcIt->second.updated);
+            QObject::disconnect(dcIt->second.stopped);
         }
 
         /*