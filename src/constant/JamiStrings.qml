savoirfairelinux/jami-client-qt

multistream: accept only audio in incoming call
GitLab: #392
Change-Id: Ifcd29af849c11166158e7f8d39b65a33325b8d4a

src/constant/JamiStrings.qml
@@ -28,6 +28,8 @@ Item {
 
     // Misc
     property string accept: qsTr("Accept")
+    property string acceptAudio: qsTr("Accept in Audio")
+    property string acceptVideo: qsTr("Accept in Video")
     property string refuse: qsTr("Refuse")
     property string endCall: qsTr("End call")
     property string incomingCallFrom: qsTr("Incoming call from")

savoirfairelinux/jami-libclient

src/constant/JamiStrings.qml
@@ -28,6 +28,8 @@ Item {
 
     // Misc
     property string accept: qsTr("Accept")
+    property string acceptAudio: qsTr("Accept in audio")
+    property string acceptVideo: qsTr("Accept in video")
     property string refuse: qsTr("Refuse")
     property string endCall: qsTr("End call")
     property string incomingAudioCallFrom: qsTr("Incoming audio call from {}")