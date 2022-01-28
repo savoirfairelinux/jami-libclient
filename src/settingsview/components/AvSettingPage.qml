savoirfairelinux/jami-client-qt

mainview: account combo box visible from settings menu
Change-Id: I8eafea9d1cf07a11a8780f8d2f3495480b2360ed

src/settingsview/components/AvSettingPage.qml
@@ -24,6 +24,7 @@ import QtQuick.Layouts 1.3
 import QtGraphicalEffects 1.14
 import QtQuick.Controls.Styles 1.4
 import net.jami.Models 1.0
+import "../../commoncomponents"
 
 Rectangle {
     id: avSettingPage
@@ -216,6 +217,7 @@ Rectangle {
     }
 
     property bool previewAvailable: false
+    signal navigateToSidePanelMenu
 
     Connections{
         target: ClientWrapper.avmodel
@@ -267,18 +269,47 @@ Rectangle {
                     Layout.maximumHeight: 10
                 }
 
-                Label {
+                RowLayout {
+
+                    Layout.alignment: Qt.AlignLeft | Qt.AlignVCenter
+                    Layout.leftMargin: 16
                     Layout.fillWidth: true
-                    Layout.minimumHeight: 25
-                    Layout.preferredHeight: 25
-                    Layout.maximumHeight: 25
+                    Layout.maximumHeight: 64
+                    Layout.minimumHeight: 64
+                    Layout.preferredHeight: 64
 
-                    text: qsTr("Audio / Video")
-                    font.pointSize: 15
-                    font.kerning: true
+                    HoverableButton {
 
-                    horizontalAlignment: Text.AlignLeft
-                    verticalAlignment: Text.AlignVCenter
+                        Layout.alignment: Qt.AlignVCenter | Qt.AlignLeft
+                        Layout.preferredWidth: 30
+                        Layout.preferredHeight: 30
+
+                        radius: 30
+                        source: "qrc:/images/icons/ic_arrow_back_24px.svg"
+                        backgroundColor: "white"
+                        onExitColor: "white"
+
+                        visible: mainViewWindow.sidePanelHidden
+
+                        onClicked: {
+                            navigateToSidePanelMenu()
+                        }
+                    }
+
+
+                    Label {
+                        Layout.fillWidth: true
+                        Layout.minimumHeight: 25
+                        Layout.preferredHeight: 25
+                        Layout.maximumHeight: 25
+
+                        text: qsTr("Audio / Video")
+                        font.pointSize: 15
+                        font.kerning: true
+
+                        horizontalAlignment: Text.AlignLeft
+                        verticalAlignment: Text.AlignVCenter
+                    }
                 }
 
                 Item {

src/settingsview/components/PluginSettingsPage.qml
@@ -45,6 +45,8 @@ Rectangle {
     Layout.fillHeight: true
     Layout.fillWidth: true
 
+    signal navigateToSidePanelMenu
+
     ColumnLayout {
         anchors.fill: parent
         spacing: 6
@@ -52,21 +54,47 @@ Rectangle {
         width: parent.width
         height: parent.height
         
-        Label {
-            width: parent.width
-            height: parent.height
+        RowLayout {
 
-            Layout.leftMargin: 35
-            Layout.topMargin: 15
-            Layout.alignment: Qt.AlignTop
+            Layout.alignment: Qt.AlignLeft | Qt.AlignVCenter
+            Layout.leftMargin: 16
+            Layout.fillWidth: true
+            Layout.maximumHeight: 64
+            Layout.minimumHeight: 64
+            Layout.preferredHeight: 64
 
-            text: qsTr("Plugin")
+            HoverableButton {
 
-            font.pointSize: 15
-            font.kerning: true
+                Layout.alignment: Qt.AlignVCenter | Qt.AlignLeft
+                Layout.preferredWidth: 30
+                Layout.preferredHeight: 30
 
-            horizontalAlignment: Text.AlignBottom
-            verticalAlignment: Text.AlignVCenter
+                radius: 30
+                source: "qrc:/images/icons/ic_arrow_back_24px.svg"
+                backgroundColor: "white"
+                onExitColor: "white"
+
+                visible: mainViewWindow.sidePanelHidden
+
+                onClicked: {
+                    navigateToSidePanelMenu()
+                }
+            }
+
+            Label {
+                Layout.fillWidth: true
+                Layout.minimumHeight: 25
+                Layout.preferredHeight: 25
+                Layout.maximumHeight: 25
+
+                text: qsTr("Plugin")
+
+                font.pointSize: 15
+                font.kerning: true
+
+                horizontalAlignment: Text.AlignLeft
+                verticalAlignment: Text.AlignVCenter
+            }
         }
 
         ScrollView {

savoirfairelinux/jami-libclient

src/settingsview/components/AvSettingPage.qml
@@ -24,6 +24,7 @@ import QtQuick.Layouts 1.3
 import QtGraphicalEffects 1.14
 import QtQuick.Controls.Styles 1.4
 import net.jami.Models 1.0
+import "../../commoncomponents"
 
 Rectangle {
     id: avSettingPage
@@ -216,6 +217,7 @@ Rectangle {
     }
 
     property bool previewAvailable: false
+    signal backArrowClicked
 
     Connections{
         target: ClientWrapper.avmodel
@@ -267,18 +269,47 @@ Rectangle {
                     Layout.maximumHeight: 10
                 }
 
-                Label {
+                RowLayout {
+
+                    Layout.alignment: Qt.AlignLeft | Qt.AlignVCenter
+                    Layout.leftMargin: 16
                     Layout.fillWidth: true
-                    Layout.minimumHeight: 25
-                    Layout.preferredHeight: 25
-                    Layout.maximumHeight: 25
+                    Layout.maximumHeight: 64
+                    Layout.minimumHeight: 64
+                    Layout.preferredHeight: 64
 
-                    text: qsTr("Audio / Video")
-                    font.pointSize: 15
-                    font.kerning: true
+                    HoverableButton {
 
-                    horizontalAlignment: Text.AlignLeft
-                    verticalAlignment: Text.AlignVCenter
+                        Layout.alignment: Qt.AlignVCenter | Qt.AlignLeft
+                        Layout.preferredWidth: 30
+                        Layout.preferredHeight: 30
+
+                        radius: 30
+                        source: "qrc:/images/icons/ic_arrow_back_24px.svg"
+                        backgroundColor: "white"
+                        onExitColor: "white"
+
+                        visible: mainViewWindow.sidePanelHidden
+
+                        onClicked: {
+                            backArrowClicked()
+                        }
+                    }
+
+
+                    Label {
+                        Layout.fillWidth: true
+                        Layout.minimumHeight: 25
+                        Layout.preferredHeight: 25
+                        Layout.maximumHeight: 25
+
+                        text: qsTr("Audio / Video")
+                        font.pointSize: 15
+                        font.kerning: true
+
+                        horizontalAlignment: Text.AlignLeft
+                        verticalAlignment: Text.AlignVCenter
+                    }
                 }
 
                 Item {

src/settingsview/components/PluginSettingsPage.qml
@@ -45,28 +45,56 @@ Rectangle {
     Layout.fillHeight: true
     Layout.fillWidth: true
 
+    signal backArrowClicked
+
     ColumnLayout {
         anchors.fill: parent
         spacing: 6
 
         width: parent.width
         height: parent.height
-        
-        Label {
-            width: parent.width
-            height: parent.height
 
-            Layout.leftMargin: 35
-            Layout.topMargin: 15
-            Layout.alignment: Qt.AlignTop
+        RowLayout {
 
-            text: qsTr("Plugin")
+            Layout.alignment: Qt.AlignLeft | Qt.AlignVCenter
+            Layout.leftMargin: 16
+            Layout.fillWidth: true
+            Layout.maximumHeight: 64
+            Layout.minimumHeight: 64
+            Layout.preferredHeight: 64
 
-            font.pointSize: 15
-            font.kerning: true
+            HoverableButton {
 
-            horizontalAlignment: Text.AlignBottom
-            verticalAlignment: Text.AlignVCenter
+                Layout.alignment: Qt.AlignVCenter | Qt.AlignLeft
+                Layout.preferredWidth: 30
+                Layout.preferredHeight: 30
+
+                radius: 30
+                source: "qrc:/images/icons/ic_arrow_back_24px.svg"
+                backgroundColor: "white"
+                onExitColor: "white"
+
+                visible: mainViewWindow.sidePanelHidden
+
+                onClicked: {
+                    backArrowClicked()
+                }
+            }
+
+            Label {
+                Layout.fillWidth: true
+                Layout.minimumHeight: 25
+                Layout.preferredHeight: 25
+                Layout.maximumHeight: 25
+
+                text: qsTr("Plugin")
+
+                font.pointSize: 15
+                font.kerning: true
+
+                horizontalAlignment: Text.AlignLeft
+                verticalAlignment: Text.AlignVCenter
+            }
         }
 
         ScrollView {
@@ -96,7 +124,7 @@ Rectangle {
 
                     onSwitchToggled: {
                         slotSetPluginEnabled(checked)
-                
+
                         pluginListSettingsView.visible = checked
                         if (!checked) {
                             pluginListPreferencesView.visible = checked
@@ -111,7 +139,7 @@ Rectangle {
                     Layout.fillHeight: true
                     width:380
                     height:100
-                
+
                     // instantiate plugin list setting page
                     PluginListSettingsView {
                         id: pluginListSettingsView