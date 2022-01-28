savoirfairelinux/jami-client-qt

settings: add troubleshooting dialog
Add a dialog to get logs from daemon.

Gitlab: #406
Change-Id: I25dd7084165660e0a03c4694d0225bbcf0f1a9d6

src/settingsadapter.cpp
@@ -1100,3 +1100,14 @@ SettingsAdapter::isAllModeratorsEnabled(const QString& accountId)
 {
     return lrcInstance_->accountModel().isAllModerators(accountId);
 }
+
+void
+SettingsAdapter::monitorAndReceiveLogs(const bool& continuous)
+{
+    lrcInstance_->monitor(continuous);
+    QObject::connect(&lrcInstance_->behaviorController(),
+                     &lrc::api::BehaviorController::debugMessageReceived,
+                     this,
+                     &SettingsAdapter::debugMessageReceived,
+                     Qt::ConnectionType::UniqueConnection);
+}

src/settingsadapter.h
@@ -228,6 +228,11 @@ public:
     Q_INVOKABLE bool isLocalModeratorsEnabled(const QString& accountId);
     Q_INVOKABLE bool isAllModeratorsEnabled(const QString& accountId);
 
+    Q_INVOKABLE void monitorAndReceiveLogs(const bool& continuous);
+
+Q_SIGNALS:
+    void debugMessageReceived(const QString& message);
+
 private:
     AppSettingsManager* settingsManager_;
 };

savoirfairelinux/jami-libclient

src/settingsadapter.cpp
@@ -26,7 +26,13 @@ SettingsAdapter::SettingsAdapter(AppSettingsManager* settingsManager,
                                  QObject* parent)
     : QmlAdapterBase(instance, parent)
     , settingsManager_(settingsManager)
-{}
+{
+    QObject::connect(&lrcInstance_->behaviorController(),
+                     &lrc::api::BehaviorController::debugMessageReceived,
+                     this,
+                     &SettingsAdapter::debugMessageReceived,
+                     Qt::ConnectionType::UniqueConnection);
+}
 
 QString
 SettingsAdapter::getDir_Document()
@@ -1099,4 +1105,10 @@ bool
 SettingsAdapter::isAllModeratorsEnabled(const QString& accountId)
 {
     return lrcInstance_->accountModel().isAllModerators(accountId);
+}
+
+void
+SettingsAdapter::monitor(const bool& continuous)
+{
+    lrcInstance_->monitor(continuous);
 }

src/settingsadapter.h
@@ -228,6 +228,11 @@ public:
     Q_INVOKABLE bool isLocalModeratorsEnabled(const QString& accountId);
     Q_INVOKABLE bool isAllModeratorsEnabled(const QString& accountId);
 
+    Q_INVOKABLE void monitor(const bool& continuous);
+
+Q_SIGNALS:
+    void debugMessageReceived(const QString& message);
+
 private:
     AppSettingsManager* settingsManager_;
 };