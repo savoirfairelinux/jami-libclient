savoirfairelinux/jami-client-qt

welcomepage: re-add credits + changelog
Change-Id: Ib253521997c3271885f83c628e918bb26ae61a2c

src/mainview/MainView.qml
@@ -42,7 +42,7 @@ Window {
     property int mainViewWindowPreferedHeight: 600
     property int sidePanelViewStackPreferedWidth: 250
     property int welcomePageGroupPreferedWidth: 250
-    property int aboutPopUpPreferedWidth: 250
+    property int aboutPopUpPreferedWidth: 400
 
     property int savedSidePanelViewMinWidth: 0
     property int savedSidePanelViewMaxWidth: 0
@@ -508,7 +508,7 @@ Window {
 
         x: Math.round((mainViewWindow.width - width) / 2)
         y: Math.round((mainViewWindow.height - height) / 2)
-        width: Math.max(mainViewWindow.width / 2, aboutPopUpPreferedWidth)
+        width: aboutPopUpPreferedWidth
         height: aboutPopUpDialog.contentHeight
     }
 

savoirfairelinux/jami-libclient

src/mainview/MainView.qml
@@ -43,7 +43,7 @@ Window {
     property int mainViewWindowPreferredHeight: 600
     property int sidePanelViewStackPreferredWidth: 250
     property int mainViewStackPreferredWidth: 250
-    property int aboutPopUpPreferredWidth: 250
+    property int aboutPopUpPreferredWidth: 400
 
     property int savedSidePanelViewMinWidth: 0
     property int savedSidePanelViewMaxWidth: 0
@@ -679,7 +679,7 @@ Window {
 
         x: Math.round((mainViewWindow.width - width) / 2)
         y: Math.round((mainViewWindow.height - height) / 2)
-        width: Math.max(mainViewWindow.width / 2, aboutPopUpPreferredWidth)
+        width: aboutPopUpPreferredWidth
         height: aboutPopUpDialog.contentHeight
     }
 