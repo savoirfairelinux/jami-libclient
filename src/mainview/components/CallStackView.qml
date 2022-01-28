savoirfairelinux/jami-client-qt

mainview: add call recording and full screen mode in audio call
Call view context menu now should always has menu items in audio call

Gitlab: #125
Change-Id: I9bea26d1f95d898c12d8ac2149545273b1494447

src/mainview/components/CallStackView.qml
@@ -25,7 +25,7 @@ import net.jami.Models 1.0
 import net.jami.Adapters 1.0
 
 import "../js/incomingcallpagecreation.js" as IncomingCallPageCreation
-import "../js/videocallfullscreenwindowcontainercreation.js" as VideoCallFullScreenWindowContainerCreation
+import "../js/callfullscreenwindowcontainercreation.js" as CallFullScreenWindowContainerCreation
 
 Rectangle {
     id: callStackViewWindow
@@ -50,7 +50,7 @@ Rectangle {
         // Close potential window, context menu releated windows.
         audioCallPage.closeContextMenuAndRelatedWindows()
 
-        VideoCallFullScreenWindowContainerCreation.closeVideoCallFullScreenWindowContainer()
+        CallFullScreenWindowContainerCreation.closeVideoCallFullScreenWindowContainer()
         videoCallPage.closeContextMenuAndRelatedWindows()
     }
 
@@ -120,6 +120,20 @@ Rectangle {
         videoCallPage.setDistantRendererId(callId)
     }
 
+    function toogleFullScreen(callPage){
+        callPage.isFullscreen = !callPage.isFullscreen
+        CallFullScreenWindowContainerCreation.createvideoCallFullScreenWindowContainerObject()
+
+        if (!CallFullScreenWindowContainerCreation.checkIfVisible()) {
+            CallFullScreenWindowContainerCreation.setAsContainerChild(
+                        callPage)
+            CallFullScreenWindowContainerCreation.showVideoCallFullScreenWindowContainer()
+        } else {
+            callPage.parent = callStackMainView
+            CallFullScreenWindowContainerCreation.closeVideoCallFullScreenWindowContainer()
+        }
+    }
+
     Connections {
         target: CallAdapter
 
@@ -168,6 +182,9 @@ Rectangle {
         id: audioCallPage
 
         property int stackNumber: 0
+        property bool isFullscreen: false
+
+        onNeedToShowInFullScreen: toogleFullScreen(this)
     }
 
     OutgoingCallPage {
@@ -187,19 +204,7 @@ Rectangle {
         property bool isFullscreen: false
 
         onNeedToShowInFullScreen: {
-
-            isFullscreen = !isFullscreen
-            VideoCallFullScreenWindowContainerCreation.createvideoCallFullScreenWindowContainerObject()
-
-            if (!VideoCallFullScreenWindowContainerCreation.checkIfVisible()) {
-                VideoCallFullScreenWindowContainerCreation.setAsContainerChild(
-                            videoCallPage)
-                VideoCallFullScreenWindowContainerCreation.showVideoCallFullScreenWindowContainer()
-            } else {
-                videoCallPage.parent = callStackMainView
-                VideoCallFullScreenWindowContainerCreation.closeVideoCallFullScreenWindowContainer()
-            }
-
+            toogleFullScreen(this)
             videoCallPage.handleParticipantsInfo(CallAdapter.getConferencesInfos())
         }
     }

src/mainview/components/CallViewContextMenu.qml
@@ -65,20 +65,31 @@ Item {
             ContextMenuGenerator.addMenuSeparator()
         }
 
-        if (!isAudioOnly && !isPaused) {
-            ContextMenuGenerator.addMenuItem(isRecording ? JamiStrings.stopRec :
-                                                           JamiStrings.startRec,
-                                             "qrc:/images/icons/av_icons/fiber_manual_record-24px.svg",
+        ContextMenuGenerator.addMenuItem(isRecording ? JamiStrings.stopRec :
+                                                       JamiStrings.startRec,
+                                         "qrc:/images/icons/av_icons/fiber_manual_record-24px.svg",
+                                         function (){
+                                              CallAdapter.recordThisCallToggle()
+                                         })
+
+        if (isAudioOnly && !isPaused)
+            ContextMenuGenerator.addMenuItem(audioCallPage.isFullscreen ? JamiStrings.exitFullScreen :
+                                                                          JamiStrings.fullScreen,
+                                             audioCallPage.isFullscreen ?
+                                                 "qrc:/images/icons/close_fullscreen-24px.svg" :
+                                                 "qrc:/images/icons/open_in_full-24px.svg",
                                              function (){
-                                                  CallAdapter.recordThisCallToggle()
+                                                  audioCallPage.needToShowInFullScreen()
                                              })
+
+        if (!isAudioOnly && !isPaused) {
             ContextMenuGenerator.addMenuItem(videoCallPage.isFullscreen ? JamiStrings.exitFullScreen :
                                                                           JamiStrings.fullScreen,
                                              videoCallPage.isFullscreen ?
                                                  "qrc:/images/icons/close_fullscreen-24px.svg" :
                                                  "qrc:/images/icons/open_in_full-24px.svg",
                                              function (){
-                                                  videoCallPageRect.needToShowInFullScreen()
+                                                  videoCallPage.needToShowInFullScreen()
                                              })
 
             ContextMenuGenerator.addMenuSeparator()

savoirfairelinux/jami-libclient

src/mainview/components/CallStackView.qml
@@ -25,7 +25,7 @@ import net.jami.Models 1.0
 import net.jami.Adapters 1.0
 
 import "../js/incomingcallpagecreation.js" as IncomingCallPageCreation
-import "../js/videocallfullscreenwindowcontainercreation.js" as VideoCallFullScreenWindowContainerCreation
+import "../js/callfullscreenwindowcontainercreation.js" as CallFullScreenWindowContainerCreation
 
 Rectangle {
     id: callStackViewWindow
@@ -50,7 +50,7 @@ Rectangle {
         // Close potential window, context menu releated windows.
         audioCallPage.closeContextMenuAndRelatedWindows()
 
-        VideoCallFullScreenWindowContainerCreation.closeVideoCallFullScreenWindowContainer()
+        CallFullScreenWindowContainerCreation.closeVideoCallFullScreenWindowContainer()
         videoCallPage.closeContextMenuAndRelatedWindows()
     }
 
@@ -120,6 +120,20 @@ Rectangle {
         videoCallPage.setDistantRendererId(callId)
     }
 
+    function toogleFullScreen(callPage){
+        callPage.isFullscreen = !callPage.isFullscreen
+        CallFullScreenWindowContainerCreation.createvideoCallFullScreenWindowContainerObject()
+
+        if (!CallFullScreenWindowContainerCreation.checkIfVisible()) {
+            CallFullScreenWindowContainerCreation.setAsContainerChild(
+                        callPage)
+            CallFullScreenWindowContainerCreation.showVideoCallFullScreenWindowContainer()
+        } else {
+            callPage.parent = callStackMainView
+            CallFullScreenWindowContainerCreation.closeVideoCallFullScreenWindowContainer()
+        }
+    }
+
     Connections {
         target: CallAdapter
 
@@ -168,6 +182,9 @@ Rectangle {
         id: audioCallPage
 
         property int stackNumber: 0
+        property bool isFullscreen: false
+
+        onShowFullScreenReqested: toogleFullScreen(this)
     }
 
     OutgoingCallPage {
@@ -186,20 +203,8 @@ Rectangle {
         property int stackNumber: 2
         property bool isFullscreen: false
 
-        onNeedToShowInFullScreen: {
-
-            isFullscreen = !isFullscreen
-            VideoCallFullScreenWindowContainerCreation.createvideoCallFullScreenWindowContainerObject()
-
-            if (!VideoCallFullScreenWindowContainerCreation.checkIfVisible()) {
-                VideoCallFullScreenWindowContainerCreation.setAsContainerChild(
-                            videoCallPage)
-                VideoCallFullScreenWindowContainerCreation.showVideoCallFullScreenWindowContainer()
-            } else {
-                videoCallPage.parent = callStackMainView
-                VideoCallFullScreenWindowContainerCreation.closeVideoCallFullScreenWindowContainer()
-            }
-
+        onShowFullScreenReqested: {
+            toogleFullScreen(this)
             videoCallPage.handleParticipantsInfo(CallAdapter.getConferencesInfos())
         }
     }

src/mainview/components/CallViewContextMenu.qml
@@ -65,20 +65,31 @@ Item {
             ContextMenuGenerator.addMenuSeparator()
         }
 
-        if (!isAudioOnly && !isPaused) {
-            ContextMenuGenerator.addMenuItem(isRecording ? JamiStrings.stopRec :
-                                                           JamiStrings.startRec,
-                                             "qrc:/images/icons/av_icons/fiber_manual_record-24px.svg",
+        ContextMenuGenerator.addMenuItem(isRecording ? JamiStrings.stopRec :
+                                                       JamiStrings.startRec,
+                                         "qrc:/images/icons/av_icons/fiber_manual_record-24px.svg",
+                                         function (){
+                                              CallAdapter.recordThisCallToggle()
+                                         })
+
+        if (isAudioOnly && !isPaused)
+            ContextMenuGenerator.addMenuItem(audioCallPage.isFullscreen ? JamiStrings.exitFullScreen :
+                                                                          JamiStrings.fullScreen,
+                                             audioCallPage.isFullscreen ?
+                                                 "qrc:/images/icons/close_fullscreen-24px.svg" :
+                                                 "qrc:/images/icons/open_in_full-24px.svg",
                                              function (){
-                                                  CallAdapter.recordThisCallToggle()
+                                                  audioCallPage.showFullScreenReqested()
                                              })
+
+        if (!isAudioOnly && !isPaused) {
             ContextMenuGenerator.addMenuItem(videoCallPage.isFullscreen ? JamiStrings.exitFullScreen :
                                                                           JamiStrings.fullScreen,
                                              videoCallPage.isFullscreen ?
                                                  "qrc:/images/icons/close_fullscreen-24px.svg" :
                                                  "qrc:/images/icons/open_in_full-24px.svg",
                                              function (){
-                                                  videoCallPageRect.needToShowInFullScreen()
+                                                  videoCallPage.showFullScreenReqested()
                                              })
 
             ContextMenuGenerator.addMenuSeparator()