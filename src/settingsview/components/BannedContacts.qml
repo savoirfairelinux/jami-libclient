savoirfairelinux/jami-client-qt

settingsview: fix banned contact list model refresh issue
A small fix to prevent username registration line edit receiving
signal when there is no input is also in since adding banned contact
back will trigger lookupAddress.

Gitlab: #152
Change-Id: I30afb42eab5903aefb7dd19e9f7a5ad77183e866

src/settingsview/components/BannedContacts.qml
@@ -51,15 +51,9 @@ ColumnLayout {
     }
 
     Connections {
-        id: accountConnections_ContactModel
-        target: AccountAdapter.contactModel
-        enabled: root.visible
+        target: SettingsAdapter
 
-        function onModelUpdated(uri, needsSorted) {
-            updateAndShowBannedContactsSlot()
-        }
-
-        function onContactAdded(contactUri) {
+        function onContactUnbanned() {
             updateAndShowBannedContactsSlot()
         }
     }
@@ -83,11 +77,6 @@ ColumnLayout {
 
     function unban(index) {
         SettingsAdapter.unbanContact(index)
-        updateAndShowBannedContactsSlot()
-    }
-
-    function connectCurrentAccount(status) {
-        accountConnections_ContactModel.enabled = status
     }
 
     RowLayout {

savoirfairelinux/jami-libclient

src/settingsview/components/BannedContacts.qml
@@ -51,15 +51,9 @@ ColumnLayout {
     }
 
     Connections {
-        id: accountConnections_ContactModel
-        target: AccountAdapter.contactModel
-        enabled: root.visible
+        target: AccountAdapter
 
-        function onModelUpdated(uri, needsSorted) {
-            updateAndShowBannedContactsSlot()
-        }
-
-        function onContactAdded(contactUri) {
+        function onContactUnbanned() {
             updateAndShowBannedContactsSlot()
         }
     }
@@ -83,11 +77,6 @@ ColumnLayout {
 
     function unban(index) {
         SettingsAdapter.unbanContact(index)
-        updateAndShowBannedContactsSlot()
-    }
-
-    function connectCurrentAccount(status) {
-        accountConnections_ContactModel.enabled = status
     }
 
     RowLayout {