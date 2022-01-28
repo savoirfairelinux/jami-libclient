savoirfairelinux/jami-client-qt

lrcinstance: remove custom setter/getter for currentAccountId
Using a custom getter setter cause the setting of the value to
require manual changed signal emission and in addition to being
error prone, this prevents the changed signal from being emitted
upon initial selection. Some excess workaround code may be removed
in following commits.

This patch also refactors the banned contact signaling as part of
an accountadapter connection cleanup.

Change-Id: I73fb751001e53e086adc7a6a8d706671c2878a77

src/lrcinstance.h
@@ -55,10 +55,8 @@ class LRCInstance : public QObject
 {
     Q_OBJECT
     QML_PROPERTY(QString, selectedConvUid)
-    Q_PROPERTY(lrc::api::profile::Type currentAccountType READ getCurrentAccountType NOTIFY
-                   currentAccountTypeChanged)
-    Q_PROPERTY(QString currentAccountId READ getCurrentAccountId WRITE setCurrentAccountId NOTIFY
-                   currentAccountIdChanged)
+    QML_PROPERTY(QString, currentAccountId)
+    QML_PROPERTY(lrc::api::profile::Type, currentAccountType)
 
 public:
     explicit LRCInstance(migrateCallback willMigrateCb = {},
@@ -88,7 +86,6 @@ public:
 
     const account::Info& getAccountInfo(const QString& accountId);
     const account::Info& getCurrentAccountInfo();
-    profile::Type getCurrentAccountType();
     QString getCallIdForConversationUid(const QString& convUid, const QString& accountId);
     const call::Info* getCallInfo(const QString& callId, const QString& accountId);
     const call::Info* getCallInfoForConversation(const conversation::Info& convInfo,
@@ -105,8 +102,6 @@ public:
     Q_INVOKABLE void makeConversationPermanent(const QString& convId = {},
                                                const QString& accountId = {});
 
-    const QString& getCurrentAccountId();
-    void setCurrentAccountId(const QString& accountId = {});
     int getCurrentAccountIndex();
     void setAvatarForAccount(const QPixmap& avatarPixmap, const QString& accountID);
     void setCurrAccAvatar(const QPixmap& avatarPixmap);
@@ -133,17 +128,12 @@ Q_SIGNALS:
     void quitEngineRequested();
     void conversationUpdated(const QString& convId, const QString& accountId);
     void draftSaved(const QString& convId);
-    void contactBanned(const QString& uri);
-    void currentAccountIdChanged(const QString& accountId);
-    void currentAccountTypeChanged(profile::Type type);
 
 private:
     std::unique_ptr<Lrc> lrc_;
     std::unique_ptr<RenderManager> renderer_;
     std::unique_ptr<UpdateManager> updateManager_;
 
-    QString currentAccountId_ {};
-    profile::Type currentAccountType_ = profile::Type::INVALID;
     MapStringString contentDrafts_;
     MapStringString lastConferences_;
 

tests/unittests/account_unittest.cpp
@@ -55,7 +55,7 @@ TEST_F(AccountFixture, CreateSIPAccountTest)
 {
     // AccountAdded signal spy
     QSignalSpy accountAddedSpy(&globalEnv.lrcInstance->accountModel(),
-                               &lrc::api::NewAccountModel::accountAdded);
+                               &NewAccountModel::accountAdded);
 
     // Create SIP Acc
     globalEnv.accountAdapter->createSIPAccount(QVariantMap());
@@ -67,24 +67,24 @@ TEST_F(AccountFixture, CreateSIPAccountTest)
     QVERIFY(accountAddedArguments.at(0).type() == QVariant::String);
 
     // Select the created account
-    globalEnv.lrcInstance->setCurrentAccountId(accountAddedArguments.at(0).toString());
+    globalEnv.lrcInstance->set_currentAccountId(accountAddedArguments.at(0).toString());
 
     auto accountListSize = globalEnv.lrcInstance->accountModel().getAccountList().size();
     ASSERT_EQ(accountListSize, 1);
 
     // Make sure the account setup is done
     QSignalSpy accountStatusChangedSpy(&globalEnv.lrcInstance->accountModel(),
-                                       &lrc::api::NewAccountModel::accountStatusChanged);
+                                       &NewAccountModel::accountStatusChanged);
 
     QVERIFY(accountStatusChangedSpy.wait());
     QCOMPARE(accountStatusChangedSpy.count(), 1);
 
     // Remove the account
     globalEnv.lrcInstance->accountModel().removeAccount(
-        globalEnv.lrcInstance->getCurrentAccountId());
+        globalEnv.lrcInstance->get_gurrentAccountId());
 
     QSignalSpy accountRemovedSpy(&globalEnv.lrcInstance->accountModel(),
-                                 &lrc::api::NewAccountModel::accountRemoved);
+                                 &NewAccountModel::accountRemoved);
 
     QVERIFY(accountRemovedSpy.wait());
     QCOMPARE(accountRemovedSpy.count(), 1);

savoirfairelinux/jami-libclient

src/lrcinstance.h
@@ -55,10 +55,8 @@ class LRCInstance : public QObject
 {
     Q_OBJECT
     QML_PROPERTY(QString, selectedConvUid)
-    Q_PROPERTY(lrc::api::profile::Type currentAccountType READ getCurrentAccountType NOTIFY
-                   currentAccountTypeChanged)
-    Q_PROPERTY(QString currentAccountId READ getCurrentAccountId WRITE setCurrentAccountId NOTIFY
-                   currentAccountIdChanged)
+    QML_PROPERTY(QString, currentAccountId)
+    QML_RO_PROPERTY(lrc::api::profile::Type, currentAccountType)
 
 public:
     explicit LRCInstance(migrateCallback willMigrateCb = {},
@@ -88,7 +86,6 @@ public:
 
     const account::Info& getAccountInfo(const QString& accountId);
     const account::Info& getCurrentAccountInfo();
-    profile::Type getCurrentAccountType();
     QString getCallIdForConversationUid(const QString& convUid, const QString& accountId);
     const call::Info* getCallInfo(const QString& callId, const QString& accountId);
     const call::Info* getCallInfoForConversation(const conversation::Info& convInfo,
@@ -109,8 +106,6 @@ public:
                                      const QString& accountId,
                                      const QString& content);
 
-    const QString& getCurrentAccountId();
-    void setCurrentAccountId(const QString& accountId = {});
     int getCurrentAccountIndex();
     void setAvatarForAccount(const QPixmap& avatarPixmap, const QString& accountID);
     void setCurrAccAvatar(const QPixmap& avatarPixmap);
@@ -134,17 +129,12 @@ Q_SIGNALS:
     void quitEngineRequested();
     void conversationUpdated(const QString& convId, const QString& accountId);
     void draftSaved(const QString& convId);
-    void contactBanned(const QString& uri);
-    void currentAccountIdChanged(const QString& accountId);
-    void currentAccountTypeChanged(profile::Type type);
 
 private:
     std::unique_ptr<Lrc> lrc_;
     std::unique_ptr<RenderManager> renderer_;
     std::unique_ptr<UpdateManager> updateManager_;
 
-    QString currentAccountId_ {};
-    profile::Type currentAccountType_ = profile::Type::INVALID;
     MapStringString contentDrafts_;
     MapStringString lastConferences_;
 

tests/unittests/account_unittest.cpp
@@ -55,7 +55,7 @@ TEST_F(AccountFixture, CreateSIPAccountTest)
 {
     // AccountAdded signal spy
     QSignalSpy accountAddedSpy(&globalEnv.lrcInstance->accountModel(),
-                               &lrc::api::NewAccountModel::accountAdded);
+                               &NewAccountModel::accountAdded);
 
     // Create SIP Acc
     globalEnv.accountAdapter->createSIPAccount(QVariantMap());
@@ -67,24 +67,24 @@ TEST_F(AccountFixture, CreateSIPAccountTest)
     QVERIFY(accountAddedArguments.at(0).type() == QVariant::String);
 
     // Select the created account
-    globalEnv.lrcInstance->setCurrentAccountId(accountAddedArguments.at(0).toString());
+    globalEnv.lrcInstance->set_currentAccountId(accountAddedArguments.at(0).toString());
 
     auto accountListSize = globalEnv.lrcInstance->accountModel().getAccountList().size();
     ASSERT_EQ(accountListSize, 1);
 
     // Make sure the account setup is done
     QSignalSpy accountStatusChangedSpy(&globalEnv.lrcInstance->accountModel(),
-                                       &lrc::api::NewAccountModel::accountStatusChanged);
+                                       &NewAccountModel::accountStatusChanged);
 
     QVERIFY(accountStatusChangedSpy.wait());
     QCOMPARE(accountStatusChangedSpy.count(), 1);
 
     // Remove the account
     globalEnv.lrcInstance->accountModel().removeAccount(
-        globalEnv.lrcInstance->getCurrentAccountId());
+        globalEnv.lrcInstance->get_currentAccountId());
 
     QSignalSpy accountRemovedSpy(&globalEnv.lrcInstance->accountModel(),
-                                 &lrc::api::NewAccountModel::accountRemoved);
+                                 &NewAccountModel::accountRemoved);
 
     QVERIFY(accountRemovedSpy.wait());
     QCOMPARE(accountRemovedSpy.count(), 1);