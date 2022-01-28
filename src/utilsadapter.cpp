savoirfairelinux/jami-client-qt

misc: add "Default" to translatable strings
GitLab: #599
Change-Id: I3b9de24ffda7275540aaed8655433f8568da1fa7

src/utilsadapter.cpp
@@ -245,7 +245,8 @@ UtilsAdapter::getStringUTF8(QString string)
 QString
 UtilsAdapter::getRecordQualityString(int value)
 {
-    return value ? QString::number(static_cast<float>(value) / 100, 'f', 1) + " Mbps" : "Default";
+    auto value = QString::number(static_cast<float>(value) / 100, 'f', 1);
+    return value ? tr("%1 Mbps").arg(value) : tr("Default");
 }
 
 QString

savoirfairelinux/jami-libclient

src/utilsadapter.cpp
@@ -245,7 +245,8 @@ UtilsAdapter::getStringUTF8(QString string)
 QString
 UtilsAdapter::getRecordQualityString(int value)
 {
-    return value ? QString::number(static_cast<float>(value) / 100, 'f', 1) + " Mbps" : "Default";
+    auto valueStr = QString::number(static_cast<float>(value) / 100, 'f', 1);
+    return value ? tr("%1 Mbps").arg(valueStr) : tr("Default");
 }
 
 QString