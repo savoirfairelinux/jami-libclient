7e9c694fe1a871a0a7f3ec5f26a79946dac8c3b8
Andreas Traczyk <andreas.traczyk@savoirfairelinux.com>
2022-02-10 11:20:11 -0500

    video interface: deobfuscate "qtwrap" layer
    + Removes the signal/slot relay mechanism.(Note: Signals can be
      connected to signals and don't need a relay slot as long as the
      signature is the same)
    + renames startedDecoding to decodingStarted and the same for
      stoppedDecoding.
    
    Change-Id: I02746137efeefa24f8ac65bde073f230b58c1e9c

--- src/qtwrapper/videomanager_wrap.cpp
+++ src/qtwrapper/videomanager_wrap.cpp
@@ -21,69 +21,27 @@
 VideoManagerInterface::VideoManagerInterface()
 {
 #ifdef ENABLE_VIDEO
-
-    proxy = new VideoManagerSignalProxy(this);
-    sender = new VideoManagerProxySender();
-
-    QObject::connect(sender,
-                     &VideoManagerProxySender::deviceEvent,
-                     proxy,
-                     &VideoManagerSignalProxy::slotDeviceEvent,
-                     Qt::QueuedConnection);
-    QObject::connect(sender,
-                     &VideoManagerProxySender::startedDecoding,
-                     proxy,
-                     &VideoManagerSignalProxy::slotStartedDecoding);
-    QObject::connect(sender,
-                     &VideoManagerProxySender::stoppedDecoding,
-                     proxy,
-                     &VideoManagerSignalProxy::slotStoppedDecoding);
-
     using DRing::exportable_callback;
     using DRing::VideoSignal;
     videoHandlers
-        = {exportable_callback<VideoSignal::DeviceEvent>([this]() { emit sender->deviceEvent(); }),
+        = {exportable_callback<VideoSignal::DeviceEvent>([this]() { Q_EMIT deviceEvent(); }),
            exportable_callback<VideoSignal::DecodingStarted>([this](const std::string& id,
                                                                     const std::string& shmPath,
                                                                     int width,
                                                                     int height,
                                                                     bool isMixer) {
-               emit sender->startedDecoding(QString(id.c_str()),
-                                            QString(shmPath.c_str()),
-                                            width,
-                                            height,
-                                            isMixer);
+               Q_EMIT decodingStarted(QString(id.c_str()),
+                                      QString(shmPath.c_str()),
+                                      width,
+                                      height,
+                                      isMixer);
            }),
            exportable_callback<VideoSignal::DecodingStopped>([this](const std::string& id,
                                                                     const std::string& shmPath,
                                                                     bool isMixer) {
-               emit sender->stoppedDecoding(QString(id.c_str()), QString(shmPath.c_str()), isMixer);
+               Q_EMIT decodingStopped(QString(id.c_str()), QString(shmPath.c_str()), isMixer);
            })};
 #endif
 }
 
 VideoManagerInterface::~VideoManagerInterface() {}
-
-VideoManagerSignalProxy::VideoManagerSignalProxy(VideoManagerInterface* parent)
-    : QObject(parent)
-    , m_pParent(parent)
-{}
-
-void
-VideoManagerSignalProxy::slotDeviceEvent()
-{
-    emit m_pParent->deviceEvent();
-}
-
-void
-VideoManagerSignalProxy::slotStartedDecoding(
-    const QString& id, const QString& shmPath, int width, int height, bool isMixer)
-{
-    emit m_pParent->startedDecoding(id, shmPath, width, height, isMixer);
-}
-
-void
-VideoManagerSignalProxy::slotStoppedDecoding(const QString& id, const QString& shmPath, bool isMixer)
-{
-    emit m_pParent->stoppedDecoding(id, shmPath, isMixer);
-}
